2fdcedf69bf91527d4d50da22ab17e3a400bea85
Devon Bush <devonbush@gmail.com>
2022-12-08 21:19:51 -0500

    adding DAO test

---
+++ core/src/test/java/bio/terra/pearl/core/dao/SiteContentDaoTests.java
@@ -0,0 +1,71 @@
+package bio.terra.pearl.core.dao;
+
+import bio.terra.pearl.core.BaseSpringBootTest;
+import bio.terra.pearl.core.dao.site.SiteContentDao;
+import bio.terra.pearl.core.factory.site.SiteContentFactory;
+import bio.terra.pearl.core.model.site.*;
+import bio.terra.pearl.core.service.site.SiteContentService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class SiteContentDaoTests extends BaseSpringBootTest {
+    @Autowired
+    private SiteContentService siteContentService;
+    @Autowired
+    private SiteContentFactory siteContentFactory;
+
+    @Autowired
+    private SiteContentDao siteContentDao;
+
+    @Test
+    @Transactional
+    public void testFindOneFull() {
+        HtmlSection helloSection = HtmlSection.builder()
+                .rawContent("helloSection").build();
+        HtmlSection goodbyeSection = HtmlSection.builder()
+                .rawContent("goodbyeSection").build();
+        HtmlPage landingPage = HtmlPage.builder()
+                .title("home")
+                .sections(Arrays.asList(helloSection, goodbyeSection)).build();
+
+        HtmlSection aboutUsSection = HtmlSection.builder()
+                .rawContent("a great team").build();
+        HtmlPage aboutUsPage = HtmlPage.builder()
+                .title("About Us Title")
+                .sections(Arrays.asList(aboutUsSection)).build();
+        NavbarItem navbarItem = NavbarItem.builder()
+                .htmlPage(aboutUsPage)
+                .label("About Us").build();
+
+        LocalizedSiteContent lsc = LocalizedSiteContent.builder()
+                .language("en")
+                .landingPage(landingPage)
+                .navbarItems(Arrays.asList(navbarItem)).build();
+
+        SiteContent content = siteContentFactory
+                .builderWithDependencies("testSiteContentCrud")
+                .localizedSiteContents(new HashSet<>(Arrays.asList(lsc)))
+                .build();
+        SiteContent savedContent = siteContentService.create(content);
+
+        SiteContent loadedContent = siteContentDao.findOneFull(savedContent.getId(), "en").get();
+
+        Assertions.assertEquals(1, loadedContent.getLocalizedSiteContents().size());
+        LocalizedSiteContent loadedLocal = loadedContent.getLocalizedSiteContents().stream().findFirst().get();
+        Assertions.assertEquals(1, loadedLocal.getNavbarItems().size());
+        NavbarItem loadedItem = loadedLocal.getNavbarItems().get(0);
+        Assertions.assertEquals("About Us", loadedItem.getLabel());
+        HtmlPage loadedAboutUs = loadedItem.getHtmlPage();
+        Assertions.assertEquals("About Us Title", loadedAboutUs.getTitle());
+        Assertions.assertEquals("a great team", loadedAboutUs.getSections().get(0).getRawContent());
+
+        HtmlPage loadedLandingPage = loadedLocal.getLandingPage();
+        Assertions.assertEquals(2, loadedLandingPage.getSections().size());
+        Assertions.assertEquals("helloSection", loadedLandingPage.getSections().get(0).getRawContent());
+    }
+}
