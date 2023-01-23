5b69a4d7e2a8c7a364365b5aec1f625de23c5c6a
Devon <devonbush@gmail.com>
2023-01-23 15:45:15 -0500

    AR-90 enabling serving of ui-admin from api-admin (#34)
    * enabling serving of ui-admin from api-admin
    
    * cleanups
    
    * fixing task dependency
    
    * fixing task dependencies
    
    * test fixes
    
    * peer review feedback
    
    * checkstyle fix
    
    * test fix

--- api-admin/src/main/java/bio/terra/pearl/api/admin/controller/PublicApiController.java
+++ api-admin/src/main/java/bio/terra/pearl/api/admin/controller/PublicApiController.java
@@ -5,6 +5,7 @@ import bio.terra.pearl.api.admin.config.VersionConfiguration;
 import bio.terra.pearl.api.admin.model.SystemStatus;
 import bio.terra.pearl.api.admin.model.VersionProperties;
 import bio.terra.pearl.api.admin.service.StatusService;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -41,13 +42,19 @@ public class PublicApiController implements PublicApi {
     return ResponseEntity.ok(currentVersion);
   }
 
-  @GetMapping(value = "/")
-  public String index() {
-    return "redirect:swagger-ui.html";
-  }
-
+  /** map the swagger ui page */
   @GetMapping(value = "/swagger-ui.html")
   public String getSwagger() {
-    return "index";
+    return "swagger-ui.html";
+  }
+
+  /**
+   * enable react router to handle all non-api, non-resource paths by routing everything else to the
+   * index path adapted from
+   * https://stackoverflow.com/questions/47689971/how-to-work-with-react-routers-and-spring-boot-controller
+   */
+  @GetMapping(value = {"/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}"})
+  public String getIndex(HttpServletRequest request) {
+    return "forward:/";
   }
 }
