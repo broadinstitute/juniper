0e4ff15a1c3ed45d59cdfafa810633a0a97e8366
Brian Reilly <breilly@broadinstitute.org>
2023-02-22 16:49:04 -0500

    Add config endpoint/resource for use by the UI (#63)

--- ui-admin/src/App.tsx
+++ ui-admin/src/App.tsx
@@ -8,8 +8,8 @@ import './App.css'
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import { ReactNotifications } from 'react-notifications-component'
 
-import { RedirectFromOAuth } from './login/RedirectFromOAuth'
-import { ProtectedRoute } from './login/ProtectedRoute'
+import { RedirectFromOAuth } from 'login/RedirectFromOAuth'
+import { ProtectedRoute } from 'login/ProtectedRoute'
 import NavbarProvider, { NavbarContext } from 'navbar/NavbarProvider'
 import AdminNavbar from 'navbar/AdminNavbar'
 import PortalList from 'portal/PortalList'
@@ -17,40 +17,47 @@ import PortalProvider from 'portal/PortalProvider'
 import PortalDashboard from 'portal/PortalDashboard'
 
 import StudyRouter from 'study/StudyRouter'
-import { getOidcConfig } from './authConfig'
+import UserProvider from 'user/UserProvider'
+import ConfigProvider, { ConfigConsumer } from 'providers/ConfigProvider'
+import { getOidcConfig } from 'authConfig'
 import { AuthProvider } from 'react-oidc-context'
-import UserProvider from './user/UserProvider'
 
 
 /** container for the app including the router  */
 function App() {
   return (
-    <AuthProvider {...getOidcConfig()}>
-      <UserProvider>
-        <div className="App">
-          <ReactNotifications />
-          <NavbarProvider>
-            <BrowserRouter>
-              <Routes>
-                <Route element={<ProtectedRoute/>}>
-                  <Route path="/" element={<PageFrame/>}>
-                    <Route path=":portalShortcode" element={<PortalProvider/>}>
-                      <Route path="studies">
-                        <Route path=":studyShortcode/*" element={<StudyRouter/>}/>
+    <ConfigProvider>
+      <ConfigConsumer>
+        { config =>
+          <AuthProvider {...getOidcConfig(config.b2cTenantName, config.b2cClientId)}>
+            <UserProvider>
+              <div className="App">
+                <ReactNotifications />
+                <NavbarProvider>
+                  <BrowserRouter>
+                    <Routes>
+                      <Route element={<ProtectedRoute/>}>
+                        <Route path="/" element={<PageFrame/>}>
+                          <Route path=":portalShortcode" element={<PortalProvider/>}>
+                            <Route path="studies">
+                              <Route path=":studyShortcode/*" element={<StudyRouter/>}/>
+                            </Route>
+                            <Route index element={<PortalDashboard/>}/>
+                          </Route>
+                          <Route index element={<PortalList/>}/>
+                        </Route>
+                        <Route path='redirect-from-oauth' element={<RedirectFromOAuth/>}/>
+                        <Route path="*" element={<div>Unknown page</div>}/>
                       </Route>
-                      <Route index element={<PortalDashboard/>}/>
-                    </Route>
-                    <Route index element={<PortalList/>}/>
-                  </Route>
-                  <Route path='redirect-from-oauth' element={<RedirectFromOAuth/>}/>
-                  <Route path="*" element={<div>Unknown page</div>}/>
-                </Route>
-              </Routes>
-            </BrowserRouter>
-          </NavbarProvider>
-        </div>
-      </UserProvider>
-    </AuthProvider>
+                    </Routes>
+                  </BrowserRouter>
+                </NavbarProvider>
+              </div>
+            </UserProvider>
+          </AuthProvider>
+        }
+      </ConfigConsumer>
+    </ConfigProvider>
   )
 }
 
