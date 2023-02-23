c9c3888e272faf2607c45cc2be65af47d6eff682
Devon Bush <devonbush@gmail.com>
2023-02-23 13:23:19 -0500

    adding more about us bios

--- ui-participant/src/landing/sections/PhotoBlurbGrid.tsx
+++ ui-participant/src/landing/sections/PhotoBlurbGrid.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import PearlImage, { PearlImageProps } from '../../util/PearlImage'
+import PearlImage, {PearlImageProps} from '../../util/PearlImage'
 import ReactMarkdown from 'react-markdown'
 
 type PhotoBlurbGridProps = {
@@ -25,12 +25,12 @@ type PhotoBio = {
 /**
  * Template for rendering a hero with centered content.
  */
-function PhotoBlurbGrid({ config: { background, backgroundColor, color, subGrids, title } }:
+function PhotoBlurbGrid({config: {background, backgroundColor, color, subGrids, title}}:
                           { config: PhotoBlurbGridProps }) {
   if (!subGrids) {
     subGrids = []
   }
-  return <div style={{ background, backgroundColor, color }}>
+  return <div style={{background, backgroundColor, color}}>
     {title && <h1 className="fs-1 fw-normal lh-sm text-center">
       {title}
     </h1>}
@@ -39,10 +39,10 @@ function PhotoBlurbGrid({ config: { background, backgroundColor, color, subGrids
 }
 
 /** renders a subgrouping of photos (e.g. "Our researchers") */
-function SubGridView({ subGrid }: { subGrid: SubGrid }) {
+function SubGridView({subGrid}: { subGrid: SubGrid }) {
   return <div className="row justify-content-center">
     <div className="col-md-8">
-      {subGrid.title && <h5>{subGrid.title}</h5>}
+      {subGrid.title && <h3 className="text-center mt-3">{subGrid.title}</h3>}
       <div className="row">
         {subGrid.photoBios.map((bio, index) => <PhotoBioView photoBio={bio} key={index}/>)}
       </div>
@@ -51,11 +51,11 @@ function SubGridView({ subGrid }: { subGrid: SubGrid }) {
 }
 
 /** renders a single bio with pic */
-function PhotoBioView({ photoBio }: { photoBio: PhotoBio }) {
-  return <div className="col-md-4 gx-3 gy-3 d-flex flex-column">
+function PhotoBioView({photoBio}: { photoBio: PhotoBio }) {
+  return <div className="col-md-4 gx-5 gy-3 d-flex flex-column">
     <PearlImage image={photoBio.image}/>
     <div>{photoBio.name} <span className="detail">{photoBio.title}</span></div>
-    <div className="detail">
+    <div className="detail" style={{lineHeight: 1}}>
       <ReactMarkdown>{photoBio.blurb ? photoBio.blurb : ''}</ReactMarkdown>
     </div>
   </div>
