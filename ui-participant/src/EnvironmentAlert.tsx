62b8f56f8db4593f5e7d08adc2fb5401719a4637
Devon Bush <devonbush@gmail.com>
2023-02-24 10:21:44 -0500

    fixing z index bug

--- ui-participant/src/EnvironmentAlert.tsx
+++ ui-participant/src/EnvironmentAlert.tsx
@@ -3,7 +3,7 @@ import { PortalEnvironment } from 'api/api'
 
 /** shows a small banner in the top center indicating a non-live environment */
 export default function EnvironmentAlert({ portalEnvironment }: { portalEnvironment: PortalEnvironment }) {
-  const [isVisible, setIsVisible] = useState(portalEnvironment.environmentName !== 'PRODUCTION')
+  const [isVisible, setIsVisible] = useState(portalEnvironment.environmentName !== 'live')
   const alertStyle = {
     background: 'rgba(200, 200, 200, 0.5)',
     color: 'black',
@@ -12,7 +12,8 @@ export default function EnvironmentAlert({ portalEnvironment }: { portalEnvironm
     borderTop: 'none',
     borderBottomLeftRadius: '5px',
     borderBottomRightRadius: '5px',
-    transform: 'rotateY(90deg)'
+    transform: 'rotateY(90deg)',
+    zIndex: 100
   }
 
   if (!isVisible) {
