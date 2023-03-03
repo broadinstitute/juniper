a1cdba021aa24c726111edc8382a1c9c3a83650c
breilly <breilly@broadinstitute.org>
2023-03-03 09:33:37 -0500

    eslint cleanup

--- ui-participant/src/authConfig.ts
+++ ui-participant/src/authConfig.ts
@@ -9,29 +9,20 @@ const aadB2cName = process.env.REACT_APP_B2C_TENANT_NAME ? process.env.REACT_APP
 const aadb2cClientId = process.env.REACT_APP_B2C_CLIENT_ID ? process.env.REACT_APP_B2C_CLIENT_ID : 'ID_NEEDED'
 
 // TODO: This is a modified copy of code from Terra UI. It could use some clean-up.
+/* eslint-disable camelcase, max-len */
 export const getOidcConfig = (b2cTenantName: string = aadB2cName, b2cClientId: string = aadb2cClientId) => {
   const metadata = {
-    // eslint-disable-next-line camelcase
     authorization_endpoint:
-      // eslint-disable-next-line max-len
       `https://${b2cTenantName}.b2clogin.com/${b2cTenantName}.onmicrosoft.com/B2C_1A_ddp_participant_signup_signin_dev/oauth2/v2.0/authorize`,
-    // eslint-disable-next-line camelcase
     token_endpoint:
-      // eslint-disable-next-line max-len
       `https://${b2cTenantName}.b2clogin.com/${b2cTenantName}.onmicrosoft.com/B2C_1A_ddp_participant_signup_signin_dev/oauth2/v2.0/token`
   }
   return {
     authority:
-      // eslint-disable-next-line max-len
       `https://${b2cTenantName}.b2clogin.com/${b2cTenantName}.onmicrosoft.com/B2C_1A_ddp_participant_signup_signin_dev`,
-    // eslint-disable-next-line camelcase
     client_id: b2cClientId,
-    // eslint-disable-next-line camelcase
     popup_redirect_uri: `${window.origin}/redirect-from-oauth`,
-    // popup_redirect_uri: `${window.origin}/redirect-from-oauth`,
-    // eslint-disable-next-line camelcase
     silent_redirect_uri: `${window.origin}/redirect-from-oauth-silent`,
-    // silent_redirect_uri: `${window.origin}/redirect-from-oauth-silent`,
     metadata,
     prompt: 'consent login',
     scope: 'openid email profile',
@@ -43,9 +34,8 @@ export const getOidcConfig = (b2cTenantName: string = aadB2cName, b2cClientId: s
     // token won't expire between 2 setCookie api calls
     accessTokenExpiringNotificationTimeInSeconds: 330,
     includeIdTokenInSilentRenew: true,
-    // eslint-disable-next-line camelcase
     extraQueryParams: { access_type: 'offline' },
-    // eslint-disable-next-line camelcase
     redirect_uri: ''
   }
 }
+/* eslint-enable camelcase, max-len */
