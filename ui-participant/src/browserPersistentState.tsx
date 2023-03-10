4c95101c68c0718379c23274780c97511c8c17e0
breilly <breilly@broadinstitute.org>
2023-03-10 16:07:17 -0500

    move some browser-persistent state from local storage to session storage

--- ui-participant/src/browserPersistentState.tsx
+++ ui-participant/src/browserPersistentState.tsx
@@ -1,13 +1,13 @@
-import { useLocalStorage } from 'util/storageUtils'
+import { useSessionStorage } from 'util/storageUtils'
 
 /** store the preregistration response id in local storage so a page refresh does not lose their progress.
  * The user isn't signed in yet (since they don't have an account), so local storage is the best way to keep this. */
-export const usePreRegResponseId = () => useLocalStorage('preRegResponseId')
+export const usePreRegResponseId = () => useSessionStorage('preRegResponseId')
 
 /** store the preregistration response id in local storage so a page refresh does not lose their progress.
  * The user might not be signed in yet (since they don't have an account),
  * so local storage is the best way to keep this. */
-export const usePreEnrollResponseId = () => useLocalStorage('preEnrollResponseId')
+export const usePreEnrollResponseId = () => useSessionStorage('preEnrollResponseId')
 
 /** store the study being enrolled in so that we remember it when coming back from B2C sign-up. */
-export const useReturnToStudy = () => useLocalStorage('returnToStudy')
+export const useReturnToStudy = () => useSessionStorage('returnToStudy')
