3b47947ab46f6fcf62d153b5e30bb44610e60613
Nick Watts <1156625+nawatts@users.noreply.github.com>
2023-03-13 14:14:34 -0400

    Rename template configuration types (#119)

--- ui-participant/src/landing/sections/RawHtmlTemplate.tsx
+++ ui-participant/src/landing/sections/RawHtmlTemplate.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
+type RawHtmlTemplateProps = {
+  anchorRef?: string
+  rawContent: string | null
+}
+
 /**
  * renders raw html content
  * TODO -- determine whether we need to sanitize the content here or whether we trust our database and
  * @param content
  * @constructor
  */
-export default function RawHtmlTemplate({ anchorRef, rawContent }: { anchorRef?: string, rawContent: string | null }) {
+export default function RawHtmlTemplate(props: RawHtmlTemplateProps) {
+  const { anchorRef, rawContent } = props
   return <div id={anchorRef} dangerouslySetInnerHTML={{ __html: rawContent ? rawContent : '' }}></div>
 }
