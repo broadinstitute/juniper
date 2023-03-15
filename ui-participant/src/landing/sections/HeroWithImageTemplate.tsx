285bff5e65191a151b5c392fb08339032828891c
Nick Watts <nwatts@broadinstitute.org>
2023-03-15 15:57:00 -0400

    Add fullWidth and imageSize configuration options to HeroWithImage

--- ui-participant/src/landing/sections/HeroWithImageTemplate.tsx
+++ ui-participant/src/landing/sections/HeroWithImageTemplate.tsx
@@ -12,10 +12,12 @@ type HeroWithImageTemplateConfig = {
   backgroundImage?: PearlImageConfig, // background image
   blurb?: string, //  text below the title
   buttons?: ButtonConfig[], // array of objects containing `text` and `href` attributes
-  title?: string, // large heading text
+  fullWidth?: boolean, // span the full page width or not
   image?: PearlImageConfig, // image
   imagePosition?: string, // left or right.  Default is right
-  logos?: PearlImageConfig[]
+  imageSize?: number, // number between 0 and 1. Amount of space given to image.
+  logos?: PearlImageConfig[],
+  title?: string // large heading text
 }
 
 type HeroWithImageTemplateProps = {
@@ -31,11 +33,13 @@ function HeroWithImageTemplate(props: HeroWithImageTemplateProps) {
     anchorRef,
     config: {
       background,
+      backgroundImage,
       blurb,
       buttons,
+      fullWidth = false,
       image,
       imagePosition,
-      backgroundImage,
+      imageSize: configuredImageSize,
       logos,
       title
     }
@@ -45,47 +49,69 @@ function HeroWithImageTemplate(props: HeroWithImageTemplateProps) {
   if (backgroundImage) {
     styleProps.backgroundImage = `url('${getImageUrl(backgroundImage.cleanFileName, backgroundImage.version)}')`
   }
+
   const isLeftImage = imagePosition === 'left' // default is right, so left has to be explicitly specified
+  const imageSize = _.isNumber(configuredImageSize) ? _.clamp(configuredImageSize, 0, 1) : (fullWidth ? 0.5 : 0.34)
+  const imageCols = Math.max(Math.floor(imageSize * 12), 1)
+
   return (
     <div
       className={classNames('row', 'mx-0', isLeftImage ? 'flex-row' : 'flex-row-reverse')}
       id={anchorRef}
       style={styleProps}
     >
-      {!!image && (
-        <div className="col-12 col-lg-6 d-flex justify-content-center align-items-center p-0">
-          <PearlImage image={image} className="img-fluid"/>
-        </div>
-      )}
       <div
-        className="col-12 col-lg-6 py-3 p-sm-3 p-lg-5 d-flex flex-column flex-grow-1 justify-content-around"
+        className={classNames(
+          'row',
+          'col-12',
+          fullWidth ? 'mx-0' : 'col-sm-10 mx-auto',
+          isLeftImage ? 'flex-row' : 'flex-row-reverse'
+        )}
       >
-        {!!title && (
-          <h1 className="fs-1 fw-normal lh-sm">
-            <ReactMarkdown>{title}</ReactMarkdown>
-          </h1>
-        )}
-        {!!blurb && (
-          <div className="fs-4">
-            <ReactMarkdown>{blurb}</ReactMarkdown>
+        {!!image && (
+          <div
+            className={classNames(
+              'col-12', `col-lg-${imageCols}`,
+              'd-flex justify-content-center align-items-center p-0'
+            )}
+          >
+            <PearlImage image={image} className="img-fluid"/>
           </div>
         )}
-        {(buttons || []).length > 0 && (
-          <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-            {
-              _.map(buttons, (buttonConfig, i) =>
-                <ConfiguredButton key={i} config={buttonConfig} className="btn-lg px-4 me-md-2"/>
-              )
-            }
-          </div>
-        )}
-        {(logos || []).length > 0 && (
-          <div className="d-flex flex-wrap align-items-center justify-content-between">
-            {_.map(logos, logo => {
-              return <PearlImage key={logo.cleanFileName} image={logo} className={'m-1'}/>
-            })}
-          </div>
-        )}
+        <div
+          className={classNames(
+            'col-12', `col-lg-${12 - imageCols}`,
+            'py-3 p-sm-3 p-lg-5',
+            'd-flex flex-column flex-grow-1 justify-content-around'
+          )}
+        >
+          {!!title && (
+            <h1 className="fs-1 fw-normal lh-sm">
+              <ReactMarkdown>{title}</ReactMarkdown>
+            </h1>
+          )}
+          {!!blurb && (
+            <div className="fs-4">
+              <ReactMarkdown>{blurb}</ReactMarkdown>
+            </div>
+          )}
+          {(buttons || []).length > 0 && (
+            <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+              {
+                _.map(buttons, (buttonConfig, i) =>
+                  <ConfiguredButton key={i} config={buttonConfig} className="btn-lg px-4 me-md-2"/>
+                )
+              }
+            </div>
+          )}
+          {(logos || []).length > 0 && (
+            <div className="d-flex flex-wrap align-items-center justify-content-between">
+              {_.map(logos, logo => {
+                return <PearlImage key={logo.cleanFileName} image={logo} className={'m-1'}/>
+              })}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
