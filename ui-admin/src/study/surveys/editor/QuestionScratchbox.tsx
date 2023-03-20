d0b12d382ea069c7718c8ec447264edbdd7c9a90
Devon Bush <devonbush@gmail.com>
2023-03-20 16:39:16 -0400

    adding family history survey

--- ui-admin/src/study/surveys/editor/QuestionScratchbox.tsx
+++ ui-admin/src/study/surveys/editor/QuestionScratchbox.tsx
@@ -8,7 +8,7 @@ import { questionFromRawText, QuestionObj, panelObjsToJson, questionToJson, Pane
 export default function QuestionScratchbox() {
   const [rawText, setRawText] = useState(' ')
   const [questionObj, setQuestionObj] = useState<QuestionObj>({
-    namePrefix: 'oh_oh_medList_', nameSuffix: '', type: 'radiogroup', isRequired: true,
+    namePrefix: 'oh_oh_lifestyle_', nameSuffix: '', type: 'radiogroup', isRequired: true,
     title: '', choices: [], otherText: '', otherPlaceholder: 'Please specify'
   })
   const [showSubPanels, setShowSubPanels] = useState(false)
@@ -73,6 +73,7 @@ export default function QuestionScratchbox() {
               <option>radiogroup</option>
               <option>checkbox</option>
               <option>text</option>
+              <option>dropdown</option>
             </select></label>
           <br/>
 
