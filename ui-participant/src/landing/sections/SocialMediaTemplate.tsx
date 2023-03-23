eb4380a0df96de6ba45a852c57da86fab574bbb4
Nick Watts <nwatts@broadinstitute.org>
2023-03-23 14:40:14 -0400

    Add social media links to home page

--- ui-participant/src/landing/sections/SocialMediaTemplate.tsx
+++ ui-participant/src/landing/sections/SocialMediaTemplate.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
+import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import React from 'react'
 
 import { SectionConfig } from 'api/api'
@@ -5,59 +8,84 @@ import { getSectionStyle } from 'util/styleUtils'
 import { withValidatedSectionConfig } from 'util/withValidatedSectionConfig'
 import { requireOptionalString } from 'util/validationUtils'
 
-import PearlImage from '../PearlImage'
-
 import { TemplateComponentProps } from './templateUtils'
 
 type SocialMediaTemplateConfig = {
-  facebookHref?: string, // URL of Facebook page
-  instagramHref?: string, // URL of Instagram page
-  twitterHref?: string, // URL of Twitter page
+  facebookHandle?: string, // Facebook handle
+  instagramHandle?: string, // Instagram handle
+  twitterHandle?: string, // Twitter handle
 }
 
 /** Validate that a section configuration object conforms to SocialMediaTemplateConfig */
 const validateSocialMediaTemplateConfig = (config: SectionConfig): SocialMediaTemplateConfig => {
   const message = 'Invalid SocialMediaTemplateConfig'
-  const facebookHref = requireOptionalString(config, 'facebookHref', message)
-  const instagramHref = requireOptionalString(config, 'instagramHref', message)
-  const twitterHref = requireOptionalString(config, 'twitterHref', message)
+  const facebookHandle = requireOptionalString(config, 'facebookHandle', message)
+  const instagramHandle = requireOptionalString(config, 'instagramHandle', message)
+  const twitterHandle = requireOptionalString(config, 'twitterHandle', message)
   return {
-    facebookHref,
-    instagramHref,
-    twitterHref
+    facebookHandle,
+    instagramHandle,
+    twitterHandle
   }
+}
+
+
+type SocialMediaLinkProps = {
+  icon: IconDefinition
+  label: string
+  url: string
+}
+
+const SocialMediaLink = (props: SocialMediaLinkProps) => {
+  const { icon, label, url } = props
+  return (
+    <a className="px-2" href={url}>
+      <span className="visually-hidden">{label}</span>
+      <FontAwesomeIcon icon={icon} style={{ height: '3rem' }} />
+    </a>
+  )
 }
 
 type SocialMediaTemplateProps = TemplateComponentProps<SocialMediaTemplateConfig>
 
 /**
  * Template for a hero with social media links.
- * TODO -- implement images
  */
 function SocialMediaTemplate(props: SocialMediaTemplateProps) {
   const { anchorRef, config } = props
   const {
-    facebookHref,
-    instagramHref,
-    twitterHref
+    facebookHandle,
+    instagramHandle,
+    twitterHandle
   } = config
 
-  return <div id={anchorRef} className="container py-5" style={getSectionStyle(config)}>
-    <div className="d-flex justify-content-center mt-5 mb-4">
-      {twitterHref &&
-        <PearlImage image={{ cleanFileName: 'twitter.png', version: 1, alt: 'Twitter' }}
-          className="m-3" style={{ width: '56px' }}/>
-      }
-      {facebookHref &&
-        <PearlImage image={{ cleanFileName: 'facebook.png', version: 1, alt: 'Facebook' }}
-          className="m-3" style={{ width: '54px' }}/>
-      }
-      {instagramHref &&
-        <PearlImage image={{ cleanFileName: 'instagram.png', version: 1, alt: 'Instagram' }}
-          className="m-3" style={{ width: '49px' }}/>
-      }
+  return (
+    <div id={anchorRef} className="row py-5" style={getSectionStyle(config)}>
+      <div className="hstack justify-content-center gap-2">
+        {twitterHandle && (
+          <SocialMediaLink
+            icon={faTwitter}
+            label="Twitter"
+            url={`https://twitter.com/${twitterHandle}`}
+          />
+        )}
+        {facebookHandle && (
+          <SocialMediaLink
+            icon={faFacebook}
+            label="Facebook"
+            url={`https://facebook.com/${facebookHandle}`}
+          />
+        )}
+        {instagramHandle && (
+          <SocialMediaLink
+            icon={faInstagram}
+            label="Instagram"
+            url={`https://instagram.com/${instagramHandle}`}
+          />
+        )}
+      </div>
     </div>
-  </div>
+  )
 }
 
 export default withValidatedSectionConfig(validateSocialMediaTemplateConfig, SocialMediaTemplate)
