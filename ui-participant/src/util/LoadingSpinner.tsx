8999753863d16af1660adbf13b160a6ad4589ee4
Nick Watts <1156625+nawatts@users.noreply.github.com>
2023-03-24 11:04:14 -0400

    Update loading spinner (#171)
    Co-authored-by: Devon <devonbush@gmail.com>

--- ui-participant/src/util/LoadingSpinner.tsx
+++ ui-participant/src/util/LoadingSpinner.tsx
@@ -1,20 +1,49 @@
-import React from 'react'
+import classNames from 'classnames'
+import React, { CSSProperties, useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDna } from '@fortawesome/free-solid-svg-icons/faDna'
 
-/** show dna spinning indicator.
- * If no arguments or children are passed in, the spinner is rendered
- * Alternatively, children can be passed in, in which case either the spinner or the children
- * are rendered, contingent on the isLoading property
- */
-export default function LoadingSpinner({ children, testId, isLoading }:
-                                         { children?: React.ReactNode, testId?: string, isLoading?: boolean }) {
-  const spinner = <FontAwesomeIcon icon={faDna} className="gene-load-spinner" data-testid={testId}/>
-  if (!children && typeof (isLoading) === 'undefined') {
-    return spinner
-  }
-  return <>
-    {isLoading && spinner}
-    {!isLoading && children}
-  </>
+import { DelayedRender } from './DelayedRender'
+
+type LoadingSpinnerProps = {
+  className?: string
+  style?: CSSProperties
+  testId?: string
+}
+
+/** Spinning DNA icon */
+const LoadingSpinner = (props: LoadingSpinnerProps) => {
+  const { className, style, testId } = props
+  return (
+    <FontAwesomeIcon
+      icon={faDna}
+      className={classNames('gene-load-spinner', className)}
+      data-testid={testId}
+      style={style}
+    />
+  )
+}
+
+/** Show an overlay with a loading page over the full page.  If the page takes >150ms to load,
+  a. loading spinner will be displayed */
+export const PageLoadingIndicator = () => {
+  // Render backdrop then add "show" class for transition on opacity.
+  const [showBackdrop, setShowBackdrop] = useState(false)
+  useEffect(() => {
+    setTimeout(() => {
+      setShowBackdrop(true)
+    }, 0)
+  }, [])
+
+  return (
+    <DelayedRender delay={150}>
+      <div
+        className={classNames('modal-backdrop fade', { show: showBackdrop })}
+        style={{ '--bs-backdrop-opacity': 0.15 } as CSSProperties}
+      />
+      <div className="modal d-flex justify-content-center align-items-center">
+        <LoadingSpinner style={{ height: 50 }} />
+      </div>
+    </DelayedRender>
+  )
 }
