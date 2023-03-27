f5b2c36543eea36d0098e3a02809db381485bc6c
Matt Bemis <MatthewBemis@users.noreply.github.com>
2023-03-27 12:49:10 -0400

    [AR-141] Data Dictionary (#155)
    Co-authored-by: Devon <devonbush@gmail.com>

---
+++ core/src/main/java/bio/terra/pearl/core/service/survey/SurveyParseUtils.java
@@ -0,0 +1,82 @@
+package bio.terra.pearl.core.service.survey;
+
+import bio.terra.pearl.core.model.survey.Survey;
+import bio.terra.pearl.core.model.survey.SurveyQuestionDefinition;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SurveyParseUtils {
+/** recursively gets all questions from the given node */
+    public static List<JsonNode> getAllQuestions(JsonNode containerElement) {
+        List<JsonNode> elements = new ArrayList<>();
+
+        if(containerElement.has("elements")) {
+            for(JsonNode element : containerElement.get("elements")) {
+                elements.addAll(getAllQuestions(element));
+            }
+        } else elements.add(containerElement);
+
+        return elements;
+    }
+
+    public static SurveyQuestionDefinition unmarshalSurveyQuestion(Survey survey, JsonNode question, Map<String, JsonNode> questionTemplates) {
+
+        SurveyQuestionDefinition definition = SurveyQuestionDefinition.builder()
+                .surveyId(survey.getId())
+                .surveyStableId(survey.getStableId())
+                .surveyVersion(survey.getVersion())
+                .questionStableId(question.get("name").asText())
+                .build();
+
+        //The following fields may either be specified in the question itself,
+        //or as part of a question template. Resolve the remaining fields against
+        //the template (if applicable), so we have the full question definition.
+        JsonNode templatedQuestion = question.has("questionTemplateName") ?
+                questionTemplates.get(question.get("questionTemplateName").asText()) :
+                question;
+
+        definition.setQuestionType(templatedQuestion.get("type").asText());
+
+        //For normal elements, we'll store the title in the question_text column
+        //For HTML elements which don't have a title, we'll store the HTML instead
+        if(templatedQuestion.has("title")) {
+            definition.setQuestionText(templatedQuestion.get("title").asText());
+        } else if(templatedQuestion.has("html")) {
+            definition.setQuestionText(templatedQuestion.get("html").asText());
+        }
+
+        if(templatedQuestion.has("isRequired")){
+            definition.setRequired(templatedQuestion.get("isRequired").asBoolean());
+        }
+
+        if(templatedQuestion.has("choices")){
+            definition.setChoices(unmarshalSurveyQuestionChoices(templatedQuestion));
+        }
+
+        return definition;
+    }
+
+    public static String unmarshalSurveyQuestionChoices(JsonNode question) {
+        Map<String, String> choices = new HashMap<>();
+        for(JsonNode choice : question.get("choices")){
+            choices.put(choice.get("value").asText(), choice.get("text").asText());
+        }
+        ObjectMapper mapper = new ObjectMapper();
+
+        String result;
+        try {
+            result = mapper.writeValueAsString(choices);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Question contains malformed choices");
+        }
+
+        return result;
+    }
+
+}
