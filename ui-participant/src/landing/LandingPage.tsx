2f72f86897c1d021dd76e11f71f8d3a101befd49
Nick Watts <nwatts@broadinstitute.org>
2023-04-03 16:34:13 -0400

    Fix mailing list link in footer

--- ui-participant/src/landing/LandingPage.tsx
+++ ui-participant/src/landing/LandingPage.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useId } from 'react'
 import { Outlet } from 'react-router-dom'
 import LandingNavbar from './LandingNavbar'
 import { LocalSiteContent } from 'api/api'
+import { MailingListModal } from './MailingListModal'
 import { HtmlSectionView } from './sections/HtmlPageView'
 
 /** renders the landing page for a portal (e.g. hearthive.org) */
 function LandingPageView({ localContent }: { localContent: LocalSiteContent }) {
+  const mailingListModalId = useId()
+
+  useEffect(() => {
+    const mailingListLinks = document.querySelectorAll<HTMLLinkElement>('a[href="#mailing-list"]')
+    Array.from(mailingListLinks).forEach(el => {
+      el.dataset.bsToggle = 'modal'
+      el.dataset.bsTarget = `#${CSS.escape(mailingListModalId)}`
+    })
+  }, [])
+
   return <div className="LandingPage">
     <div className="container-fluid bg-white min-vh-100 d-flex flex-column p-0">
       <div>
@@ -17,6 +28,7 @@ function LandingPageView({ localContent }: { localContent: LocalSiteContent }) {
       {localContent.footerSection && <footer>
         <HtmlSectionView section={localContent.footerSection}/>
       </footer>}
+      <MailingListModal id={mailingListModalId} />
     </div>
   </div>
 }
