09097312a9da22d8b4cdb4eeb513e61c387e09c9
Devon Bush <devonbush@gmail.com>
2023-04-03 14:07:05 -0400

    updating timeutils signature

--- ui-admin/src/util/timeUtils.tsx
+++ ui-admin/src/util/timeUtils.tsx
@@ -1,5 +1,5 @@
 /** renders a java instant as a time string.  this should be sensitive to the user's locale */
-export function instantToDefaultString(instant: number) {
+export function instantToDefaultString(instant?: number) {
   if (!instant) {
     return ''
   }
