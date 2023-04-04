128a6dda0c7e46432075fa201f5b1f54cdb4a0ec
Devon <devonbush@gmail.com>
2023-04-04 13:12:34 -0400

    AR-249: adding real admin task list (#209)

---
+++ ui-admin/src/study/participants/tasks/ParticipantTaskView.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import { ParticipantTask, Enrollee } from 'api/api'
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable
+} from '@tanstack/react-table'
+import { sortableTableHeader } from 'util/tableUtils'
+import { instantToDefaultString } from 'util/timeUtils'
+
+const columns: ColumnDef<ParticipantTask>[] = [{
+  header: 'Task',
+  accessorKey: 'taskType'
+}, {
+  header: 'Item',
+  accessorKey: 'targetName'
+}, {
+  header: 'status',
+  accessorKey: 'status'
+}, {
+  header: 'asssigned',
+  accessorKey: 'createdAt',
+  cell: info => instantToDefaultString(info.getValue() as number)
+}, {
+  header: 'completed',
+  accessorKey: 'completedAt',
+  cell: info => instantToDefaultString(info.getValue() as number)
+}]
+
+/** show the task list in table */
+const ParticipantTaskView = ({ enrollee }: {enrollee: Enrollee}) => {
+  const [sorting, setSorting] = useState<SortingState>([{ id: 'createdAt', desc: true }])
+  const [rowSelection, setRowSelection] = React.useState<Record<string, boolean>>({})
+
+  const table = useReactTable({
+    data: enrollee.participantTasks,
+    columns,
+    state: {
+      sorting,
+      rowSelection
+    },
+    enableRowSelection: true,
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onRowSelectionChange: setRowSelection,
+    debugTable: true
+  })
+
+  return <div className="container p-3">
+    <h1 className="h4">Tasks </h1>
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          {table.getFlatHeaders().map(header => sortableTableHeader(header))}
+        </tr>
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map(row => {
+          return (
+            <tr key={row.id}>
+              {row.getVisibleCells().map(cell => {
+                return (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                )
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  </div>
+}
+
+export default ParticipantTaskView
+
--- ui-admin/src/util/timeUtils.tsx
+++ ui-admin/src/util/timeUtils.tsx
@@ -1,5 +1,8 @@
 /** renders a java instant as a time string.  this should be sensitive to the user's locale */
-export function instantToDefaultString(instant: number) {
+export function instantToDefaultString(instant?: number) {
+  if (!instant) {
+    return ''
+  }
   return new Date(instant * 1000).toLocaleString()
 }
 /** renders a java LocalDate as a date string.  this should be sensitive to the user's locale */
