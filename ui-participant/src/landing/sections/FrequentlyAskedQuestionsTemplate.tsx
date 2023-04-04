55efe6d5e8743687b3c4bdbb203932b6f8c080f2
Nick Watts <nwatts@broadinstitute.org>
2023-04-04 14:24:33 -0400

    Add expand/collapse all button to FAQ

--- ui-participant/src/landing/sections/FrequentlyAskedQuestionsTemplate.tsx
+++ ui-participant/src/landing/sections/FrequentlyAskedQuestionsTemplate.tsx
@@ -1,9 +1,10 @@
 import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus'
 import { faXmark } from '@fortawesome/free-solid-svg-icons/faXmark'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Collapse } from 'bootstrap'
 import classNames from 'classnames'
 import _ from 'lodash'
-import React from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 
 import { SectionConfig } from 'api/api'
@@ -58,6 +59,52 @@ const validateFrequentlyAskedQuestionsConfig = (config: SectionConfig): Frequent
   }
 }
 
+type FrequentlyAskedQuestionProps = {
+  question: string
+  answer: string
+  onToggle: (isExpanded: boolean) => void
+}
+
+const FrequentlyAskedQuestion = (props: FrequentlyAskedQuestionProps) => {
+  const { question, answer, onToggle } = props
+
+  const collapseRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    collapseRef.current?.addEventListener('show.bs.collapse', () => { onToggle(true) })
+    collapseRef.current?.addEventListener('hide.bs.collapse', () => { onToggle(false) })
+  }, [onToggle])
+
+  return (
+    <>
+      <button
+        aria-controls={targetFor(question)}
+        aria-expanded="false"
+        className={classNames(
+          'btn btn-link btn-lg',
+          'w-100 py-3 px-0 px-sm-2',
+          'd-flex',
+          'text-black fw-bold text-start text-decoration-none'
+        )}
+        data-bs-target={targetFor(question)}
+        data-bs-toggle="collapse"
+      >
+        <span className="me-2 text-center" style={{ width: 20 }}>
+          <FontAwesomeIcon className="hidden-when-expanded" icon={faPlus} />
+          <FontAwesomeIcon className="hidden-when-collapsed" icon={faXmark} />
+        </span>
+        {question}
+      </button>
+      <div ref={collapseRef} className="collapse px-0 px-sm-2 ms-2" id={idFor(question)}>
+        <div className="fs-5" style={{ marginLeft: 20 }}>
+          <ReactMarkdown>
+            {answer}
+          </ReactMarkdown>
+        </div>
+      </div>
+    </>
+  )
+}
+
 type FrequentlyAskedQuestionsProps = TemplateComponentProps<FrequentlyAskedQuestionsConfig>
 
 /**
@@ -71,45 +118,56 @@ function FrequentlyAskedQuestionsTemplate(props: FrequentlyAskedQuestionsProps) 
     title = 'Frequently Asked Questions'
   } = config
 
-  return <div id={anchorRef} className="row mx-0 justify-content-center" style={getSectionStyle(config)}>
+  const [numExpandedQuestions, setNumExpandedQuestions] = useState(0)
+  const allQuestionsAreExpanded = numExpandedQuestions === questions.length
+
+  const onToggleQuestion = useCallback((isExpanded: boolean) => {
+    setNumExpandedQuestions(isExpanded ? n => n + 1 : n => n - 1)
+  }, [])
+
+  const questionsListRef = useRef<HTMLUListElement>(null)
+
+  const expandAll = useCallback(() => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const collapses = Array.from(questionsListRef.current!.querySelectorAll('.collapse'))
+    collapses.forEach(el => { Collapse.getOrCreateInstance(el).show() })
+  }, [])
+
+  const collapseAll = useCallback(() => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const collapses = Array.from(questionsListRef.current!.querySelectorAll('.collapse'))
+    collapses.forEach(el => { Collapse.getOrCreateInstance(el).hide() })
+  }, [])
+
+  return <div id={anchorRef} className="row mx-0 py-5 justify-content-center" style={getSectionStyle(config)}>
     <div className="col-12 col-sm-8 col-lg-6">
       {!!title && (
-        <h2 className="fs-1 fw-normal lh-sm mt-5 mb-4 text-center">{title}</h2>
+        <h2 className="fs-1 fw-normal lh-sm mb-4 text-center">{title}</h2>
       )}
       {!!blurb && (
         <div className="fs-4 mb-4 text-center">
           {blurb && <ReactMarkdown>{blurb}</ReactMarkdown>}
         </div>
       )}
-      <ul className="mx-0 px-0 border-top" style={{ listStyle: 'none' }}>
+      <div className="mb-4">
+        <button
+          className="btn btn-outline-dark btn-rounded text-uppercase"
+          onClick={allQuestionsAreExpanded ? collapseAll : expandAll}
+        >
+          <span className="d-inline-block me-2 text-center" style={{ width: 20 }}>
+            <FontAwesomeIcon icon={allQuestionsAreExpanded ? faXmark : faPlus} />
+          </span>
+          {allQuestionsAreExpanded ? 'Collapse' : 'Expand'} all
+        </button>
+      </div>
+      <ul ref={questionsListRef} className="mx-0 px-0 border-top" style={{ listStyle: 'none' }}>
         {
           questions.map(({ question, answer }, i) => {
-            return <li key={i} className="border-bottom">
-              <button
-                type="button"
-                className={classNames(
-                  'btn btn-link btn-lg',
-                  'w-100 py-3 px-0 px-sm-2',
-                  'd-flex',
-                  'text-black fw-bold text-start text-decoration-none'
-                )}
-                data-bs-toggle="collapse" data-bs-target={targetFor(question)}
-                aria-expanded="false" aria-controls={targetFor(question)}
-              >
-                <span className="me-2 text-center" style={{ width: 20 }}>
-                  <FontAwesomeIcon className="hidden-when-expanded" icon={faPlus} />
-                  <FontAwesomeIcon className="hidden-when-collapsed" icon={faXmark} />
-                </span>
-                {question}
-              </button>
-              <div className="collapse px-0 px-sm-2 ms-2" id={idFor(question)}>
-                <div className="fs-5" style={{ marginLeft: 20 }}>
-                  <ReactMarkdown>
-                    {answer}
-                  </ReactMarkdown>
-                </div>
-              </div>
-            </li>
+            return (
+              <li key={i} className="border-bottom">
+                <FrequentlyAskedQuestion question={question} answer={answer} onToggle={onToggleQuestion} />
+              </li>
+            )
           })
         }
       </ul>
