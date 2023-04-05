736aacbd2661ef1154aba21b593039cd713e67d3
Devon Bush <devonbush@gmail.com>
2023-04-05 15:02:46 -0400

    updating auth util service dependencies

--- api-admin/src/main/java/bio/terra/pearl/api/admin/service/CurrentUnauthedUserService.java
+++ api-admin/src/main/java/bio/terra/pearl/api/admin/service/CurrentUnauthedUserService.java
@@ -13,54 +13,53 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CurrentUnauthedUserService {
-    private AdminUserDao adminUserDao;
+  private AdminUserDao adminUserDao;
 
-    public CurrentUnauthedUserService(AdminUserDao adminUserDao) {
+  public CurrentUnauthedUserService(AdminUserDao adminUserDao) {
     this.adminUserDao = adminUserDao;
-    }
+  }
 
-    @Transactional
-    public Optional<AdminUserWithPermissions> unauthedLogin(String username) {
-        Optional<AdminUserWithPermissions> userOpt =
-            adminUserDao.findByUsernameWithPermissions(username);
-        userOpt.ifPresent(
-            userWithPermissions -> {
-              AdminUser user = userWithPermissions.user();
-              user.setToken(generateFakeJwtToken(username));
-              user.setLastLogin(Instant.now());
-              adminUserDao.update(user);
-            });
-        return userOpt;
-    }
+  @Transactional
+  public Optional<AdminUserWithPermissions> unauthedLogin(String username) {
+    Optional<AdminUserWithPermissions> userOpt =
+        adminUserDao.findByUsernameWithPermissions(username);
+    userOpt.ifPresent(
+        userWithPermissions -> {
+          AdminUser user = userWithPermissions.user();
+          user.setToken(generateFakeJwtToken(username));
+          user.setLastLogin(Instant.now());
+          adminUserDao.update(user);
+        });
+    return userOpt;
+  }
 
-    protected String generateFakeJwtToken(String username) {
-        UUID token = UUID.randomUUID();
-        return JWT.create()
-            .withClaim("token", token.toString())
-            .withClaim("email", username)
-            .sign(Algorithm.none());
-    }
+  protected String generateFakeJwtToken(String username) {
+    UUID token = UUID.randomUUID();
+    return JWT.create()
+        .withClaim("token", token.toString())
+        .withClaim("email", username)
+        .sign(Algorithm.none());
+  }
 
-    @Transactional
-    public Optional<AdminUserWithPermissions> tokenLogin(String token) {
-        var email = getEmailFromToken(token);
-        return adminUserDao.findByUsernameWithPermissions(email);
-    }
+  @Transactional
+  public Optional<AdminUserWithPermissions> tokenLogin(String token) {
+    var email = getEmailFromToken(token);
+    return adminUserDao.findByUsernameWithPermissions(email);
+  }
 
-    @Transactional
-    public void logout(String token) {
-        var email = getEmailFromToken(token);
-        Optional<AdminUser> userOpt = adminUserDao.findByUsername(email);
-        userOpt.ifPresent(
+  @Transactional
+  public void logout(String token) {
+    var email = getEmailFromToken(token);
+    Optional<AdminUser> userOpt = adminUserDao.findByUsername(email);
+    userOpt.ifPresent(
         user -> {
           user.setToken(null);
           adminUserDao.update(user);
         });
-    }
+  }
 
-    protected String getEmailFromToken(String token) {
-        var decodedJWT = JWT.decode(token);
-        return decodedJWT.getClaim("email").asString();
-    }
-
+  protected String getEmailFromToken(String token) {
+    var decodedJWT = JWT.decode(token);
+    return decodedJWT.getClaim("email").asString();
+  }
 }
