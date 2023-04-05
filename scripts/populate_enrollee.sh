201a63d6c60de2c0dbe05cdbcd6d7cd063c0ebe7
Devon <devonbush@gmail.com>
2023-04-05 14:59:38 -0400

    JN-204 safeguarding populate service (#219)

--- scripts/populate_enrollee.sh
+++ scripts/populate_enrollee.sh
@@ -3,8 +3,9 @@
 # populate_enrollee.sh [portalShortcode] [envName] [studyShortcode] [filePath from populate/src/main/resources/seed]
 # e.g. populate_enrollee.sh ourhealth sandbox ourheart portals/ourhealth/studies/ourheart/enrollees/jsalk.json
 SERVER_NAME="localhost:8080"
-set -x
+set -u
 
-curl -X POST "$SERVER_NAME/api/internal/v1/populate/enrollee/$1/env/$2/study/$3?filePathName=$4"
+ACCESS_TOKEN=$(az account get-access-token | jq -r .accessToken)
+curl -X POST -H "Authorization: Bearer $ACCESS_TOKEN" "$SERVER_NAME/api/internal/v1/populate/enrollee/$1/env/$2/study/$3?filePathName=$4"
 
 echo ""
--- scripts/populate_portal.sh
+++ scripts/populate_portal.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 SERVER_NAME="localhost:8080"
-set -x
 set -u
 
-curl -X POST "$SERVER_NAME/api/internal/v1/populate/portal?filePathName=portals/$1/portal.json"
+ACCESS_TOKEN=$(az account get-access-token | jq -r .accessToken)
+curl -X POST -H "Authorization: Bearer $ACCESS_TOKEN" "$SERVER_NAME/api/internal/v1/populate/portal?filePathName=portals/$1/portal.json"
 
 echo ""
