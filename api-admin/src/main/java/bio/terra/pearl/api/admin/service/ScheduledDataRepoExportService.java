356b04462afed2db51b348ce0b6f2e4b0183d1b3
Matt Bemis <mbemis@broadinstitute.org>
2023-04-10 15:37:29 -0400

    add isTdrConfigured

--- api-admin/src/main/java/bio/terra/pearl/api/admin/service/ScheduledDataRepoExportService.java
+++ api-admin/src/main/java/bio/terra/pearl/api/admin/service/ScheduledDataRepoExportService.java
@@ -3,8 +3,10 @@ package bio.terra.pearl.api.admin.service;
 import bio.terra.pearl.core.service.datarepo.DataRepoExportService;
 import java.util.concurrent.TimeUnit;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -12,14 +14,22 @@ import org.springframework.stereotype.Service;
 public class ScheduledDataRepoExportService {
   private static final Logger logger = LoggerFactory.getLogger(DataRepoExportService.class);
   private DataRepoExportService dataRepoExportService;
+  private Environment env;
 
-  public ScheduledDataRepoExportService(DataRepoExportService dataRepoExportService) {
+  public ScheduledDataRepoExportService(
+      Environment env, DataRepoExportService dataRepoExportService) {
+    this.env = env;
     this.dataRepoExportService = dataRepoExportService;
   }
 
   @Scheduled(timeUnit = TimeUnit.MINUTES, fixedDelay = 10, initialDelay = 0)
   public void pingDataRepoStatus() {
-    logger.info("Pinging Terra Data Repo. Up: " + dataRepoExportService.getServiceStatus());
+    if (isTdrConfigured()) {
+      logger.info("Pinging Terra Data Repo. Up: " + dataRepoExportService.getServiceStatus());
+    } else {
+      logger.error(
+          "Error: Skipping TDR status ping, as TDR has not been configured for this environment.");
+    }
   }
 
   /* Create any missing datasets every hour or so. There is one dataset per study
@@ -35,8 +45,13 @@ public class ScheduledDataRepoExportService {
       lockAtMostFor = "10m",
       lockAtLeastFor = "5m")
   public void initializeStudyEnvironmentDatasets() {
-    logger.info("Creating datasets...");
-    dataRepoExportService.createDatasetsForStudyEnvironments();
+    if (isTdrConfigured()) {
+      logger.info("Creating datasets...");
+      dataRepoExportService.createDatasetsForStudyEnvironments();
+    } else {
+      logger.error(
+          "Error: Skipping TDR dataset creation, as TDR has not been configured for this environment.");
+    }
   }
 
   @Scheduled(timeUnit = TimeUnit.MINUTES, fixedDelay = 10, initialDelay = 0)
@@ -45,7 +60,17 @@ public class ScheduledDataRepoExportService {
       lockAtMostFor = "5m",
       lockAtLeastFor = "2m")
   public void pollRunningInitializeJobs() {
-    logger.info("Polling running TDR dataset creation jobs...");
-    dataRepoExportService.pollRunningCreateDatasetJobs();
+    if (isTdrConfigured()) {
+      logger.info("Polling running TDR dataset creation jobs...");
+      dataRepoExportService.pollRunningCreateDatasetJobs();
+    } else {
+      logger.error(
+          "Error: Skipping TDR job polling, as TDR has not been configured for this environment.");
+    }
+  }
+
+  public boolean isTdrConfigured() {
+    String envProp = env.getProperty("env.tdr.serviceAccountCreds");
+    return !StringUtils.isBlank(envProp) && !envProp.equals("missing_tdr_sa_creds");
   }
 }
