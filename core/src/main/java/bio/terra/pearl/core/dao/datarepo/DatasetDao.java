24a5737a575c9a114fab04c2a9cd301d78e102ff
Devon Bush <devonbush@gmail.com>
2023-04-13 20:29:06 -0400

    updating service method

--- core/src/main/java/bio/terra/pearl/core/dao/datarepo/DatasetDao.java
+++ core/src/main/java/bio/terra/pearl/core/dao/datarepo/DatasetDao.java
@@ -2,6 +2,7 @@ package bio.terra.pearl.core.dao.datarepo;
 
 import bio.terra.pearl.core.dao.BaseMutableJdbiDao;
 import bio.terra.pearl.core.model.datarepo.Dataset;
+import java.util.List;
 import java.util.UUID;
 import org.jdbi.v3.core.Jdbi;
 import org.springframework.stereotype.Component;
@@ -17,7 +18,7 @@ public class DatasetDao extends BaseMutableJdbiDao<Dataset> {
         return Dataset.class;
     }
 
-    public void deleteByStudyEnvironmentId(UUID studyEnvId) {
-        deleteByProperty("study_environment_id", studyEnvId);
+    public List<Dataset> findByStudyEnvironmentId(UUID studyEnvId) {
+        return findAllByProperty("study_environment_id", studyEnvId);
     }
 }
--- core/src/main/java/bio/terra/pearl/core/service/datarepo/DatasetService.java
+++ core/src/main/java/bio/terra/pearl/core/service/datarepo/DatasetService.java
@@ -2,7 +2,10 @@ package bio.terra.pearl.core.service.datarepo;
 
 import bio.terra.pearl.core.dao.datarepo.DatasetDao;
 import bio.terra.pearl.core.model.datarepo.Dataset;
+import bio.terra.pearl.core.service.CascadeProperty;
 import bio.terra.pearl.core.service.CrudService;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +16,16 @@ public class DatasetService extends CrudService<Dataset, DatasetDao> {
         super(dao);
     }
 
-    public void deleteByStudyEnvironmentId(UUID studyEnvId) {
+    @Override
+    public void delete(UUID id, Set<CascadeProperty> cascades) {
         // TODO clean up the TDR dataset itself
-        dao.deleteByStudyEnvironmentId(studyEnvId);
+        dao.delete(id);
+    }
+
+    public void deleteByStudyEnvironmentId(UUID studyEnvId) {
+        List<Dataset> datasets = dao.findByStudyEnvironmentId(studyEnvId);
+        for (Dataset dataset : datasets) {
+            delete(dataset.getId(), CascadeProperty.EMPTY_SET);
+        }
     }
 }
