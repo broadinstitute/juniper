d119b72a644681c7d035034d1e889e5c5b28c84e
Devon Bush <devonbush@gmail.com>
2023-04-19 00:52:59 -0400

    fixing timer typo

--- ui-participant/src/hub/survey/SurveyView.tsx
+++ ui-participant/src/hub/survey/SurveyView.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useRef, useState} from 'react'
-import {useNavigate, useParams, useSearchParams} from 'react-router-dom'
+import React, { useEffect, useRef, useState } from 'react'
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom'
 
 import Api, {
   ConsentForm,
@@ -11,7 +11,7 @@ import Api, {
   SurveyWithResponse
 } from 'api/api'
 
-import {Survey as SurveyComponent} from 'survey-react-ui'
+import { Survey as SurveyComponent } from 'survey-react-ui'
 import {
   getResumeData,
   getSurveyJsAnswerList,
@@ -21,25 +21,25 @@ import {
   useRoutablePageNumber,
   useSurveyJSModel
 } from 'util/surveyJsUtils'
-import {HubUpdate} from 'hub/hubUpdates'
-import {usePortalEnv} from 'providers/PortalProvider'
-import {useUser} from 'providers/UserProvider'
-import {PageLoadingIndicator} from 'util/LoadingSpinner'
-import {withErrorBoundary} from '../../util/ErrorBoundary'
+import { HubUpdate } from 'hub/hubUpdates'
+import { usePortalEnv } from 'providers/PortalProvider'
+import { useUser } from 'providers/UserProvider'
+import { PageLoadingIndicator } from 'util/LoadingSpinner'
+import { withErrorBoundary } from '../../util/ErrorBoundary'
 
 const TASK_ID_PARAM = 'taskId'
-const AUTO_SAVE_INTERVAL = 10 * 7000  // auto-save every 7 seconds
+const AUTO_SAVE_INTERVAL = 3 * 1000  // auto-save every 3 seconds if there are changes
 
 /**
  * display a single survey form to a participant.
  */
-function RawSurveyView({form, enrollee, resumableData, pager, studyShortcode, taskId, activeResponse}:
+function RawSurveyView({ form, enrollee, resumableData, pager, studyShortcode, taskId, activeResponse }:
                          {
                            form: ConsentForm, enrollee: Enrollee, taskId: string, activeResponse?: SurveyResponse,
                            resumableData: SurveyJsResumeData | null, pager: PageNumberControl, studyShortcode: string
                          }) {
   const navigate = useNavigate()
-  const {updateEnrollee} = useUser()
+  const { updateEnrollee } = useUser()
   const prevSave = useRef(resumableData?.data ?? {})
 
   /** Submit the response to the server */
@@ -71,14 +71,14 @@ function RawSurveyView({form, enrollee, resumableData, pager, studyShortcode, ta
           type: 'success'
         }
       }
-      navigate('/hub', {state: hubUpdate})
+      navigate('/hub', { state: hubUpdate })
     }).catch(() => {
       refreshSurvey(surveyModel, null)
       alert('an error occurred')
     })
   }
 
-  const {surveyModel, refreshSurvey, setSurveyModel} = useSurveyJSModel(form, resumableData,
+  const { surveyModel, refreshSurvey, setSurveyModel } = useSurveyJSModel(form, resumableData,
     onComplete, pager, enrollee.profile)
 
   const saveDiff = () => {
@@ -131,7 +131,7 @@ function RawSurveyView({form, enrollee, resumableData, pager, studyShortcode, ta
 }
 
 /** handles paging the form */
-function PagedSurveyView({form, activeResponse, enrollee, studyShortcode, taskId}:
+function PagedSurveyView({ form, activeResponse, enrollee, studyShortcode, taskId }:
                            {
                              form: StudyEnvironmentSurvey, activeResponse?: SurveyResponse, enrollee: Enrollee,
                              studyShortcode: string, taskId: string
@@ -142,13 +142,13 @@ function PagedSurveyView({form, activeResponse, enrollee, studyShortcode, taskId
   const pager = useRoutablePageNumber()
 
   return <RawSurveyView enrollee={enrollee} form={form.survey} taskId={taskId} activeResponse={activeResponse}
-                        resumableData={resumableData} pager={pager} studyShortcode={studyShortcode}/>
+    resumableData={resumableData} pager={pager} studyShortcode={studyShortcode}/>
 }
 
 /** handles loading the survey form and responses from the server */
 function SurveyView() {
-  const {portal} = usePortalEnv()
-  const {enrollees} = useUser()
+  const { portal } = usePortalEnv()
+  const { enrollees } = useUser()
   const [formAndResponses, setFormAndResponse] = useState<SurveyWithResponse | null>(null)
   const params = useParams()
   const stableId = params.stableId
@@ -171,8 +171,8 @@ function SurveyView() {
       .then(response => {
         setFormAndResponse(response)
       }).catch(() => {
-      alert('error loading survey form - please retry')
-    })
+        alert('error loading survey form - please retry')
+      })
   }, [])
 
   if (!formAndResponses) {
