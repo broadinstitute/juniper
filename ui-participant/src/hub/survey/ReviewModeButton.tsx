60553b7cde207a7ed21d74dba41d18d7fb037c23
Devon Bush <devonbush@gmail.com>
2023-04-21 14:39:55 -0400

    linting

--- ui-participant/src/hub/survey/ReviewModeButton.tsx
+++ ui-participant/src/hub/survey/ReviewModeButton.tsx
@@ -1,17 +1,18 @@
-import React, {useState} from 'react'
-import {SurveyModel} from "survey-core";
-import {getEnvSpec} from "api/api";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faToggleOff, faToggleOn} from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from 'react'
+import { SurveyModel } from 'survey-core'
+import { getEnvSpec } from 'api/api'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faToggleOff, faToggleOn } from '@fortawesome/free-solid-svg-icons'
 
-export default function SurveyReviewModeButton({surveyModel}: { surveyModel: SurveyModel | null }) {
+/** button for facilitating quick review of survey contents without validation or visibility constraints */
+export default function SurveyReviewModeButton({ surveyModel }: { surveyModel: SurveyModel | null }) {
   const [isReviewMode, setIsReviewMode] = useState(false)
-  const {envName} = getEnvSpec()
+  const { envName } = getEnvSpec()
 
   const toggleSurveyVisibilityMode = () => {
     if (surveyModel) {
       surveyModel.showInvisibleElements = !surveyModel.showInvisibleElements
-      surveyModel.checkErrorsMode = surveyModel.showInvisibleElements ? "onComplete" : "onNextPage"
+      surveyModel.checkErrorsMode = surveyModel.showInvisibleElements ? 'onComplete' : 'onNextPage'
       setIsReviewMode(!isReviewMode)
     }
   }
@@ -19,9 +20,9 @@ export default function SurveyReviewModeButton({surveyModel}: { surveyModel: Sur
     return null
   }
   return <button className="float-end btn" aria-label="toggle show all items"
-                 title="toggle showing all items, regardless of visibility rules.
+    title="toggle showing all items, regardless of visibility rules.
                                    (Available only in sandbox & irb environments)"
-                 onClick={toggleSurveyVisibilityMode}>
+    onClick={toggleSurveyVisibilityMode}>
     Review mode: <FontAwesomeIcon icon={isReviewMode ? faToggleOn : faToggleOff} title={isReviewMode ? 'on' : 'off'}/>
   </button>
 }
