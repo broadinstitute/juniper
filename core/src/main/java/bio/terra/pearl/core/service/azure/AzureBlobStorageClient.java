d2295ded5e001716a8ad2a6865f10a1462656280
Matt Bemis <mbemis@broadinstitute.org>
2023-05-01 15:03:10 -0400

    cleanup

--- core/src/main/java/bio/terra/pearl/core/service/azure/AzureBlobStorageClient.java
+++ core/src/main/java/bio/terra/pearl/core/service/azure/AzureBlobStorageClient.java
@@ -1,8 +1,6 @@
 package bio.terra.pearl.core.service.azure;
 
 import com.azure.storage.blob.*;
-import com.azure.storage.blob.models.BlockBlobItem;
-import com.azure.storage.blob.sas.BlobContainerSasPermission;
 import com.azure.storage.blob.sas.BlobSasPermission;
 import com.azure.storage.blob.sas.BlobServiceSasSignatureValues;
 import com.azure.storage.blob.specialized.BlockBlobClient;
@@ -13,7 +11,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -31,52 +28,37 @@ public class AzureBlobStorageClient {
     }
 
     public String uploadBlob(String fileName, String data) throws IOException {
-
-        /*
-         * From the Azure portal, get your Storage account's name and account key.
-         */
         String accountName = env.getProperty("env.tdr.storageAccountName");
         String accountKey = env.getProperty("env.tdr.storageAccountKey");
         String containerName = env.getProperty("env.tdr.storageContainerName");
 
-        /*
-         * Use your Storage account's name and key to create a credential object; this is used to access your account.
-         */
+        //Get a credential object to access the storage container
         StorageSharedKeyCredential credential = new StorageSharedKeyCredential(accountName, accountKey);
 
-        String endpoint = String.format(Locale.ROOT, "https://%s.blob.core.windows.net", accountName);
+        String storageClientEndpoint = String.format(Locale.ROOT, "https://%s.blob.core.windows.net", accountName);
 
-        /*
-         * Create a BlobServiceClient object that wraps the service endpoint, credential and a request pipeline.
-         */
-        BlobServiceClient storageClient = new BlobServiceClientBuilder().endpoint(endpoint).credential(credential).buildClient();
+        //Create a BlobServiceClient object that wraps the service endpoint, credential and a request pipeline.
+        BlobServiceClient storageClient = new BlobServiceClientBuilder().endpoint(storageClientEndpoint).credential(credential).buildClient();
 
-        /*
-         * Create a client that references a to-be-created container in your Azure Storage account. This returns a
-         * ContainerClient object that wraps the container's endpoint, credential and a request pipeline (inherited from storageClient).
-         * Note that container names require lowercase.
-         */
+        //Create a client that references the storage container
         BlobContainerClient blobContainerClient = storageClient.getBlobContainerClient(containerName);
 
-        /*
-         * Create a client that references a to-be-created blob in your Azure Storage account's container.
-         * This returns a BlockBlobClient object that wraps the blob's endpoint, credential and a request pipeline
-         * (inherited from containerClient). Note that blob names can be mixed case.
-         */
+        //Create a client that references the to-be-created blob in the storage container
         BlockBlobClient blobClient = blobContainerClient.getBlobClient(fileName).getBlockBlobClient();
 
         InputStream dataStream = new ByteArrayInputStream(data.getBytes(StandardCharsets.UTF_8));
 
-        /*
-         * Create the blob with string (plain text) content.
-         */
+        //Create the blob
         blobClient.upload(dataStream, data.length());
 
         dataStream.close();
 
-        //Return SAS-signed URL for the uploaded TSV
+        //Generate a SAS-signed URL that is good for 1 hour (ingest should be much quicker than this)
         BlobSasPermission blobSasPermission = new BlobSasPermission().setReadPermission(true);
-        BlobServiceSasSignatureValues builder = new BlobServiceSasSignatureValues(OffsetDateTime.now().plusDays(1), blobSasPermission).setProtocol(SasProtocol.HTTPS_ONLY);
-        return String.format("https://%s.blob.core.windows.net/%s/%s?%s",blobClient.getAccountName(), blobClient.getContainerName(), blobClient.getBlobName(), blobClient.generateSas(builder));
+        BlobServiceSasSignatureValues builder = new BlobServiceSasSignatureValues(OffsetDateTime.now().plusHours(1), blobSasPermission).setProtocol(SasProtocol.HTTPS_ONLY);
+
+        //Return SAS-signed URL for the uploaded TSV
+        String sasUrl = String.format("https://%s.blob.core.windows.net/%s/%s?%s",blobClient.getAccountName(), blobClient.getContainerName(), blobClient.getBlobName(), blobClient.generateSas(builder));
+        return sasUrl;
     }
 }
