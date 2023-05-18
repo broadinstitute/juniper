202347a37d81bb40fb71a0d59f565a1cc9aad05a
Nick Watts <1156625+nawatts@users.noreply.github.com>
2023-05-18 14:36:08 -0400

    [JN-382] Move types for landing page config to ui-core (#384)

--- ui-core/src/index.ts
+++ ui-core/src/index.ts
@@ -1,3 +1,5 @@
 export { InvestigatorTermsOfUse } from './terms/InvestigatorTermsOfUse'
 export { ParticipantTermsOfUse } from './terms/ParticipantTermsOfUse'
 export { PrivacyPolicy } from './terms/PrivacyPolicy'
+
+export * from './types/landingPageConfig'
