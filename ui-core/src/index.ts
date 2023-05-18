e62784902d7b3e654d44ab4dfded370162d4db40
Nick Watts <nwatts@broadinstitute.org>
2023-05-18 15:50:30 -0400

    Rename file

--- ui-core/src/index.ts
+++ ui-core/src/index.ts
@@ -2,5 +2,5 @@ export { InvestigatorTermsOfUse } from './terms/InvestigatorTermsOfUse'
 export { ParticipantTermsOfUse } from './terms/ParticipantTermsOfUse'
 export { PrivacyPolicy } from './terms/PrivacyPolicy'
 
+export * from './types/forms'
 export * from './types/landingPageConfig'
-export * from './types/surveys'
