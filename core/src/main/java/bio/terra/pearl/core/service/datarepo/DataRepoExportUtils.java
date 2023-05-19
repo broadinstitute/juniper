0af8d5d2e5297874daf7d0bfd915b7eb7d46e22c
Matt Bemis <mbemis@broadinstitute.org>
2023-05-19 10:55:19 -0400

    add comment

--- core/src/main/java/bio/terra/pearl/core/service/datarepo/DataRepoExportUtils.java
+++ core/src/main/java/bio/terra/pearl/core/service/datarepo/DataRepoExportUtils.java
@@ -17,6 +17,9 @@ public class DataRepoExportUtils {
     }
 
     //Converts a Juniper column name into a TDR-accepted column name
+    //Note that if two columns have the same initial 64 characters, this will cause
+    //TDR to reject the schema creation request with a validation error. See
+    //discussion here: https://github.com/broadinstitute/pearl/pull/376/files#r1198272411
     public static String juniperToDataRepoColumnName(String columnName) {
         return columnName
                 .substring(0, Math.min(columnName.length(), 63)) //TDR accepts a maximum column name length of 64 chars
