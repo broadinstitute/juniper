d0a2506dafa52cde0289af5a33fcd255c171ef7c
Nick Watts <1156625+nawatts@users.noreply.github.com>
2023-06-29 08:35:52 -0400

    [JN-378] Update HTML designer component (#455)

--- ui-admin/src/forms/designer/HtmlDesigner.tsx
+++ ui-admin/src/forms/designer/HtmlDesigner.tsx
@@ -2,33 +2,37 @@ import React from 'react'
 
 import { HtmlElement } from '@juniper/ui-core'
 
+import { Textarea } from 'components/forms/Textarea'
+
 export type HtmlDesignerProps = {
+  element: HtmlElement
   readOnly: boolean
-  value: HtmlElement
   onChange: (newValue: HtmlElement) => void
 }
 
 /** UI for editing an HTML element in a form. */
 export const HtmlDesigner = (props: HtmlDesignerProps) => {
-  const { readOnly, value, onChange } = props
+  const { element, readOnly, onChange } = props
 
   return (
     <div className="d-flex flex-column h-100">
-      <h2>{value.name}</h2>
-      <textarea
+      <h2>{element.name}</h2>
+      <label className="form-label fs-4 mb-0" htmlFor="html-element-html">HTML</label>
+      <Textarea
         className="w-100 flex-grow-1 font-monospace"
-        readOnly={readOnly}
+        disabled={readOnly}
+        id="html-element-html"
         style={{
           overflowX: 'auto',
           resize: 'none',
           // @ts-ignore TS thinks this isn't a valid style property
           textWrap: 'nowrap'
         }}
-        value={value.html}
-        onChange={e => {
+        value={element.html}
+        onChange={newHtml => {
           onChange({
-            ...value,
-            html: e.target.value
+            ...element,
+            html: newHtml
           })
         }}
       />
