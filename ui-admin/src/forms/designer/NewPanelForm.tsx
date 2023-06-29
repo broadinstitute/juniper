b5487f770454edda1ade2bb5a31da923e1340782
Nick Watts <1156625+nawatts@users.noreply.github.com>
2023-06-29 14:35:49 -0400

    [JN-378] Allow creating panels from existing questions (#462)

---
+++ ui-admin/src/forms/designer/NewPanelForm.tsx
@@ -0,0 +1,64 @@
+import { concat, flow, uniq, without } from 'lodash/fp'
+import React, { useState } from 'react'
+
+import { FormPanel } from '@juniper/ui-core'
+
+import { Button } from 'components/forms/Button'
+import { Checkbox } from 'components/forms/Checkbox'
+
+type NewPanelFormProps = {
+  availableElements: FormPanel['elements']
+  onCreate: (newPanel: FormPanel) => void
+}
+
+/** UI for creating a new panel. */
+export const NewPanelForm = (props: NewPanelFormProps) => {
+  const { availableElements, onCreate } = props
+
+  const [selectedElements, setSelectedElements] = useState<string[]>([])
+
+  return (
+    <>
+      <fieldset className="pb-3 border-bottom mb-3">
+        <legend className="form-label fs-6 mb-2">Select elements to include</legend>
+
+        {availableElements.map(element => {
+          return (
+            <div key={element.name} className="mb-2">
+              <Checkbox
+                checked={selectedElements.includes(element.name)}
+                label={element.name}
+                onChange={checked => {
+                  if (checked) {
+                    setSelectedElements(flow(concat([element.name]), uniq))
+                  } else {
+                    setSelectedElements(without([element.name]))
+                  }
+                }}
+              />
+            </div>
+          )
+        })}
+      </fieldset>
+      <div className="text-align-right">
+        <Button
+          disabled={selectedElements.length === 0}
+          tooltip={
+            selectedElements.length === 0
+              ? 'Select questions to create a panel'
+              : undefined
+          }
+          variant="primary"
+          onClick={() => {
+            onCreate({
+              type: 'panel',
+              elements: availableElements.filter(element => selectedElements.includes(element.name))
+            })
+          }}
+        >
+          Create panel
+        </Button>
+      </div>
+    </>
+  )
+}
---
+++ ui-admin/src/forms/designer/PageDesigner.test.tsx
@@ -0,0 +1,87 @@
+import { act, getAllByRole, getByLabelText, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+
+import { FormContentPage } from '@juniper/ui-core'
+
+import { PageDesigner } from './PageDesigner'
+
+describe('PageDesigner', () => {
+  describe('creating a panel', () => {
+    const page: FormContentPage = {
+      elements: [
+        { name: 'q1', title: '1?', type: 'text' },
+        { name: 'q2', title: '2?', type: 'text' },
+        { name: 'q3', type: 'html', html: '3' },
+        {
+          type: 'panel',
+          elements: [
+            { name: 'q4', title: '4?', type: 'text' },
+            { name: 'q5', title: '5?', type: 'text' }
+          ]
+        },
+        { name: 'q6', title: '6?', type: 'text' }
+      ]
+    }
+
+    it('shows a modal with questions available to include in panel', async () => {
+      // Arrange
+      const user = userEvent.setup()
+
+      render(<PageDesigner readOnly={false} value={page} onChange={jest.fn()} />)
+
+      const createPanelButton = screen.getByText('Add panel')
+
+      // Act
+      await act(() => user.click(createPanelButton))
+
+      // Assert
+      const elementsToIncludeFieldset = screen.getByRole('group', { name: 'Select elements to include' })
+      // Should only include questions / HTML elements, not panels
+      const elementCheckboxes = getAllByRole(elementsToIncludeFieldset, 'checkbox')
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const elementNames = elementCheckboxes.map(el => (el as HTMLInputElement).labels![0].textContent)
+      expect(elementNames).toEqual(['q1', 'q2', 'q3', 'q6'])
+    })
+
+    it('updates page with new panel', async () => {
+      // Arrange
+      const user = userEvent.setup()
+
+      const onChange = jest.fn()
+      render(<PageDesigner readOnly={false} value={page} onChange={onChange} />)
+
+      await act(() => user.click(screen.getByText('Add panel')))
+
+      // Act
+      const elementsToIncludeFieldset = screen.getByRole('group', { name: 'Select elements to include' })
+      await act(() => user.click(getByLabelText(elementsToIncludeFieldset, 'q3')))
+      await act(() => user.click(getByLabelText(elementsToIncludeFieldset, 'q6')))
+
+      await act(() => user.click(screen.getByText('Create panel')))
+
+      // Assert
+      expect(onChange).toHaveBeenCalledWith({
+        ...page,
+        elements: [
+          { name: 'q1', title: '1?', type: 'text' },
+          { name: 'q2', title: '2?', type: 'text' },
+          {
+            type: 'panel',
+            elements: [
+              { name: 'q3', type: 'html', html: '3' },
+              { name: 'q6', title: '6?', type: 'text' }
+            ]
+          },
+          {
+            type: 'panel',
+            elements: [
+              { name: 'q4', title: '4?', type: 'text' },
+              { name: 'q5', title: '5?', type: 'text' }
+            ]
+          }
+        ]
+      })
+    })
+  })
+})
