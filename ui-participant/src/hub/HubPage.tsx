ca1cbc6424ed87705c0521115c63223896cfde46
Matt Bemis <MatthewBemis@users.noreply.github.com>
2023-07-05 16:56:20 -0400

    [JN-444] Fix "Welcome to study" banner not appearing (#469)

--- ui-participant/src/hub/HubPage.tsx
+++ ui-participant/src/hub/HubPage.tsx
@@ -17,7 +17,7 @@ export default function HubPage() {
   const { enrollees } = useUser()
 
   const hubUpdate = useHubUpdate()
-  const [displayedHubMessage, setDisplayedHubMessage] = useState(hubUpdate?.message)
+  const [showMessage, setShowMessage] = useState(true)
 
   const unjoinedStudies = portal.portalStudies.filter(pStudy => !userHasJoinedPortalStudy(pStudy, enrollees))
   const hasUnjoinedStudies = unjoinedStudies.length > 0
@@ -28,14 +28,14 @@ export default function HubPage() {
         className="hub-dashboard-background flex-grow-1"
         style={{ background: 'linear-gradient(270deg, #D5ADCC 0%, #E5D7C3 100%' }}
       >
-        {!!displayedHubMessage && (
+        {!!hubUpdate?.message && showMessage && (
           <HubMessageAlert
-            message={displayedHubMessage}
+            message={hubUpdate.message}
             className="mx-1 mx-md-auto my-1 my-md-5 shadow-sm"
             role="alert"
             style={{ maxWidth: 768 }}
             onDismiss={() => {
-              setDisplayedHubMessage(undefined)
+              setShowMessage(false)
             }}
           />
         )}
