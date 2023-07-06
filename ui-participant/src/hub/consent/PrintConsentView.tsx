dc3867f277a2f567f3caff8fd8afe31a2969b9ee
Devon Bush <devonbush@gmail.com>
2023-07-06 15:18:50 -0400

    more import fix

--- ui-participant/src/hub/consent/PrintConsentView.tsx
+++ ui-participant/src/hub/consent/PrintConsentView.tsx
@@ -3,15 +3,13 @@ import { useParams } from 'react-router-dom'
 import { Model } from 'survey-core'
 import { Survey as SurveyComponent } from 'survey-react-ui'
 
-import { surveyJSModelFromForm } from '@juniper/ui-core'
+import { surveyJSModelFromForm, makeSurveyJsData } from '@juniper/ui-core'
 
 import Api, { Answer, Enrollee } from 'api/api'
 import { usePortalEnv } from 'providers/PortalProvider'
 import { useUser } from 'providers/UserProvider'
 import { DocumentTitle } from 'util/DocumentTitle'
 import { PageLoadingIndicator } from 'util/LoadingSpinner'
-import { makeSurveyJsData } from '@juniper/ui-core'
-
 import { enrolleeForStudy } from './ConsentView'
 
 /**
