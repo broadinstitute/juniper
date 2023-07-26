47f473ad2f37174d69c59ab71c6a64913d45d613
Matt Bemis <mbemis@broadinstitute.org>
2023-07-26 10:29:17 -0400

    scratchbox

--- ui-admin/src/forms/designer/NewQuestionForm.tsx
+++ ui-admin/src/forms/designer/NewQuestionForm.tsx
@@ -7,6 +7,8 @@ import { QuestionDesigner } from './QuestionDesigner'
 import { TextInput } from 'components/forms/TextInput'
 import { baseQuestions } from './questions/questionTypes'
 import _ from 'lodash'
+import { Textarea } from '../../components/forms/Textarea'
+import { questionFromRawText } from '../../util/pearlSurveyUtils'
 
 type NewQuestionFormProps = {
     onCreate: (newQuestion: Question) => void
@@ -18,6 +20,8 @@ export const NewQuestionForm = (props: NewQuestionFormProps) => {
   const { onCreate, readOnly } = props
   const [selectedQuestionType, setSelectedQuestionType] = useState<QuestionType>('text')
   const [questionName, setQuestionName] = useState<string>('')
+  const [freetextMode, setFreetextMode] = useState<boolean>(false)
+  const [freetext, setFreetext] = useState<string>('')
 
   const [question, setQuestion] = useState<Question>(baseQuestions[selectedQuestionType])
 
@@ -42,7 +46,13 @@ export const NewQuestionForm = (props: NewQuestionFormProps) => {
               const newQuestionType = e.target.value as QuestionType
               setSelectedQuestionType(newQuestionType)
               // @ts-ignore
-              setQuestion({ ...baseQuestions[newQuestionType], ...question, type: newQuestionType })
+              if (e.target.value === 'autofill') {
+                setFreetextMode(true)
+              } else {
+                // @ts-ignore
+                setQuestion({ ...baseQuestions[newQuestionType], ...question, type: newQuestionType })
+                setFreetextMode(false)
+              }
             }}>
             <option value="text">Text</option>
             <option value="checkbox">Checkbox</option>
@@ -50,10 +60,40 @@ export const NewQuestionForm = (props: NewQuestionFormProps) => {
             <option value="medications">Medications</option>
             <option value="radiogroup">Radio group</option>
             <option value="signaturepad">Signature</option>
+            <option value="autofill">Automatically detect my question type (freetext)</option>
           </select>
         </div>
 
-        { selectedQuestionType && <QuestionDesigner
+        { freetextMode && <div className="mb-3">
+          <Textarea
+            description="Enter your question text here.
+            We'll try to automatically detect the question type and populate the fields."
+            disabled={readOnly}
+            label="Freetext"
+            rows={2}
+            value={freetext}
+            onChange={value => {
+              setFreetext(value)
+              const newQuestionObj = questionFromRawText(value)
+              const newType = newQuestionObj.type as QuestionType
+              setSelectedQuestionType(newType)
+              console.log(newQuestionObj)
+              //todo: which types actually need to be set here?
+              setQuestion({
+                ...question,
+                // @ts-ignore
+                type: newType,
+                name: questionName,
+                title: newQuestionObj.title || '',
+                //TODO: harmonize the question choice types
+                // @ts-ignore
+                choices: newQuestionObj.choices
+              })
+            }}
+          />
+        </div> }
+
+        { (freetext || !freetextMode) && selectedQuestionType && <QuestionDesigner
           question={question}
           showName={false}
           readOnly={readOnly}
