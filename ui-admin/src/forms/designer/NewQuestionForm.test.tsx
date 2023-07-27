4049a9622ea5bfb7a8f1afab23d764113212db0a
Matt Bemis <mbemis@broadinstitute.org>
2023-07-27 16:18:01 -0400

    scratchbox

--- ui-admin/src/forms/designer/NewQuestionForm.test.tsx
+++ ui-admin/src/forms/designer/NewQuestionForm.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import { NewQuestionForm } from './NewQuestionForm'
 import { questionTypeDescriptions } from './questions/questionTypes'
+import userEvent from '@testing-library/user-event'
 
 describe('NewQuestionForm', () => {
   test('renders the default view for a new question', () => {
@@ -13,7 +14,7 @@ describe('NewQuestionForm', () => {
     const questionTypeSelect = screen.getByLabelText('Question type')
     expect(questionStableIdInput).toBeInTheDocument()
     expect(questionTypeSelect).toBeInTheDocument()
-    expect((questionTypeSelect as HTMLSelectElement).value).toBe('text')
+    expect((questionTypeSelect as HTMLSelectElement).value).toBe('placeholder')
   })
 
   test('updates to the appropriate QuestionDesigner when a new question type is selected', () => {
@@ -27,5 +28,36 @@ describe('NewQuestionForm', () => {
     //Assert
     expect((questionTypeSelect as HTMLSelectElement).value).toBe('checkbox')
     expect(screen.getByText(questionTypeDescriptions.checkbox)).toBeInTheDocument()
+  })
+
+  test('renders freetext input', () => {
+    //Arrange
+    render(<NewQuestionForm onCreate={() => jest.fn()} readOnly={false} />)
+
+    //Act
+    const questionTypeSelect = screen.getByLabelText('Question type')
+    fireEvent.change(questionTypeSelect, { target: { value: 'freetext' } })
+
+    //Assert
+    const freetextInput = screen.getByLabelText('Freetext')
+    expect((questionTypeSelect as HTMLSelectElement).value).toBe('freetext')
+    expect(freetextInput as HTMLInputElement).toBeInTheDocument()
+    expect(screen.getByText(questionTypeDescriptions.text)).toBeInTheDocument() //initial question type is text
+  })
+
+  test('updates to the appropriate QuestionDesigner based on freetext input', async () => {
+    //Arrange
+    render(<NewQuestionForm onCreate={() => jest.fn()} readOnly={false}/>)
+
+    //Act
+    const questionTypeSelect = screen.getByLabelText('Question type')
+    fireEvent.change(questionTypeSelect, {target: {value: 'freetext'}})
+    const freetextInput = screen.getByLabelText('Freetext') as HTMLInputElement
+    userEvent.pointer({target: freetextInput, offset: 0, keys: '[MouseLeft]'})
+    userEvent.keyboard('This is a question!\nThis is an option!\nThis is another option!')
+
+    //Assert
+    await waitFor(() => expect((questionTypeSelect as HTMLSelectElement).value).toBe('radiogroup'))
+    await waitFor(() => expect(screen.getByText(questionTypeDescriptions.radiogroup)).toBeInTheDocument())
   })
 })
--- ui-admin/src/forms/designer/NewQuestionForm.tsx
+++ ui-admin/src/forms/designer/NewQuestionForm.tsx
@@ -7,6 +7,8 @@ import { QuestionDesigner } from './QuestionDesigner'
 import { TextInput } from 'components/forms/TextInput'
 import { baseQuestions } from './questions/questionTypes'
 import _ from 'lodash'
+import { Textarea } from 'components/forms/Textarea'
+import { questionFromRawText } from 'util/pearlSurveyUtils'
 
 type NewQuestionFormProps = {
     onCreate: (newQuestion: Question) => void
@@ -16,9 +18,10 @@ type NewQuestionFormProps = {
 /** UI for creating a new question. */
 export const NewQuestionForm = (props: NewQuestionFormProps) => {
   const { onCreate, readOnly } = props
-  const [selectedQuestionType, setSelectedQuestionType] = useState<QuestionType>('text')
 
-  const [question, setQuestion] = useState<Question>(baseQuestions[selectedQuestionType])
+  const [question, setQuestion] = useState<Question>(baseQuestions['text'])
+  const [selectedQuestionType, setSelectedQuestionType] = useState<QuestionType | undefined>(undefined)
+  const [freetext, setFreetext] = useState<string | undefined>(undefined)
   const { name: questionName } = question
 
   return (
@@ -36,22 +39,54 @@ export const NewQuestionForm = (props: NewQuestionFormProps) => {
             />
           </div>
           <label className="form-label" htmlFor="questionType">Question type</label>
-          <select id="questionType" className="form-select" value={selectedQuestionType}
+          <select id="questionType" defaultValue="placeholder" className="form-select" value={selectedQuestionType}
             onChange={e => {
-              const newQuestionType = e.target.value as QuestionType
-              setSelectedQuestionType(newQuestionType)
-              setQuestion({ ...baseQuestions[newQuestionType], ...question, type: newQuestionType } as Question)
+              if (e.target.value === 'freetext') {
+                setFreetext('')
+              } else {
+                const newQuestionType = e.target.value as QuestionType
+                setSelectedQuestionType(newQuestionType)
+                setQuestion({ ...baseQuestions[newQuestionType], ...question, type: newQuestionType } as Question)
+                setFreetext(undefined)
+              }
             }}>
+            <option value="placeholder" disabled={true}>Select a question type</option>
             <option value="text">Text</option>
             <option value="checkbox">Checkbox</option>
             <option value="dropdown">Dropdown</option>
             <option value="medications">Medications</option>
             <option value="radiogroup">Radio group</option>
             <option value="signaturepad">Signature</option>
+            <option value="freetext">Automatically detect my question type (freetext)</option>
           </select>
         </div>
 
-        { selectedQuestionType && <QuestionDesigner
+        { freetext !== undefined && <div className="mb-3">
+          <Textarea
+            description="Enter your question text here.
+            As you type, we'll try to automatically detect the various question fields."
+            disabled={readOnly}
+            label="Freetext"
+            rows={2}
+            value={freetext}
+            onChange={value => {
+              setFreetext(value)
+              const newQuestionObj = questionFromRawText(value)
+              const newType = newQuestionObj.type as QuestionType
+              setSelectedQuestionType(newType)
+              //questionFromRawText can only reasonably predict the type of question, it's choices, and it's text.
+              //We'll pick those three fields out and allow the user to decide the rest of the fields explicitly.
+              setQuestion({
+                ...question,
+                type: newType,
+                title: newQuestionObj.title || '',
+                choices: newQuestionObj.choices
+              } as Question)
+            }}
+          />
+        </div> }
+
+        { (freetext !== undefined || selectedQuestionType) && <QuestionDesigner
           question={question}
           showName={false}
           readOnly={readOnly}
@@ -62,10 +97,12 @@ export const NewQuestionForm = (props: NewQuestionFormProps) => {
 
         <Button
           variant="primary"
+          disabled={readOnly || !selectedQuestionType || !questionName}
           onClick={() => {
             //While preserving the state during editing, we may have accumulated some extra fields
             //that don't exist on the final question type. So we need to remove them before saving.
-            const sanitizedQuestion = _.pick(question, Object.keys(baseQuestions[selectedQuestionType])) as Question
+            const sanitizedQuestion = _.pick(question,
+              Object.keys(baseQuestions[selectedQuestionType || 'text'])) as Question
             onCreate(sanitizedQuestion)
           }}
         >
