310a44fcc58f89374a10547f211442e99765810e
Devon Bush <dbush@broadinstitute.org>
2023-08-15 08:08:13 -0400

    lintfix

--- ui-participant/src/login/RedirectFromOAuth.tsx
+++ ui-participant/src/login/RedirectFromOAuth.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from 'react-oidc-context'
 import { usePortalEnv } from 'providers/PortalProvider'
