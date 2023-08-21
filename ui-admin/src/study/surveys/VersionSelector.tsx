2178beedfd1b987d3e442292e29d18f298f7b611
Matt Bemis <mbemis@broadinstitute.org>
2023-08-21 11:20:22 -0400

    better dropdown

--- ui-admin/src/study/surveys/VersionSelector.tsx
+++ ui-admin/src/study/surveys/VersionSelector.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useId, useState } from 'react'
 import LoadingSpinner from 'util/LoadingSpinner'
-import Api, { Survey, VersionedForm } from 'api/api'
+import Api, { VersionedForm } from 'api/api'
 import Modal from 'react-bootstrap/Modal'
 import { Button } from '../../components/forms/Button'
+import { instantToDefaultString } from 'util/timeUtils'
+import Select from 'react-select'
 
-/** component for selecting versions of a survey */
+/** component for selecting versions of a form */
 export default function VersionSelector({
   portalShortcode, stableId, show, setShow,
   previewedVersions, setPreviewedVersions
@@ -13,10 +15,12 @@ export default function VersionSelector({
                                             show: boolean, setShow: (show: boolean) => void
                                             previewedVersions: VersionedForm[],
                                             setPreviewedVersions: (versions: VersionedForm[]) => void}) {
-  const [versionList, setVersionList] = useState<Survey[]>([])
+  const [versionList, setVersionList] = useState<VersionedForm[]>([])
   const [selectedVersion, setSelectedVersion] = useState<number>()
   const [isLoading, setIsLoading] = useState(true)
+  const selectId = useId()
   useEffect(() => {
+    setIsLoading(true)
     Api.getSurveyVersions(portalShortcode, stableId).then(result => {
       setVersionList(result.sort((a, b) => b.version - a.version))
       setIsLoading(false)
@@ -29,27 +33,29 @@ export default function VersionSelector({
     })
   }
 
+  const versionOpts = versionList.map(formVersion => ({
+    label: <span>
+            Version <strong>{ formVersion.version }</strong>
+      <span className="text-muted fst-italic ms-2">
+                ({instantToDefaultString(formVersion.createdAt)})
+      </span>
+    </span>,
+    value: formVersion.version
+  })).filter(opt => previewedVersions.every(previewedVersion => previewedVersion.version !== opt.value))
+
+  const selectedOpt = versionOpts
+    .find(opt => opt.value === selectedVersion!)
+
   return <Modal show={show} onHide={() => setShow(false)}>
     <Modal.Header closeButton>
       <Modal.Title>View a previous version</Modal.Title>
     </Modal.Header>
     <Modal.Body>
-      <p>Selecting a previous survey version will allow you to preview it in a new tab of the survey editor.</p>
       <LoadingSpinner isLoading={isLoading}>
-        <select
-          className="form-control"
-          id="form-preview-version"
-          value={selectedVersion}
-          onChange={e => {
-            setSelectedVersion(parseInt(e.target.value))
-          }}
-        >
-          {versionList.map(version => (
-            <option key={version.version} value={version.version}>
-              {version.version}
-            </option>
-          ))}
-        </select>
+        <p>Selecting a previous form version will allow you to preview it in a new tab of the form editor.</p>
+        <label htmlFor={selectId}>Select version to preview</label>
+        <Select inputId={selectId} options={versionOpts} value={selectedOpt} onChange={opt =>
+          setSelectedVersion(opt?.value)}/>
       </LoadingSpinner>
     </Modal.Body>
     <Modal.Footer>
