405ccf7292fc4ce7c2a7ca40a2b35c9a8ff8fc15
Devon Bush <dbush@broadinstitute.org>
2023-08-22 16:25:09 -0400

    WIP

--- api-admin/src/main/java/bio/terra/pearl/api/admin/controller/notifications/NotificationConfigController.java
+++ api-admin/src/main/java/bio/terra/pearl/api/admin/controller/notifications/NotificationConfigController.java
@@ -6,22 +6,29 @@ import bio.terra.pearl.api.admin.service.notifications.NotificationConfigExtServ
 import bio.terra.pearl.core.model.EnvironmentName;
 import bio.terra.pearl.core.model.admin.AdminUser;
 import javax.servlet.http.HttpServletRequest;
+
+import bio.terra.pearl.core.model.notification.NotificationConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+
+import java.util.UUID;
 
 @Controller
 public class NotificationConfigController implements NotificationConfigApi {
   private AuthUtilService authUtilService;
   private HttpServletRequest request;
   private NotificationConfigExtService notificationConfigExtService;
+  private ObjectMapper objectMapper;
 
   public NotificationConfigController(
-      AuthUtilService authUtilService,
-      HttpServletRequest request,
-      NotificationConfigExtService notificationConfigExtService) {
+          AuthUtilService authUtilService,
+          HttpServletRequest request,
+          NotificationConfigExtService notificationConfigExtService, ObjectMapper objectMapper) {
     this.authUtilService = authUtilService;
     this.request = request;
     this.notificationConfigExtService = notificationConfigExtService;
+    this.objectMapper = objectMapper;
   }
 
   @Override
@@ -32,6 +39,18 @@ public class NotificationConfigController implements NotificationConfigApi {
     var configs =
         notificationConfigExtService.findForStudy(
             adminUser, portalShortcode, studyShortcode, environmentName);
+    return ResponseEntity.ok(configs);
+  }
+
+  @Override
+  public ResponseEntity<Object> replace(
+          String portalShortcode, String studyShortcode, String envName, UUID configId, Object body ) {
+    AdminUser adminUser = authUtilService.requireAdminUser(request);
+    EnvironmentName environmentName = EnvironmentName.valueOfCaseInsensitive(envName);
+    NotificationConfig config = objectMapper.convertValue(body, NotificationConfig.class);
+    var configs =
+            notificationConfigExtService.findForStudy(
+                    adminUser, portalShortcode, studyShortcode, environmentName);
     return ResponseEntity.ok(configs);
   }
 }
--- api-admin/src/main/java/bio/terra/pearl/api/admin/service/notifications/NotificationConfigExtService.java
+++ api-admin/src/main/java/bio/terra/pearl/api/admin/service/notifications/NotificationConfigExtService.java
@@ -1,30 +1,44 @@
 package bio.terra.pearl.api.admin.service.notifications;
 
 import bio.terra.pearl.api.admin.service.AuthUtilService;
+import bio.terra.pearl.core.dao.publishing.PortalEnvironmentChangeRecordDao;
 import bio.terra.pearl.core.model.EnvironmentName;
 import bio.terra.pearl.core.model.admin.AdminUser;
 import bio.terra.pearl.core.model.notification.NotificationConfig;
 import bio.terra.pearl.core.model.portal.Portal;
+import bio.terra.pearl.core.model.portal.PortalEnvironment;
+import bio.terra.pearl.core.model.publishing.PortalEnvironmentChange;
+import bio.terra.pearl.core.model.publishing.StudyEnvironmentChange;
 import bio.terra.pearl.core.model.study.PortalStudy;
 import bio.terra.pearl.core.model.study.StudyEnvironment;
 import bio.terra.pearl.core.service.notification.NotificationConfigService;
+import bio.terra.pearl.core.service.portal.PortalEnvironmentService;
+import bio.terra.pearl.core.service.publishing.PortalDiffService;
 import bio.terra.pearl.core.service.study.StudyEnvironmentService;
 import java.util.List;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class NotificationConfigExtService {
   private NotificationConfigService notificationConfigService;
   private AuthUtilService authUtilService;
   private StudyEnvironmentService studyEnvironmentService;
+  private PortalEnvironmentService portalEnvironmentService;
+  private PortalEnvironmentChangeRecordDao portalEnvironmentChangeRecordDao;
 
   public NotificationConfigExtService(
-      NotificationConfigService notificationConfigService,
-      AuthUtilService authUtilService,
-      StudyEnvironmentService studyEnvironmentService) {
+          NotificationConfigService notificationConfigService,
+          AuthUtilService authUtilService,
+          StudyEnvironmentService studyEnvironmentService, PortalEnvironmentService portalEnvironmentService,
+          PortalEnvironmentChangeRecordDao portalEnvironmentChangeRecordDao) {
     this.notificationConfigService = notificationConfigService;
     this.authUtilService = authUtilService;
     this.studyEnvironmentService = studyEnvironmentService;
+    this.portalEnvironmentService = portalEnvironmentService;
+    this.portalEnvironmentChangeRecordDao = portalEnvironmentChangeRecordDao;
   }
 
   public List<NotificationConfig> findForStudy(
@@ -40,5 +54,35 @@ public class NotificationConfigExtService {
     var configs = notificationConfigService.findByStudyEnvironmentId(studyEnvironment.getId());
     notificationConfigService.attachTemplates(configs);
     return configs;
+  }
+
+  /**
+   * deactivates the notification config with configId, and adds a new config as specified in the update object. Note though that the
+   * portalEnvironmentId and studyEnvironmentId will be set from the portalShortcode and studyShortcode params.
+   * If the update contains a new email template, that template will be created as well.
+   */
+  @Transactional
+  public NotificationConfig replace(String portalShortcode, String studyShortcode, EnvironmentName environmentName, UUID configId,
+                                    NotificationConfig update, AdminUser user) {
+    authUtilService.authUserToPortal(user, portalShortcode);
+    PortalEnvironment portalEnvironment = portalEnvironmentService.findOne(portalShortcode, environmentName).get();
+    authUtilService.authUserToStudy(user, portalShortcode, studyShortcode);
+    StudyEnvironment studyEnvironment =
+            studyEnvironmentService.findByStudy(studyShortcode, environmentName).get();
+    NotificationConfig existing = notificationConfigService.find(configId).get();
+
+    update.cleanForCopying();
+    update.setStudyEnvironmentId(studyEnvironment.getId());
+    update.setPortalEnvironmentId(portalEnvironment.getId());
+    if (update.getEmailTemplate() != null && update.getEmailTemplateId() == null) {
+      // this is a new email template, set the portal appropriately
+      update.getEmailTemplate().setPortalId(portalEnvironment.getPortalId());
+    }
+    NotificationConfig newConfig = notificationConfigService.create(update);
+
+    // once the new creation succeeds, deactivate the prior config.
+    existing.setActive(false);
+    notificationConfigService.update(existing);
+    return newConfig;
   }
 }
