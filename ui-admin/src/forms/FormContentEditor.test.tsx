cb074e1d12e2209fa2ac4fd0d29a6857a7c10c8f
Matt Bemis <mbemis@broadinstitute.org>
2023-08-22 16:47:04 -0400

    add error boundaries to survey editor tabs

---
+++ ui-admin/src/forms/FormContentEditor.test.tsx
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+
+import { FormContentEditor } from './FormContentEditor'
+
+const formContent: string = JSON.stringify({
+  title: 'Test survey',
+  pages: [
+    {}
+  ]
+})
+
+describe('FormContentEditor', () => {
+  it('should trap errors in an ErrorBoundary', () => {
+    // Arrange
+    const { container } = render(<FormContentEditor
+      initialContent={formContent} readOnly={false} onChange={jest.fn()}
+    />)
+
+    // Assert
+    // Our custom ErrorBoundary text
+    expect(container).toHaveTextContent('Something went wrong')
+    // JSON Editor and Preview tabs should still be visible
+    expect(container).toHaveTextContent('JSON Editor')
+    expect(container).toHaveTextContent('Preview')
+  })
+})
