8cac0ec8b737498a29403861927cc24df39bec31
Matt Bemis <MatthewBemis@users.noreply.github.com>
2023-09-08 11:24:57 -0400

    [JN-548] Add Download JSON button to form editor (#531)

--- ui-admin/src/study/surveys/SurveyEditorView.tsx
+++ ui-admin/src/study/surveys/SurveyEditorView.tsx
@@ -8,11 +8,12 @@ import LoadedLocalDraftModal from 'forms/designer/modals/LoadedLocalDraftModal'
 import DiscardLocalDraftModal from 'forms/designer/modals/DiscardLocalDraftModal'
 import { deleteDraft, FormDraft, getDraft, getFormDraftKey, saveDraft } from 'forms/designer/utils/formDraftUtils'
 import { useAutosaveEffect } from '@juniper/ui-core/build/autoSaveUtils'
-import { faClockRotateLeft, faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
+import { faClockRotateLeft, faDownload, faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import VersionSelector from './VersionSelector'
 import { StudyEnvContextT } from '../StudyEnvironmentRouter'
 import { isEmpty } from 'lodash'
+import { saveBlobAsDownload } from 'util/downloadUtils'
 
 type SurveyEditorViewProps = {
   studyEnvContext: StudyEnvContextT
@@ -106,6 +107,25 @@ const SurveyEditorView = (props: SurveyEditorViewProps) => {
             <button className="btn btn-secondary" onClick={() => setShowVersionSelector(true)}>
               <FontAwesomeIcon icon={faClockRotateLeft}/> History
             </button>
+            <Button variant="secondary"
+              disabled={!isEmpty(validationErrors)}
+              tooltip={isEmpty(validationErrors) ?
+                'Download the current contents of the JSON Editor as a JSON file.' :
+                'The form contains invalid JSON. Please correct the errors before downloading.'
+              }
+              onClick={() => {
+                const content = draft?.content || currentForm.content
+                // To get this formatted nicely and not as one giant line, need to parse
+                // this as an object and then stringify the result.
+                const blob = new Blob(
+                  [JSON.stringify(JSON.parse(content), null, 2)],
+                  { type: 'application/json' })
+                const filename = !draft ? `${currentForm.stableId}_v${currentForm.version}.json` :
+                  `${currentForm.stableId}_v${currentForm.version}_draft_${Date.now()}.json`
+                saveBlobAsDownload(blob, filename)
+              }}>
+              <FontAwesomeIcon icon={faDownload}/> Download JSON
+            </Button>
           </h5>
         </div>
         { savingDraft && <span className="detail me-2 ms-2">Saving draft...</span> }
