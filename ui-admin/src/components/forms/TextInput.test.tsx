03e78f506fe3f3da0f9f5c310742dfb862af05a6
Matt Bemis <mbemis@broadinstitute.org>
2023-09-15 11:43:43 -0400

    lint, add test

--- ui-admin/src/components/forms/TextInput.test.tsx
+++ ui-admin/src/components/forms/TextInput.test.tsx
@@ -69,4 +69,19 @@ describe('TextInput', () => {
     const description = screen.getByText('More information')
     expect(input).toHaveAttribute('aria-describedby', description.id)
   })
+
+  it('renders a required TextInput', async () => {
+    // Arrange
+    const user = userEvent.setup()
+
+    // Act
+    render(<TextInput label="My required value" value="" required={true}/>)
+
+    // Assert
+    const input = screen.getByLabelText('My required value*')
+    expect(input).toHaveClass('form-control is-invalid')
+
+    await act(() => user.type(input, 'Y'))
+    expect(input).toHaveClass('form-control')
+  })
 })
