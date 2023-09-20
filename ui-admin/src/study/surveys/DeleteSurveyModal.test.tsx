8a826379e2405e1a911a6af111b235a3f52e0ff7
Matt Bemis <mbemis@broadinstitute.org>
2023-09-20 15:59:09 -0400

    basic ui unit tests for modals

--- ui-admin/src/study/surveys/DeleteSurveyModal.test.tsx
+++ ui-admin/src/study/surveys/DeleteSurveyModal.test.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { mockConfiguredSurvey, mockStudyEnvContext } from 'test-utils/mocking-utils'
+import  { setupRouterTest } from 'test-utils/router-testing-utils'
+import { render, screen } from '@testing-library/react'
+import DeleteSurveyModal from './DeleteSurveyModal'
+import userEvent from '@testing-library/user-event'
 
 describe('DeleteSurveyModal', () => {
-  test('does nothing right now', () => {
-    expect(true).toBe(true)
+  test('enables Delete button when confirmation prompt is filled out', async () => {
+    //Arrange
+    const user = userEvent.setup()
+    const studyEnvContext = mockStudyEnvContext()
+    const { RoutedComponent } = setupRouterTest(<DeleteSurveyModal
+      studyEnvContext={studyEnvContext}
+      show={true}
+      setShow={jest.fn()}
+      selectedSurveyConfig={mockConfiguredSurvey()}/>)
+    render(RoutedComponent)
+
+    //Act
+    //gets the confirmation prompt input
+    const confirmDeleteSurveyInput = screen.getByLabelText('Confirm by typing "delete Survey number one" below.')
+    const deleteButton = screen.getByText('Delete survey')
+    expect(deleteButton).toBeDisabled()
+
+    await user.type(confirmDeleteSurveyInput, 'delete Survey number one')
+
+    //Assert
+    expect(deleteButton).toBeEnabled()
   })
 })
--- ui-admin/src/study/surveys/RemoveSurveyModal.test.tsx
+++ ui-admin/src/study/surveys/RemoveSurveyModal.test.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { mockConfiguredSurvey, mockStudyEnvContext } from 'test-utils/mocking-utils'
+import  { setupRouterTest } from 'test-utils/router-testing-utils'
+import { render, screen } from '@testing-library/react'
+import RemoveSurveyModal from './RemoveSurveyModal'
+import userEvent from '@testing-library/user-event'
 
 describe('RemoveSurveyModal', () => {
-  test('does nothing right now', () => {
-    expect(true).toBe(true)
+  test('enables Remove button when confirmation prompt is filled out', async () => {
+    //Arrange
+    const user = userEvent.setup()
+    const studyEnvContext = mockStudyEnvContext()
+    const { RoutedComponent } = setupRouterTest(<RemoveSurveyModal
+      studyEnvContext={studyEnvContext}
+      show={true}
+      setShow={jest.fn()}
+      selectedSurveyConfig={mockConfiguredSurvey()}/>)
+    render(RoutedComponent)
+
+    //Act
+    const confirmRemoveSurveyInput = screen.getByLabelText('Confirm by typing "remove Survey number one" below.')
+    const removeButton = screen.getByText('Remove survey from Fake study: sandbox')
+    expect(removeButton).toBeDisabled()
+
+    await user.type(confirmRemoveSurveyInput, 'remove Survey number one')
+
+    //Assert
+    expect(removeButton).toBeEnabled()
   })
 })
