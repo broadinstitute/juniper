442d7f4ed55ebbfb5735ec75d7bf932b032fc7e0
Devon <devonbush@gmail.com>
2023-09-20 21:01:50 -0400

    JN-467 add url to JS errors (#552)

---
+++ ui-participant/src/util/loggingUtils.test.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react'
+import { render, screen } from '@testing-library/react'
+import setupErrorLogger from './loggingUtils'
+import Api from 'api/api'
+
+const TestComponent = () => {
+  useEffect(() => {
+    setupErrorLogger()
+  }, [])
+  useEffect(() => {
+    window.setTimeout(() => { throw { message: 'foo' } }, 100)
+  }, [])
+  return <span>
+        Will throw error
+  </span>
+}
+
+
+test('logs JS exceptions', async () => {
+  const logSpy = jest.spyOn(Api, 'log').mockImplementation(jest.fn())
+  render(<TestComponent/>)
+  expect(screen.getByText('Will throw error')).toBeInTheDocument()
+  await new Promise(r => setTimeout(r, 200))
+  expect(logSpy).toHaveBeenCalledTimes(1)
+  expect(logSpy).toHaveBeenCalledWith({
+    eventDetail: '{"message":"foo"}\nhttp://localhost/',
+    eventName: 'jserror', eventType: 'ERROR', stackTrace: undefined
+  })
+})
