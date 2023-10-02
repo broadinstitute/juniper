18441158df13fb5564f2f5920356e3047e48a4a8
Matt Bemis <mbemis@broadinstitute.org>
2023-10-02 10:48:18 -0400

    super basic test

---
+++ ui-core/src/components/MultipleCombobox.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+
+import { MultipleComboBox } from './MultipleCombobox'
+import { ItemValue } from 'src/surveyjs/multiple-combobox-question'
+import medications from 'src/surveyjs/medications.json'
+import { useVirtualizer } from '@tanstack/react-virtual'
+
+jest.mock('@tanstack/react-virtual', () => ({
+  ...jest.requireActual('@tanstack/react-virtual'),
+  useVirtualizer: jest.fn()
+}))
+
+describe('MultipleCombobox', () => {
+  test('renders a combobox', () => {
+    //Arrange
+    render(<MultipleComboBox<ItemValue>
+      id={'test'}
+      itemToString={(item: ItemValue) => item.text}
+      placeholder={'Select an option'}
+      options={[]} />)
+
+    //Assert
+    expect(screen.getByRole('combobox')).toBeInTheDocument()
+    expect(screen.getByLabelText('toggle menu')).toBeInTheDocument()
+  })
+})
