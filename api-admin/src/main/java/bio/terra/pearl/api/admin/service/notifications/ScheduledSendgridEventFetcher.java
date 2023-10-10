1c57c5c5efcf9c954985ce5425e728fa9284b3cd
Matt Bemis <mbemis@broadinstitute.org>
2023-10-10 17:17:39 -0400

    slf4j annotation

--- api-admin/src/main/java/bio/terra/pearl/api/admin/service/notifications/ScheduledSendgridEventFetcher.java
+++ api-admin/src/main/java/bio/terra/pearl/api/admin/service/notifications/ScheduledSendgridEventFetcher.java
@@ -1,9 +1,17 @@
 package bio.terra.pearl.api.admin.service.notifications;
 
 import java.util.concurrent.TimeUnit;
+
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+/*
+ * This class is responsible for fetching Sendgrid events from the Sendgrid API and storing them. Sendgrid
+ * only stores 30 days of email activity history, and we don't want to lose any of that information. This is
+ * important both for auditability and for debugging purposes. This fetcher is scheduled to run every 30 minutes,
+ * and it will fetch all events since the last recorded event.
+ */
 
 @Service
 public class ScheduledSendgridEventFetcher {
--- api-admin/src/main/java/bio/terra/pearl/api/admin/service/notifications/SendgridEventService.java
+++ api-admin/src/main/java/bio/terra/pearl/api/admin/service/notifications/SendgridEventService.java
@@ -7,22 +7,13 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 public class SendgridEventService {
-
-  /*
-   * This class is responsible for fetching Sendgrid events from the Sendgrid API and storing them. Sendgrid
-   * only stores 30 days of email activity history, and we don't want to lose any of that information. This is
-   * important both for auditability and for debugging purposes. This fetcher is scheduled to run every 30 minutes,
-   * and it will fetch all events since the last recorded event.
-   */
-
-  private static final Logger logger = LoggerFactory.getLogger(SendgridEventService.class);
   private SendgridClient sendgridClient;
   private SendgridEventDao sendgridEventDao;
 
@@ -47,7 +38,7 @@ public class SendgridEventService {
     // we've accumulated duplicate events at the fringes of the pages. The database will handle
     // this fine, but this could result in the following log message indicating that we've inserted
     // more events than we actually have.
-    logger.info("Sendgrid activity query returned {} events", events.size());
+    log.info("Sendgrid activity query returned {} events", events.size());
 
     bulkUpsert(events);
   }
@@ -60,7 +51,7 @@ public class SendgridEventService {
   public List<SendgridEvent> getAllRecentSendgridEvents(Instant startDate, Instant endDate) {
     final int PAGE_SIZE = 1000; // This is the maximum page size allowed by SendGrid
 
-    logger.info("Querying for SendGrid activity between {} and {}", startDate, endDate);
+    log.info("Querying for SendGrid activity between {} and {}", startDate, endDate);
 
     List<SendgridEvent> events;
     try {
@@ -71,7 +62,7 @@ public class SendgridEventService {
 
     if (events.size() == PAGE_SIZE) {
       endDate = events.get(events.size() - 1).getLastEventTime();
-      logger.info(
+      log.info(
           "Sendgrid activity query returned the limit of {} events. Querying for additional events between {} and {}",
           PAGE_SIZE,
           startDate,
