b2f5213fde8b4c14e1b2aa3f1512d1922ac48964
Matt Bemis <mbemis@broadinstitute.org>
2023-10-23 13:48:41 -0400

    initial

---
+++ ui-admin/src/portal/siteContent/sectionTemplates.ts
@@ -0,0 +1,12 @@
+//templates for all section types
+export const sectionTemplates = {
+  'faq': {
+    'title': '',
+    'questions': [
+      {
+        'question': '',
+        'answer': ''
+      }
+    ]
+  }
+}
