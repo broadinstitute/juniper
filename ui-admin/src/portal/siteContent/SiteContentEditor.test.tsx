5f3e2faa5ca92f53d8e64eaad9f92e47da6ce68c
Devon Bush <dbush@broadinstitute.org>
2023-10-30 13:38:47 -0400

    lint

--- ui-admin/src/portal/siteContent/SiteContentEditor.test.tsx
+++ ui-admin/src/portal/siteContent/SiteContentEditor.test.tsx
@@ -56,7 +56,7 @@ test('clicking on the Preview tab shows full page preview', async () => {
   const siteContent = mockSiteContent()
   const { RoutedComponent } = setupRouterTest(
     <SiteContentEditor siteContent={siteContent} previewApi={emptyApi} readOnly={false}
-      loadSiteContent={jest.fn()} createNewVersion={jest.fn()} 
+      loadSiteContent={jest.fn()} createNewVersion={jest.fn()}
       switchToVersion={jest.fn()}
       portalEnvContext={mockPortalEnvContext('sandbox')}/>)
   render(RoutedComponent)
