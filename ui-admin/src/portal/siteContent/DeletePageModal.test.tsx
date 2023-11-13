0c488311651f5918fdaed24d0c34bf0ac78907d8
Matt Bemis <mbemis@broadinstitute.org>
2023-11-13 15:04:20 -0500

    update test

--- ui-admin/src/portal/siteContent/DeletePageModal.test.tsx
+++ ui-admin/src/portal/siteContent/DeletePageModal.test.tsx
@@ -6,41 +6,6 @@ import React from 'react'
 import { mockHtmlPage } from 'test-utils/mock-site-content'
 
 describe('DeletePageModal', () => {
-  test('disables Delete button when confirm string is not typed in', async () => {
-    //Arrange
-    const { RoutedComponent } = setupRouterTest(<DeletePageModal
-      onDismiss={jest.fn()}
-      deletePage={jest.fn()}
-      renderedPage={mockHtmlPage()}
-    />)
-
-    render(RoutedComponent)
-
-    //Assert
-    const deleteButton = screen.getByText('Delete')
-    expect(deleteButton).toBeDisabled()
-  })
-
-  test('enables Delete button when confirm string is typed in', async () => {
-    //Arrange
-    const { RoutedComponent } = setupRouterTest(<DeletePageModal
-      onDismiss={jest.fn()}
-      deletePage={jest.fn()}
-      renderedPage={mockHtmlPage()}
-    />)
-
-    render(RoutedComponent)
-
-    //Act
-    const confirmDeleteInput = screen.getByLabelText('Confirm by typing "delete example page" below.')
-    const deleteButton = screen.getByText('Delete')
-
-    await userEvent.type(confirmDeleteInput, 'delete example page')
-
-    //Assert
-    expect(deleteButton).toBeEnabled()
-  })
-
   test('Delete button calls deletePage with the page', async () => {
     //Arrange
     const mockDeletePageFn = jest.fn()
@@ -54,10 +19,8 @@ describe('DeletePageModal', () => {
     render(RoutedComponent)
 
     //Act
-    const confirmDeleteInput = screen.getByLabelText('Confirm by typing "delete example page" below.')
     const deleteButton = screen.getByText('Delete')
 
-    await userEvent.type(confirmDeleteInput, 'delete example page')
     await userEvent.click(deleteButton)
 
     //Assert
