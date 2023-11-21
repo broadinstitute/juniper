cad63d62e8a71f7c52ed28f490a407fd16141043
Dennis <cunningh@broadinstitute.org>
2023-11-20 21:17:46 -0500

    Handle HttpRequestMethodNotSupportedException properly

--- api-admin/src/main/java/bio/terra/pearl/api/admin/controller/GlobalExceptionHandler.java
+++ api-admin/src/main/java/bio/terra/pearl/api/admin/controller/GlobalExceptionHandler.java
@@ -8,11 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
+
 
 @Slf4j
 @RestControllerAdvice
@@ -56,7 +58,8 @@ public class GlobalExceptionHandler {
   @ExceptionHandler({
     NotFoundException.class,
     javax.ws.rs.NotFoundException.class,
-    bio.terra.pearl.core.service.exception.NotFoundException.class
+    bio.terra.pearl.core.service.exception.NotFoundException.class,
+    HttpRequestMethodNotSupportedException.class
   })
   public ResponseEntity<ErrorReport> notFoundExceptionHandler(Exception ex) {
     return buildErrorReport(ex, HttpStatus.NOT_FOUND, request);
