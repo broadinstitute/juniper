71daa88288f8fca93be36007c57a94c8e6bcf6d3
denniscunningham <129115982+denniscunningham@users.noreply.github.com>
2023-11-21 16:22:25 -0500

    [JN-657] Handle HttpRequestMethodNotSupportedException properly (#647)

--- api-participant/src/main/java/bio/terra/pearl/api/participant/controller/GlobalExceptionHandler.java
+++ api-participant/src/main/java/bio/terra/pearl/api/participant/controller/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package bio.terra.pearl.api.participant.controller;
 
 import bio.terra.common.exception.BadRequestException;
 import bio.terra.common.exception.InternalServerErrorException;
+import bio.terra.common.exception.NotFoundException;
 import bio.terra.common.exception.UnauthorizedException;
 import bio.terra.common.exception.ValidationException;
 import bio.terra.pearl.api.participant.model.ErrorReport;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -50,6 +52,11 @@ public class GlobalExceptionHandler {
   })
   public ResponseEntity<ErrorReport> authorizationExceptionHandler(Exception ex) {
     return buildErrorReport(ex, HttpStatus.FORBIDDEN, request);
+  }
+
+  @ExceptionHandler({NotFoundException.class, HttpRequestMethodNotSupportedException.class})
+  public ResponseEntity<ErrorReport> notFoundExceptionHandler(Exception ex) {
+    return buildErrorReport(ex, HttpStatus.NOT_FOUND, request);
   }
 
   // catchall - internal server error
