b9d16a1e80522828d5f259b123542bd28b82fe83
Matt Bemis <mbemis@broadinstitute.org>
2023-12-01 09:53:32 -0500

    cleanup

--- ui-admin/src/portal/publish/StudyEnvDiff.test.tsx
+++ ui-admin/src/portal/publish/StudyEnvDiff.test.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { emptyStudyEnvChange } from './PortalEnvDiffView'
 import StudyEnvDiff from './StudyEnvDiff'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 test('StudyEnvDiff renders the name of the study', () => {
-  const { getByText } = render(<StudyEnvDiff studyName="Test Study"
+  render(<StudyEnvDiff studyName="Test Study"
     studyEnvChange={emptyStudyEnvChange}
     selectedChanges={emptyStudyEnvChange}
     setSelectedChanges={jest.fn()}
   />)
-  expect(getByText('Test Study')).toBeInTheDocument()
+  expect(screen.getByText('Test Study')).toBeInTheDocument()
 })
