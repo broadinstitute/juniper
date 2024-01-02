e26efa85151596ffbaca19d0989d6768b0125521
Connor Barker <connorlbark@gmail.com>
2024-01-02 13:13:33 -0500

    WIP 2

--- ui-admin/src/study/participants/enrolleeView/EnrolleeNotifications.tsx
+++ ui-admin/src/study/participants/enrolleeView/EnrolleeNotifications.tsx
@@ -1,17 +1,38 @@
 import React, { useEffect, useState } from 'react'
-import Api, { Enrollee, Event, Notification } from 'api/api'
+import Api, { Enrollee, Event, Notification, NotificationConfig } from 'api/api'
 import { notificationConfigPath, StudyEnvContextT } from '../../StudyEnvironmentRouter'
 import LoadingSpinner from 'util/LoadingSpinner'
 import { instantToDefaultString } from 'util/timeUtils'
 import NotificationConfigTypeDisplay from '../../notifications/NotifcationConfigTypeDisplay'
 import { Link } from 'react-router-dom'
 
+const eventClassToHumanReadable = (eventClass: string): string => {
+  return eventClass
+    .split('_')
+    .map(str => str.toLowerCase())
+    .map(str => str[0].toUpperCase() + str.substring(1))
+    .join(' ')
+}
+
+type EventNotificationTableEntry = {
+  id: string,
+  createdAt: number,
+  deliveryStatus?: string,
+  deliveryType?: string,
+  notificationConfig?: NotificationConfig,
+  notificationConfigId?: string,
+  sentTo?: string,
+  eventClass?: string,
+}
+
+
 /** loads the list of notifications for a given enrollee and displays them in the UI */
 export default function EnrolleeNotifications({ enrollee, studyEnvContext }:
-                                                  { enrollee: Enrollee, studyEnvContext: StudyEnvContextT }) {
+                                                { enrollee: Enrollee, studyEnvContext: StudyEnvContextT }) {
   const { currentEnv, study, portal, currentEnvPath } = studyEnvContext
   const [notifications, setNotifications] = useState<Notification[]>([])
   const [events, setEvents] = useState<Event[]>([])
+  const [tableData, setTableData] = useState<EventNotificationTableEntry[]>([])
   const [isLoading, setIsLoading] = useState(true)
 
   /** matches each notification to a corresponding config by id */
@@ -22,6 +43,25 @@ export default function EnrolleeNotifications({ enrollee, studyEnvContext }:
     })
   }
 
+  const notificationToTableEntry = (val: Notification): EventNotificationTableEntry => {
+    return {
+      id: val.id,
+      deliveryType: val.deliveryType,
+      deliveryStatus: val.deliveryStatus,
+      notificationConfig: val.notificationConfig,
+      notificationConfigId: val.notificationConfigId,
+      createdAt: val.createdAt
+    }
+  }
+
+  const eventToTableEntry = (val: Event): EventNotificationTableEntry => {
+    return {
+      id: val.id,
+      createdAt: val.createdAt,
+      eventClass: val.eventClass
+    }
+  }
+
   useEffect(() => {
     Api.fetchEnrolleeNotifications(portal.shortcode, study.shortcode, currentEnv.environmentName, enrollee.shortcode)
       .then(response => {
@@ -30,11 +70,32 @@ export default function EnrolleeNotifications({ enrollee, studyEnvContext }:
         Api.fetchEnrolleeEvents(portal.shortcode, study.shortcode, currentEnv.environmentName, enrollee.shortcode)
           .then(response => {
             setEvents(response)
-            console.log(events)
             setIsLoading(false)
           })
       })
   }, [enrollee.shortcode])
+
+  useEffect(() => {
+    const newTableData: EventNotificationTableEntry[] = []
+
+    newTableData.push(...notifications.map(notificationToTableEntry))
+    newTableData.push(...events.map(eventToTableEntry))
+
+    const sorted = newTableData.sort((obj1, obj2) => {
+      if (obj1.createdAt > obj2.createdAt) {
+        return 1
+      }
+
+      if (obj1.createdAt < obj2.createdAt) {
+        return -1
+      }
+
+      return 0
+    })
+
+    setTableData(sorted)
+  }, [events, notifications])
+
   return <div>
     <h5>Notifications</h5>
     <LoadingSpinner isLoading={isLoading}>
@@ -42,6 +103,7 @@ export default function EnrolleeNotifications({ enrollee, studyEnvContext }:
         <thead>
           <tr>
             <th>notification</th>
+            <th>event</th>
             <th>method</th>
             <th>status</th>
             <th>time</th>
@@ -49,25 +111,27 @@ export default function EnrolleeNotifications({ enrollee, studyEnvContext }:
           </tr>
         </thead>
         <tbody>
-          {notifications.map(notification => {
+          {tableData.map(row => {
             const matchedConfig = currentEnv.notificationConfigs
-              .find(cfg => cfg.id === notification.notificationConfigId)
-            return <tr key={notification.id}>
+              .find(cfg => cfg.id === row.notificationConfigId)
+            return <tr key={row.id}>
               <td>
-                <NotificationConfigTypeDisplay config={notification.notificationConfig}/>
+                {row.notificationConfig && <NotificationConfigTypeDisplay config={row.notificationConfig}/>}
+                {row.eventClass && eventClassToHumanReadable(row.eventClass)}
+              </td>
+
+              <td>
+                {row.deliveryType && row.deliveryType}
               </td>
               <td>
-                {notification.deliveryType}
+                {row.deliveryStatus && row.deliveryStatus}
               </td>
               <td>
-                {notification.deliveryStatus}
-              </td>
-              <td>
-                {instantToDefaultString(notification.createdAt)}
+                {instantToDefaultString(row.createdAt)}
               </td>
               <td>
                 {matchedConfig &&
-                                <Link to={notificationConfigPath(matchedConfig, currentEnvPath)}>config</Link>}
+                  <Link to={notificationConfigPath(matchedConfig, currentEnvPath)}>config</Link>}
               </td>
             </tr>
           })}
