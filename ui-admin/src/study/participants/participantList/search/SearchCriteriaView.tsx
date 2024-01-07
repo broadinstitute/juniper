a0ebda368b7d5a0f879d5ee39bb1dfa5f6d8021f
denniscunningham <129115982+denniscunningham@users.noreply.github.com>
2024-01-06 21:45:14 -0500

    Update ui-admin/src/study/participants/participantList/search/SearchCriteriaView.tsx
    Remove dependence on material UI.
    
    Co-authored-by: Matt Bemis <MatthewBemis@users.noreply.github.com>

--- ui-admin/src/study/participants/participantList/search/SearchCriteriaView.tsx
+++ ui-admin/src/study/participants/participantList/search/SearchCriteriaView.tsx
@@ -1,34 +1,34 @@
 import React from 'react'
-import { FacetValue, facetNameAndValueString } from '../../../../api/enrolleeSearch'
-
-import Chip from '@mui/material/Chip'
-import Stack from '@mui/material/Stack'
+import { FacetValue, facetNameAndValueString } from 'api/enrolleeSearch'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 /**
  * Provides a view of the current search criteria showing the facets and values that have been selected,
  * and allowing the user to delete criteria.
  */
 const SearchCriteriaView = ({ facetValues, updateFacetValues }:
-                               { facetValues: FacetValue[],
-                                 updateFacetValues: (values: FacetValue[]) => void,
-                               }) => {
+                              { facetValues: FacetValue[], updateFacetValues: (values: FacetValue[]) => void }) => {
   const handleDelete = (deleteFacetValue: FacetValue) => {
     updateFacetValues(facetValues.filter(facetValue =>
       facetValue.facet.keyName !== deleteFacetValue.facet.keyName))
   }
 
   return (
-    <Stack direction="row" spacing={1}>
+    <div className="d-flex flex-wrap gap-2">
       {facetValues.filter(facetValue => !facetValue.isDefault()).map(facetValue => {
         return (
-          <Chip
+          <button
             key={facetValue.facet.keyName}
-            label={facetNameAndValueString(facetValue)}
-            onDelete={() => handleDelete(facetValue)}
-          />
+            className="btn btn-outline-secondary btn-sm rounded-pill"
+            onClick={() => handleDelete(facetValue)}
+          >
+            {facetNameAndValueString(facetValue)}
+            <FontAwesomeIcon icon={faTimes} className="ms-2"/>
+          </button>
         )
       })}
-    </Stack>
+    </div>
   )
 }
 
