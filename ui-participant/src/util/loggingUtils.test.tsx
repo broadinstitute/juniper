7b474222ef9440e5f2de42105e996d0d064b8674
Devon Bush <dbush@broadinstitute.org>
2024-02-01 12:00:09 -0500

    handling circular metrics references

--- ui-participant/src/util/loggingUtils.test.tsx
+++ ui-participant/src/util/loggingUtils.test.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { render, screen } from '@testing-library/react'
-import setupErrorLogger from './loggingUtils'
+import setupErrorLogger, { logVitals } from './loggingUtils'
 import Api from 'api/api'
 
 const TestComponent = () => {
@@ -25,7 +25,25 @@ test('logs JS exceptions', async () => {
   await new Promise(r => setTimeout(r, 200))
   expect(logSpy).toHaveBeenCalledTimes(1)
   expect(logSpy).toHaveBeenCalledWith({
+    environmentName: 'live',
+    portalShortcode: 'localhost',
     eventDetail: '{"message":"foo"}\nhttp://localhost/',
     eventName: 'jserror', eventType: 'ERROR', stackTrace: undefined
+  })
+})
+
+// See JN-840
+test('handles metrics with circular reference', async () => {
+  const logSpy = jest.spyOn(Api, 'log').mockImplementation(jest.fn())
+  const metricsObj: Record<string, any> = { stuff: 1, things: 'blah' }
+  metricsObj.circular = metricsObj
+
+  logVitals(metricsObj)
+  expect(logSpy).toHaveBeenCalledWith({
+    environmentName: 'live',
+    eventDetail: '{"stuff":1,"things":"blah","circular":"[Circular ~]"}',
+    eventType: 'STATS',
+    eventName: 'webvitals',
+    portalShortcode: 'localhost'
   })
 })
