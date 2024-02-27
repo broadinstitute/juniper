4fd4852a3deb3a1231a42f55dfe6f1d04d697a4e
Devon Bush <dbush@broadinstitute.org>
2024-02-27 14:43:51 -0500

    more substitutor fixes

--- core/src/main/java/bio/terra/pearl/core/service/notification/substitutors/EnrolleeEmailSubstitutor.java
+++ core/src/main/java/bio/terra/pearl/core/service/notification/substitutors/EnrolleeEmailSubstitutor.java
@@ -38,6 +38,8 @@ public class EnrolleeEmailSubstitutor implements StringLookup {
                 "siteLink", getSiteLink(contextInfo.portalEnv(), contextInfo.portal()),
                 "participantSupportEmailLink", getParticipantSupportEmailLink(contextInfo.portalEnv()),
                 "siteMediaBaseUrl", getImageBaseUrl(contextInfo.portalEnv(), contextInfo.portal().getShortcode()),
+                // legacy template substitution support
+                "siteImageBaseUrl", getImageBaseUrl(contextInfo.portalEnv(), contextInfo.portal().getShortcode()),
                 // providing a study isn't required, since emails might come from the portal, rather than a study
                 // but immutable map doesn't allow nulls
                 "study", contextInfo.study() != null ? contextInfo.study() : ""));
