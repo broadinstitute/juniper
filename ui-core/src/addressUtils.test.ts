8734cf3d2a4fadfc3c71f0a8a00129b1f366bcda
Matt Bemis <mbemis@broadinstitute.org>
2024-02-28 07:21:51 -0500

    revert intentionally failing tests

--- ui-core/src/addressUtils.test.ts
+++ ui-core/src/addressUtils.test.ts
@@ -62,8 +62,6 @@ describe('explainAddressValidationResults', () => {
 
     expect(explanation).toHaveLength(1)
 
-    expect(1).toBeGreaterThan(2)
-
     expect(explanation[0]).toEqual(
       'The country and city fields could not be verified. Please check them and try again.'
     )
