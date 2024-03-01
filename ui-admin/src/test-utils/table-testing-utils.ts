1636783ce0d3b31894254747a3829e2dd85693b5
Devon Bush <dbush@broadinstitute.org>
2024-03-01 15:36:49 -0500

    more lintfix

--- ui-admin/src/test-utils/table-testing-utils.ts
+++ ui-admin/src/test-utils/table-testing-utils.ts
@@ -1,9 +1,12 @@
+
+/** gets the element of the table cell at the intersection of the row and column headers */
 export function getTableCell(table: HTMLTableElement, rowHeader: string, colHeader: string) {
   const colIndex = Array.from(table.rows[0].cells).findIndex(cell => cell.textContent === colHeader)
   const rowIndex = Array.from(table.rows).findIndex(row => row.cells[0].textContent === rowHeader)
   return table.rows[rowIndex].cells[colIndex]
 }
 
+/** expects the table cell at the intersection of the row and column headers to have the given text */
 export function expectCellToHaveText(table: HTMLTableElement, rowHeader: string, colHeader: string, text: string) {
   expect(getTableCell(table, rowHeader, colHeader).textContent).toBe(text)
 }
