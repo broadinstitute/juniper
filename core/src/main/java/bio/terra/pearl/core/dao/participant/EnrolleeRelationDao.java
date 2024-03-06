390df5b3a0230bb7898e6185113de7ac0f28913d
ptaheri <ptaheri@broadinstitute.org>
2024-03-05 23:51:23 -0500

    Jn-882 Added checks for the end date

--- core/src/main/java/bio/terra/pearl/core/dao/participant/EnrolleeRelationDao.java
+++ core/src/main/java/bio/terra/pearl/core/dao/participant/EnrolleeRelationDao.java
@@ -25,15 +25,15 @@ public class EnrolleeRelationDao extends BaseMutableJdbiDao<EnrolleeRelation> {
 
 
     public List<EnrolleeRelation> findByEnrolleeIdAndRelationshipType(UUID enrolleeId, RelationshipType type) {
-        return findAllByTwoProperties("enrollee_id", enrolleeId,"relationship_type", type);
+        return findAllValidByTwoProperties("enrollee_id", enrolleeId,"relationship_type", type);
     }
 
     public List<EnrolleeRelation> findByEnrolleeIdsAndRelationshipType(List<UUID> enrolleeIds, RelationshipType type) {
-        return findAllByTwoProperties("relationship_type", type, "enrollee_id", enrolleeIds);
+        return findAllValidByTwoProperties("relationship_type", type, "enrollee_id", enrolleeIds);
     }
 
     public List<EnrolleeRelation> findByTargetEnrolleeId(UUID enrolleeId) {
-        return findAllByProperty("target_enrollee_id", enrolleeId);
+        return findAllValidByProperty("target_enrollee_id", enrolleeId);
     }
 
     public List<EnrolleeRelation> findEnrolleeRelationsByProxyParticipantUser(UUID participantUserId, List<UUID> targetEnrolleeIds) {
@@ -55,6 +55,28 @@ public class EnrolleeRelationDao extends BaseMutableJdbiDao<EnrolleeRelation> {
         for (int i = 0; i < relations.size(); i++) {
             relations.get(i).setTargetEnrollee(enrollees.get(i));
         }
+    }
+
+    protected List<EnrolleeRelation> findAllValidByTwoProperties(String column1Name, Object column1Value,
+                                             String column2Name, Object column2Value) {
+        return jdbi.withHandle(handle ->
+                handle.createQuery("select * from " + tableName + " where " + column1Name + " = :column1Value"
+                                + " and " + column2Name + " = :column2Value and end_date >= NOW();")
+                        .bind("column1Value", column1Value)
+                        .bind("column2Value", column2Value)
+                        .mapTo(clazz)
+                        .list()
+        );
+    }
+
+    protected List<EnrolleeRelation> findAllValidByProperty(String columnName, Object columnValue) {
+        return jdbi.withHandle(handle ->
+                handle.createQuery("select * from " + tableName + " where " + columnName + " = :columnValue "
+                                + " and end_date >= NOW();")
+                        .bind("columnValue", columnValue)
+                        .mapTo(clazz)
+                        .list()
+        );
     }
 
 }
