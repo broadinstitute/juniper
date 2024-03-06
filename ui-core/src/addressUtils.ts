44cb3b6ac5e9a9d3724478f7cd0d81291f5a52c5
Connor Barker <connorlbark@gmail.com>
2024-03-06 10:59:50 -0500

    work

--- ui-core/src/addressUtils.ts
+++ ui-core/src/addressUtils.ts
@@ -3,13 +3,24 @@ import { isEmpty, isNil } from 'lodash'
 import { findDifferencesBetweenObjects } from './objectUtils'
 
 
-const FIELD_TO_ADDR_COMPONENTS : { [index: string]: AddressComponent[] } = {
+const FIELD_TO_ADDR_COMPONENTS: { [index: string]: AddressComponent[] } = {
   'city': ['CITY'],
   'street1': ['STREET_TYPE', 'HOUSE_NUMBER', 'STREET_NAME', 'SUBPREMISE'],
   'street2': [],
   'country': ['COUNTRY'],
   'state': ['STATE_PROVINCE'],
   'postalCode': ['POSTAL_CODE']
+}
+
+const ADDR_COMPONENTS_TO_FIELD: { [index: string]: keyof MailingAddress } = {
+  'CITY': 'city',
+  'STREET_TYPE': 'street1',
+  'HOUSE_NUMBER': 'street1',
+  'STREET_NAME': 'street1',
+  'SUBPREMISE': 'street1',
+  'COUNTRY': 'country',
+  'STATE_PROVINCE': 'state',
+  'POSTAL_CODE': 'postalCode'
 }
 
 const ADDR_COMPONENT_TO_NAME = {
@@ -96,7 +107,49 @@ export function explainAddressValidationResults(
 }
 
 /**
- *
+ * Explains the address validation result errors similarly to above, but separates
+ * errors by field.
+ */
+export function explainAddressValidationResultsByField(
+  validation: AddressValidationResult | undefined
+): { [index: string]: string } {
+  const out: { [index: string]: string } = {}
+
+  if (!validation) {
+    return {}
+  }
+
+  const fieldToMissingComponentNames: { [index: string]: string[] } = {}
+
+  for (const missingComponent of validation.invalidComponents || []) {
+    const field = ADDR_COMPONENTS_TO_FIELD[missingComponent]
+    if (fieldToMissingComponentNames[field]) {
+      fieldToMissingComponentNames[field].push(ADDR_COMPONENT_TO_NAME[missingComponent])
+    } else {
+      fieldToMissingComponentNames[field] = [ADDR_COMPONENT_TO_NAME[missingComponent]]
+    }
+  }
+
+  for (const field of Object.keys(fieldToMissingComponentNames)) {
+    const missingComponentNames = fieldToMissingComponentNames[field]
+
+    if (missingComponentNames.length === 1) {
+      out[field] = `The ${
+        missingComponentNames[0]
+      } could not be verified. Please check and try again.`
+    } else {
+      out[field] = `The ${
+        missingComponentNames.slice(0, missingComponentNames.length - 1).join(', ')
+      } and ${
+        missingComponentNames[missingComponentNames.length - 1]
+      } could not be verified. Please check them and try again.`
+    }
+  }
+
+  return out
+}
+/**
+ * Compares two addresses deeply. Ignores metadata (id, createdAt, etc.)
  */
 export function isSameAddress(addr1: MailingAddress, addr2: MailingAddress): boolean {
   return findDifferencesBetweenObjects(addr1, addr2)
