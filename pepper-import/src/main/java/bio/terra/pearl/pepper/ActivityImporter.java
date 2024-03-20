ed479933ddfdd475867991d180aa840146a8d33a
Devon Bush <dbush@broadinstitute.org>
2024-03-20 13:32:56 -0400

    working parse

--- pepper-import/src/main/java/bio/terra/pearl/pepper/ActivityImporter.java
+++ pepper-import/src/main/java/bio/terra/pearl/pepper/ActivityImporter.java
@@ -1,23 +1,51 @@
 package bio.terra.pearl.pepper;
 
+import bio.terra.pearl.core.model.survey.Survey;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigResolveOptions;
+import org.broadinstitute.ddp.model.activity.definition.ActivityDef;
 import org.broadinstitute.ddp.studybuilder.ActivityBuilder;
+import org.broadinstitute.ddp.util.ConfigUtil;
+import org.broadinstitute.ddp.util.GsonUtil;
+import com.google.gson.Gson;
 
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.file.Path;
 
 public class ActivityImporter extends ActivityBuilder {
-    public ActivityImporter(Path dirPath) {
+    private final Gson gson;
+    private final Config varsConfig;
+    public ActivityImporter(Path dirPath, Config varsConfig) {
         super(dirPath, null, null, null, 1L);
+        this.varsConfig = varsConfig;
+        gson = GsonUtil.standardGson();
     }
 
-    public void buildActivity(Path path) {
-        try {
-            Method m = getClass().getSuperclass().getDeclaredMethod("readDefinitionConfig", new Class<?>[]{String.class, boolean.class});
-            m.setAccessible(true);
-            m.invoke(this, new Object[]{path, true});
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+    public ActivityDef buildActivity(Path path) {
+        Path dirPath = PepperImportCliApp.getFilePath("studies/atcp");
+        File file = dirPath.resolve(path).toFile();
+        if (!file.exists()) {
+            throw new RuntimeException("Activity definition file is missing: " + file);
         }
+
+        Config definition = ConfigFactory.parseFile(file)
+            // going to resolve first the external global variables that might be used in this configuration
+            // using setAllowUnresolved = true so we can do a second pass that will allow us to resolve variables
+            // within the configuration
+            .resolveWith(varsConfig, ConfigResolveOptions.defaults().setAllowUnresolved(false));
+        if (definition.isEmpty()) {
+            throw new RuntimeException("Activity definition file is empty: " + file);
+        }
+
+        ActivityDef activityDef = gson.fromJson(ConfigUtil.toJson(definition), ActivityDef.class);
+        return activityDef;
+    }
+
+    public static Survey convert(ActivityDef activityDef) {
+        activityDef.get
     }
 
 }
--- pepper-import/src/main/java/bio/terra/pearl/pepper/PepperImportCliApp.java
+++ pepper-import/src/main/java/bio/terra/pearl/pepper/PepperImportCliApp.java
@@ -1,8 +1,11 @@
 package bio.terra.pearl.pepper;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,6 +19,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 public class PepperImportCliApp implements CommandLineRunner {
 
+  public static final String ABSOLUTE_SEED_ROOT = "pepper-import/src/main/resources/pepper";
+
   public static void main(String[] args) {
     SpringApplication.run(PepperImportCliApp.class, args);
   }
@@ -23,7 +28,19 @@ public class PepperImportCliApp implements CommandLineRunner {
   @Override
   public void run(String... args) throws IOException {
     log.info("EXECUTING : command line importer");
-    ActivityImporter activityImporter = new ActivityImporter(Path.of("../pepper-apis/studybuilder-cli/studies/atcp/prequal.conf"));
-    activityImporter.buildActivity(Path.of("../pepper-apis/studybuilder-cli/studies/atcp/prequal.conf"));
+    Config varsCfg = ConfigFactory.parseFile(getFilePath("studies/atcp/substitutions.conf").toFile());
+    ActivityImporter activityImporter = new ActivityImporter(getFilePath("studies/atcp"), varsCfg);
+
+    activityImporter.buildActivity(Path.of("prequal.conf"));
+  }
+
+  public static Path getFilePath(String path) {
+    /**
+     * depending on whether you are running gradle or spring boot, the root directory could either be
+     * the root folder or pepper-import.  So strip out api-admin or populate if it's there
+     */
+    String projectDir = System.getProperty("user.dir").replace("/pepper-import", "");
+    String pathName = projectDir + "/" + ABSOLUTE_SEED_ROOT + "/" + path;
+    return Path.of(pathName);
   }
 }
