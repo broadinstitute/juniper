787cf439ed26463aeb972f4bacbf26c19dc42cf0
Devon Bush <dbush@broadinstitute.org>
2024-03-22 12:38:33 -0400

    POC form parser

--- pepper-import/src/main/java/bio/terra/pearl/pepper/ActivityImporter.java
+++ pepper-import/src/main/java/bio/terra/pearl/pepper/ActivityImporter.java
@@ -1,36 +1,63 @@
 package bio.terra.pearl.pepper;
 
 import bio.terra.pearl.core.model.survey.Survey;
+import bio.terra.pearl.core.model.survey.SurveyQuestionDefinition;
+import bio.terra.pearl.core.service.exception.internal.IOInternalException;
+import bio.terra.pearl.populate.dto.survey.SurveyPopDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import com.typesafe.config.ConfigResolveOptions;
+import org.broadinstitute.ddp.content.I18nContentRenderer;
 import org.broadinstitute.ddp.model.activity.definition.ActivityDef;
+import org.broadinstitute.ddp.model.activity.definition.FormActivityDef;
+import org.broadinstitute.ddp.model.activity.definition.FormBlockDef;
+import org.broadinstitute.ddp.model.activity.definition.FormSectionDef;
+import org.broadinstitute.ddp.model.activity.definition.question.QuestionDef;
+import org.broadinstitute.ddp.model.activity.instance.FormSection;
 import org.broadinstitute.ddp.studybuilder.ActivityBuilder;
 import org.broadinstitute.ddp.util.ConfigUtil;
 import org.broadinstitute.ddp.util.GsonUtil;
 import com.google.gson.Gson;
+import org.springframework.stereotype.Service;
 
+import javax.json.Json;
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class ActivityImporter extends ActivityBuilder {
-    private final Gson gson;
-    private final Config varsConfig;
-    public ActivityImporter(Path dirPath, Config varsConfig) {
-        super(dirPath, null, null, null, 1L);
-        this.varsConfig = varsConfig;
-        gson = GsonUtil.standardGson();
+@Service
+public class ActivityImporter {
+    private final Gson gson = GsonUtil.standardGson();
+    private final ObjectMapper objectMapper;
+    private final I18nContentRenderer i18nContentRenderer = new I18nContentRenderer();
+
+    public ActivityImporter(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
     }
 
-    public ActivityDef buildActivity(Path path) {
-        Path dirPath = PepperImportCliApp.getFilePath("studies/atcp");
+    public Survey parsePepperForm(Config varsConfig, Path dirPath, Path path) {
         File file = dirPath.resolve(path).toFile();
         if (!file.exists()) {
             throw new RuntimeException("Activity definition file is missing: " + file);
         }
 
+        FormActivityDef activityDef = buildActivity(file, FormActivityDef.class, varsConfig);
+        // for now just read english translations
+        Map<String, Object> langMap = varsConfig.getConfig("i18n.en").entrySet().stream()
+                .collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue()));
+        return convert(activityDef, langMap);
+    }
+
+    public <T> T buildActivity(File file, Class<T> targetClass, Config varsConfig) {
         Config definition = ConfigFactory.parseFile(file)
             // going to resolve first the external global variables that might be used in this configuration
             // using setAllowUnresolved = true so we can do a second pass that will allow us to resolve variables
@@ -40,12 +67,45 @@ public class ActivityImporter extends ActivityBuilder {
             throw new RuntimeException("Activity definition file is empty: " + file);
         }
 
-        ActivityDef activityDef = gson.fromJson(ConfigUtil.toJson(definition), ActivityDef.class);
+        T activityDef = gson.fromJson(ConfigUtil.toJson(definition), targetClass);
         return activityDef;
     }
 
-    public static Survey convert(ActivityDef activityDef) {
-        activityDef.get
+    public SurveyPopDto convert(FormActivityDef activityDef, Map<String, Object> langMap) {
+        SurveyPopDto survey = SurveyPopDto.builder()
+                            .stableId(activityDef.getActivityCode())
+                            .version(1)
+                            .name(activityDef.getTag())
+                            .build();
+
+        ObjectNode root = objectMapper.createObjectNode();
+        ArrayNode pages = root.putArray("pages");
+        for (FormSectionDef section: activityDef.getSections()) {
+            ObjectNode page = objectMapper.createObjectNode();
+            pages.add(page);
+            ArrayNode elements = objectMapper.createArrayNode();
+            page.set("elements", elements);
+            for (FormBlockDef blockDef : section.getBlocks()) {
+                ObjectNode panel = objectMapper.createObjectNode();
+                elements.add(panel);
+                ArrayNode panelElements = objectMapper.createArrayNode();
+                panel.set("elements", panelElements);
+                for (QuestionDef pepperQuestionDef : blockDef.getQuestions().toList()) {
+                    String questionText = i18nContentRenderer.renderToString(pepperQuestionDef.getPromptTemplate().getTemplateText(), langMap);
+                    SurveyQuestionDefinition questionDef = SurveyQuestionDefinition.builder()
+                            .questionStableId(pepperQuestionDef.getStableId())
+                            .questionText(questionText)
+                            .questionType(pepperQuestionDef.getQuestionType().name())
+                            .build();
+                    JsonNode questionNode = objectMapper.valueToTree(questionDef);
+                    panelElements.add(questionNode);
+                }
+            }
+        }
+        survey.setJsonContent(root);
+        return survey;
     }
+
+
 
 }
--- pepper-import/src/main/java/bio/terra/pearl/pepper/PepperImportCliApp.java
+++ pepper-import/src/main/java/bio/terra/pearl/pepper/PepperImportCliApp.java
@@ -1,46 +1,73 @@
 package bio.terra.pearl.pepper;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
+import bio.terra.pearl.core.model.survey.Survey;
+import bio.terra.pearl.core.service.exception.internal.IOInternalException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * CLI populator -- see the "run" method for argument descriptions. this will no-op if no arguments
- * are passed
+ * CLI importer -- see the "run" method for argument descriptions.
  */
-@SpringBootApplication(scanBasePackages = {"bio.terra.pearl.core", "bio.terra.pearl.populate"})
+@SpringBootApplication(scanBasePackages = {"bio.terra.pearl.core", "bio.terra.pearl.populate", "bio.terra.pearl.pepper"})
 @Slf4j
 public class PepperImportCliApp implements CommandLineRunner {
 
   public static final String ABSOLUTE_SEED_ROOT = "pepper-import/src/main/resources/pepper";
-
+  public static final String OUTPUT_ROOT = "pepper-import/out";
   public static void main(String[] args) {
     SpringApplication.run(PepperImportCliApp.class, args);
   }
 
-  @Override
-  public void run(String... args) throws IOException {
-    log.info("EXECUTING : command line importer");
-    Config varsCfg = ConfigFactory.parseFile(getFilePath("studies/atcp/substitutions.conf").toFile());
-    ActivityImporter activityImporter = new ActivityImporter(getFilePath("studies/atcp"), varsCfg);
+  public static String SUBSTITUTIONS_FILE = "substitutions.conf";
 
-    activityImporter.buildActivity(Path.of("prequal.conf"));
+  @Autowired
+  private ActivityImporter activityImporter;
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @Override
+  public void run(String... args) {
+    log.info("EXECUTING : command line importer");
+    // these two vars should be read from command line eventually
+    String studyDir = "atcp";
+    String formFile = "prequal.conf";
+
+    Config varsCfg = ConfigFactory.parseFile(getFilePath("studies/%s/%s".formatted(studyDir, SUBSTITUTIONS_FILE), ABSOLUTE_SEED_ROOT).toFile());
+
+
+    Survey survey = activityImporter.parsePepperForm(varsCfg, getFilePath("studies/" + studyDir, ABSOLUTE_SEED_ROOT), Path.of(formFile));
+    try {
+      Path outFilePath = getFilePath(formFile, OUTPUT_ROOT);
+      String surveyString = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(survey);
+      Files.write(outFilePath, surveyString.getBytes());
+    } catch (IOException e) {
+      throw new IOInternalException("couldn't write survey", e);
+    }
   }
 
-  public static Path getFilePath(String path) {
-    /**
-     * depending on whether you are running gradle or spring boot, the root directory could either be
-     * the root folder or pepper-import.  So strip out api-admin or populate if it's there
-     */
+
+  /**
+   * Get the absolute path for a file.
+   * depending on whether you are running gradle or spring boot, the root directory could either be
+   * the root folder or pepper-import.  So strip out pepper-import if it's there
+   */
+  public static Path getFilePath(String path, String root) {
+
     String projectDir = System.getProperty("user.dir").replace("/pepper-import", "");
-    String pathName = projectDir + "/" + ABSOLUTE_SEED_ROOT + "/" + path;
+    String pathName = projectDir + "/" + root + "/" + path;
     return Path.of(pathName);
   }
 }
