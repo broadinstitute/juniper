506f01dad1212b344dbe4afa25550120f87cc438
Connor Barker <connorlbark@gmail.com>
2024-03-28 10:22:06 -0400

    feedback

--- ui-participant/src/login/AuthError.tsx
+++ ui-participant/src/login/AuthError.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Navbar from '../Navbar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons'
+import { usePortalEnv } from '../providers/PortalProvider'
 
 /**
  * Displays when there is an error from b2c.
  */
 export default function AuthError() {
+  const portalEnv = usePortalEnv()
+  const supportEmail = portalEnv.portalEnv.portalEnvironmentConfig.emailSourceAddress
   return (
     <div className="container-fluid bg-white min-vh-100 d-flex flex-column p-0">
       <Navbar aria-label="Primary"/>
@@ -14,11 +17,14 @@ export default function AuthError() {
         <div className="fs-1 fw-bold d-flex justify-content-center">
           <div>
             <FontAwesomeIcon className="me-2" icon={faCircleExclamation}/>
-            <span>Oops!</span>
+            <span>Something went wrong</span>
           </div>
         </div>
         <div className="fs-2 fw-light d-flex justify-content-center text-center">
-            Something went wrong with our authentication service. Please try again later.
+          <div>
+            <span>There was an issue with our authentication service. Please try again.</span>
+            <span>If the problem persists, please contact <a href={`mailto:${supportEmail}`}>{supportEmail}</a>.</span>
+          </div>
         </div>
       </main>
     </div>
