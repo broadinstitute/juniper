3a55bfd61d42e7934757df57c04dc557e775641c
Matt Bemis <mbemis@broadinstitute.org>
2024-03-29 15:51:34 -0400

    remove console log

--- ui-admin/src/util/downloadUtils.ts
+++ ui-admin/src/util/downloadUtils.ts
@@ -1,6 +1,5 @@
 /** escapes double quotes with an extra ", and adds double quotes around any values that contain commas or newlines */
 export const escapeCsvValue = (value: string) => {
-  console.log('value', value)
   value = value.replaceAll('"', '""')
   if (value && (value.includes(',') || value.includes('\n') || value.includes('"'))) {
     return `"${value}"`
