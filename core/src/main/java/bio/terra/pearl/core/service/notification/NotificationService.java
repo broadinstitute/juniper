10c905bd40ae0f1344c1634e68c396c1c643b1c3
Matt Bemis <MatthewBemis@users.noreply.github.com>
2024-04-03 14:08:56 -0400

    [JN-794] Fix dangling foreign key when deleting notifications (#831)

--- core/src/main/java/bio/terra/pearl/core/service/notification/NotificationService.java
+++ core/src/main/java/bio/terra/pearl/core/service/notification/NotificationService.java
@@ -1,20 +1,24 @@
 package bio.terra.pearl.core.service.notification;
 
 import bio.terra.pearl.core.dao.notification.NotificationDao;
+import bio.terra.pearl.core.dao.notification.SendgridEventDao;
 import bio.terra.pearl.core.model.notification.Notification;
 import bio.terra.pearl.core.service.CrudService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class NotificationService extends CrudService<Notification, NotificationDao> {
-    private ObjectMapper objectMapper;
-    public NotificationService(NotificationDao dao, ObjectMapper objectMapper) {
+    private final ObjectMapper objectMapper;
+    private final SendgridEventDao sendgridEventDao;
+    public NotificationService(NotificationDao dao, ObjectMapper objectMapper, SendgridEventDao sendgridEventDao) {
         super(dao);
         this.objectMapper = objectMapper;
+        this.sendgridEventDao = sendgridEventDao;
     }
 
     public List<Notification> findByEnrolleeId(UUID enrolleeId) {
@@ -25,7 +29,12 @@ public class NotificationService extends CrudService<Notification, NotificationD
         return notifications;
     }
 
+    @Transactional
     public void deleteByEnrolleeId(UUID enrolleeId) {
+        List<Notification> notifications = dao.findByEnrolleeId(enrolleeId);
+        for (Notification notification : notifications) {
+            sendgridEventDao.deleteByNotificationId(notification.getId());
+        }
         dao.deleteByEnrolleeId(enrolleeId);
     }
 }
