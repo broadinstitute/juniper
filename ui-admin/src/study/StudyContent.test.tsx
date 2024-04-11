28d797a81aa1c3d4d2574c61c1f7cbeccaf29004
Devon Bush <dbush@broadinstitute.org>
2024-04-11 12:18:26 -0400

    test fixes

--- ui-admin/src/study/StudyContent.test.tsx
+++ ui-admin/src/study/StudyContent.test.tsx
@@ -3,8 +3,8 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 
 import StudyContent from './StudyContent'
 import { mockConfiguredSurvey, mockStudyEnvContext, mockSurvey } from 'test-utils/mocking-utils'
-import { setupRouterTest } from '../test-utils/router-testing-utils'
-import Api from '../api/api'
+import { setupRouterTest } from 'test-utils/router-testing-utils'
+import Api from 'api/api'
 
 test('renders surveys in-order', async () => {
   const studyEnvContext = mockStudyEnvContext()
@@ -40,7 +40,7 @@ test('renders a Create Survey modal', async () => {
   await waitFor(() => expect(screen.queryByTestId('loading-spinner')).not.toBeInTheDocument())
   const addSurveyButton = screen.getByTestId('addResearchSurvey')
   fireEvent.click(addSurveyButton)
-  expect(screen.getByText('Create new research')).toBeInTheDocument()
+  expect(screen.getByText('Create new research form')).toBeInTheDocument()
   expect(screen.getByText('Name')).toBeInTheDocument()
   expect(screen.getByText('Stable ID')).toBeInTheDocument()
 })
@@ -53,7 +53,7 @@ test('renders a Create Outreach Survey modal', async () => {
   await waitFor(() => expect(screen.queryByTestId('loading-spinner')).not.toBeInTheDocument())
   const addSurveyButton = screen.getByTestId('addOutreachSurvey')
   fireEvent.click(addSurveyButton)
-  expect(screen.getByText('Create new outreach')).toBeInTheDocument()
+  expect(screen.getByText('Create new outreach form')).toBeInTheDocument()
   expect(screen.getByText('Name')).toBeInTheDocument()
   expect(screen.getByText('Stable ID')).toBeInTheDocument()
 })
