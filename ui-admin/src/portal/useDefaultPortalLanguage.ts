c87d711229edc9356e9e40f730df8c99605a525c
Matt Bemis <mbemis@broadinstitute.org>
2024-04-16 13:18:56 -0400

    tweaks

--- ui-admin/src/portal/useDefaultPortalLanguage.ts
+++ ui-admin/src/portal/useDefaultPortalLanguage.ts
@@ -1,15 +1,14 @@
 import { useContext } from 'react'
 import { PortalContext } from './PortalProvider'
-import { useParams } from 'react-router-dom'
-import { StudyParams } from 'study/StudyRouter'
+import { useStudyEnvParamsFromPath } from '../study/StudyEnvironmentRouter'
 
 /**
  * Returns the default language for the current portal environment.
  */
 export function useDefaultLanguage() {
   const { portal } = useContext(PortalContext)
-  const params = useParams<StudyParams>()
-  const envName: string | undefined = params.studyEnv
+  const studyEnvParams = useStudyEnvParamsFromPath()
+  const envName: string | undefined = studyEnvParams.envName
 
   const defaultLanguage = portal?.portalEnvironments.find(env =>
     env.environmentName === envName)?.portalEnvironmentConfig.defaultLanguage
