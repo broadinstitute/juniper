a9cec5c879e17fb839f45ff77bb27e73e5d2f90d
Connor Barker <connorlbark@gmail.com>
2024-04-19 10:49:12 -0400

    fix comment

--- api-participant/src/main/java/bio/terra/pearl/api/participant/service/AuthUtilService.java
+++ api-participant/src/main/java/bio/terra/pearl/api/participant/service/AuthUtilService.java
@@ -11,10 +11,11 @@ import bio.terra.pearl.core.service.participant.EnrolleeService;
 import bio.terra.pearl.core.service.participant.PortalParticipantUserService;
 import bio.terra.pearl.core.service.portal.PortalService;
 import bio.terra.pearl.core.service.portal.PortalWithPortalUser;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import org.springframework.stereotype.Service;
 
 @Service
 public class AuthUtilService {
@@ -38,7 +39,7 @@ public class AuthUtilService {
    * returns the enrollee if the user is authorized to access/modify it, throws an error otherwise
    */
   public Enrollee authParticipantUserToEnrollee(UUID participantUserId, String enrolleeShortcode) {
-    // for now, a user is only allowed to access an enrollee if it's themselves or their proxy
+    // a user is only allowed to access an enrollee if it's themself or their proxy
     Optional<Enrollee> enrolleeOpt =
         enrolleeService.findByParticipantUserIdAndShortcode(participantUserId, enrolleeShortcode);
     return enrolleeOpt.orElseGet(
