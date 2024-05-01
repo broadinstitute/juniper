3f5642c03c5cd8c1a1ea3cb2c075b38ce40ef4d4
Devon Bush <dbush@broadinstitute.org>
2024-05-01 15:39:14 -0400

    [JN-1042] make admin enrollee routes linkable by id

--- ui-admin/src/study/participants/enrolleeView/useRoutedEnrollee.ts
+++ ui-admin/src/study/participants/enrolleeView/useRoutedEnrollee.ts
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import { StudyParams } from '../../StudyRouter'
 import Api, { Enrollee } from 'api/api'
 import { StudyEnvContextT } from '../../StudyEnvironmentRouter'
 import { useLoadingEffect } from 'api/api-utils'
 
 export type EnrolleeParams = StudyParams & {
-  enrolleeShortcode: string,
+  enrolleeShortcodeOrId: string,
   surveyStableId: string,
   consentStableId: string
 }
@@ -15,15 +15,26 @@ export type EnrolleeParams = StudyParams & {
 export default function useRoutedEnrollee(studyEnvContext: StudyEnvContextT) {
   const { portal, study, currentEnv } = studyEnvContext
   const params = useParams<EnrolleeParams>()
-  const enrolleeShortcode = params.enrolleeShortcode as string
+  const enrolleeShortcodeOrId = params.enrolleeShortcodeOrId as string
   const [enrollee, setEnrollee] = useState<Enrollee>()
+  const navigate = useNavigate()
+  const location = useLocation()
 
   const { isLoading, reload } = useLoadingEffect(async () => {
-    const result = await Api.getEnrollee(
-      portal.shortcode, study.shortcode, currentEnv.environmentName, enrolleeShortcode
-    )
-    setEnrollee(result)
-  }, [enrolleeShortcode])
+    let enrollee = location?.state?.enrollee
+    if (!enrollee) {
+      enrollee = await Api.getEnrollee(portal.shortcode, study.shortcode,
+        currentEnv.environmentName, enrolleeShortcodeOrId)
+    } else {
+      // clear the state so this page will update if we navigate to a new enrollee or refresh
+      window.history.replaceState({}, '')
+    }
+    if (enrollee.shortcode != enrolleeShortcodeOrId) {
+      // we got an id -- reroute to the shortcode path
+      navigate(location.pathname.replace(enrollee.id, enrollee.shortcode), { replace: true, state: { enrollee } })
+    }
+    setEnrollee(enrollee)
+  }, [enrolleeShortcodeOrId])
 
   return { isLoading, enrollee, reload }
 }
