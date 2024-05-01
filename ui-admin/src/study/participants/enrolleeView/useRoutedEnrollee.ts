abeaed0cb23c9e401849bbe0ea3353e9c2db09fa
Devon Bush <dbush@broadinstitute.org>
2024-05-01 17:54:34 -0400

    cleanup

--- ui-admin/src/study/participants/enrolleeView/useRoutedEnrollee.ts
+++ ui-admin/src/study/participants/enrolleeView/useRoutedEnrollee.ts
@@ -23,14 +23,16 @@ export default function useRoutedEnrollee(studyEnvContext: StudyEnvContextT) {
   const { isLoading, reload } = useLoadingEffect(async () => {
     let enrollee = location?.state?.enrollee
     if (!enrollee) {
+      // if we haven't already loaded the enrollee, get it from the server
       enrollee = await Api.getEnrollee(portal.shortcode, study.shortcode,
         currentEnv.environmentName, enrolleeShortcodeOrId)
     } else {
-      // clear the state so this page will update if we navigate to a new enrollee or refresh
+      // if we have, clear the state so the enrollee will be reloaded if the page is refreshed
       window.history.replaceState({}, '')
     }
     if (enrollee.shortcode != enrolleeShortcodeOrId) {
-      // we got an id -- reroute to the shortcode path
+      // the page was routed by id rather than shortcode--use the shortcode route instead
+      // but put the enrollee in state so we don't double-fetch
       navigate(location.pathname.replace(enrollee.id, enrollee.shortcode), { replace: true, state: { enrollee } })
     }
     setEnrollee(enrollee)
