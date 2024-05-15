fe4c962c61230e0f249a6eaeec16750a2d758a40
Devon Bush <dbush@broadinstitute.org>
2024-05-14 23:45:50 -0400

    fixing test

--- ui-admin/src/portal/siteContent/DeleteNavItemModal.test.tsx
+++ ui-admin/src/portal/siteContent/DeleteNavItemModal.test.tsx
@@ -4,6 +4,7 @@ import DeleteNavItemModal from './DeleteNavItemModal'
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 import { mockNavbarItem } from 'test-utils/mock-site-content'
+import { NavbarItemInternal } from '@juniper/ui-core'
 
 describe('DeletePageModal', () => {
   test('Delete button calls delete with the text', async () => {
@@ -37,7 +38,7 @@ describe('DeletePageModal', () => {
     render(<DeleteNavItemModal
       onDismiss={jest.fn()}
       deleteNavItem={mockDeleteItemFn}
-      navItem={navItem}
+      navItem={navItem as NavbarItemInternal}
     />)
     expect(screen.getByText('This will also delete the page')).toBeInTheDocument()
   })
