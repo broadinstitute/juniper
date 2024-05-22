e9a86f1d324d9fea786de8f8e77b5102b9a685b3
Devon <devonbush@gmail.com>
2024-05-22 13:26:30 -0400

    [JN-1077] adding stableId to survey form editor (#907)

--- ui-admin/src/study/surveys/SurveyEditorView.test.tsx
+++ ui-admin/src/study/surveys/SurveyEditorView.test.tsx
@@ -7,7 +7,7 @@ import { mockStudyEnvContext } from 'test-utils/mocking-utils'
 import userEvent from '@testing-library/user-event'
 
 describe('SurveyEditorView', () => {
-  const mockForm: Survey = {
+  const mockForm = ():Survey => ({
     ...defaultSurvey,
     createdAt: 0,
     lastUpdatedAt: 0,
@@ -17,17 +17,17 @@ describe('SurveyEditorView', () => {
     stableId: 'testStableId',
     name: 'Test survey',
     surveyType: 'RESEARCH'
-  }
+  })
 
   test('shows the user a LoadedLocalDraftModal when a draft is loaded', async () => {
     //Arrange
-    const FORM_DRAFT_KEY = getFormDraftKey({ form: mockForm })
+    const FORM_DRAFT_KEY = getFormDraftKey({ form: mockForm() })
     localStorage.setItem(FORM_DRAFT_KEY, JSON.stringify({}))
 
     jest.spyOn(Storage.prototype, 'getItem')
     renderWithRouter(<SurveyEditorView
       studyEnvContext={mockStudyEnvContext()}
-      currentForm={mockForm}
+      currentForm={mockForm()}
       onCancel={jest.fn()}
       onSave={jest.fn()}
     />)
@@ -39,13 +39,13 @@ describe('SurveyEditorView', () => {
 
   test('checks local storage for a draft', async () => {
     //Arrange
-    const FORM_DRAFT_KEY = getFormDraftKey({ form: mockForm })
+    const FORM_DRAFT_KEY = getFormDraftKey({ form: mockForm() })
 
     jest.spyOn(Storage.prototype, 'getItem')
 
     renderWithRouter(<SurveyEditorView
       studyEnvContext={mockStudyEnvContext()}
-      currentForm={mockForm}
+      currentForm={mockForm()}
       onCancel={jest.fn()}
       onSave={jest.fn()}
     />)
@@ -58,24 +58,48 @@ describe('SurveyEditorView', () => {
   test('shows a dropdown with options', async () => {
     renderWithRouter(<SurveyEditorView
       studyEnvContext={mockStudyEnvContext()}
-      currentForm={mockForm}
+      currentForm={mockForm()}
       onCancel={jest.fn()}
       onSave={jest.fn()}
     />)
     expect(screen.getByLabelText('form options menu')).toBeInTheDocument()
     await userEvent.click(screen.getByLabelText('form options menu'))
     await userEvent.click(screen.getByText('Configuration'))
-    expect(screen.getByText(`${mockForm.name} - configuration`)).toBeInTheDocument()
+    expect(screen.getByText(`${mockForm().name} - configuration`)).toBeInTheDocument()
   })
 
   test('allows the user to download the JSON file', async () => {
     renderWithRouter(<SurveyEditorView
       studyEnvContext={mockStudyEnvContext()}
-      currentForm={mockForm}
+      currentForm={mockForm()}
       onCancel={jest.fn()}
       onSave={jest.fn()}
     />)
     await userEvent.click(screen.getByLabelText('form options menu'))
     expect(screen.getByText('Download form JSON')).toBeInTheDocument()
+  })
+
+  test('shows stableId and version', async () => {
+    const stableId = 'testStableId'
+    renderWithRouter(<SurveyEditorView
+      studyEnvContext={mockStudyEnvContext()}
+      currentForm={{ ...mockForm(), stableId }}
+      onCancel={jest.fn()}
+      onSave={jest.fn()}
+    />)
+    expect(screen.getByText('testStableId v12')).toBeInTheDocument()
+    expect(screen.queryByText('published')).not.toBeInTheDocument()
+  })
+
+  test('shows published version if applicable', async () => {
+    const stableId = 'testStableId'
+    renderWithRouter(<SurveyEditorView
+      studyEnvContext={mockStudyEnvContext()}
+      currentForm={{ ...mockForm(), stableId, publishedVersion: 2 }}
+      onCancel={jest.fn()}
+      onSave={jest.fn()}
+    />)
+    expect(screen.getByText('testStableId v12')).toBeInTheDocument()
+    expect(screen.getByText('- published v2')).toBeInTheDocument()
   })
 })
