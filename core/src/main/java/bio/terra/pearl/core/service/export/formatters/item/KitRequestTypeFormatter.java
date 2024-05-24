df57b99b9607d0fac09cfa3d908559ba6e9e59cc
Sampath K. Settipalli <sampath@broadinstitute.org>
2024-05-24 09:15:02 -0500

    JN-1055 . kit-request data import

--- core/src/main/java/bio/terra/pearl/core/service/export/formatters/item/KitRequestTypeFormatter.java
+++ core/src/main/java/bio/terra/pearl/core/service/export/formatters/item/KitRequestTypeFormatter.java
@@ -1,8 +1,12 @@
 package bio.terra.pearl.core.service.export.formatters.item;
 
+import bio.terra.pearl.core.model.kit.KitType;
 import bio.terra.pearl.core.service.export.DataValueExportType;
 import bio.terra.pearl.core.service.kit.KitRequestDto;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.beanutils.PropertyUtils;
 
+@Slf4j
 public class KitRequestTypeFormatter extends PropertyItemFormatter<KitRequestDto> {
     public KitRequestTypeFormatter() {
         super("kitType", KitRequestDto.class);
@@ -14,4 +18,16 @@ public class KitRequestTypeFormatter extends PropertyItemFormatter<KitRequestDto
     public String getExportString(KitRequestDto bean) {
         return bean.getKitType().getName();
     }
+
+    @Override
+    public void importValueToBean(KitRequestDto bean, String exportString) {
+        if (exportString != null) {
+            try {
+                PropertyUtils.setNestedProperty(bean, getPropertyName(), KitType.builder().name(exportString).build());
+            } catch (Exception e) {
+                log.warn("error setting property " + getPropertyName(), e);
+            }
+        }
+    }
+
 }
