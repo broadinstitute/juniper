a756f7118c3550d2c940b48b579113bd1bc95715
Matt Bemis <mbemis@broadinstitute.org>
2024-06-04 15:05:46 -0400

    tests

--- ui-admin/src/study/participants/survey/SurveyResponseView.test.tsx
+++ ui-admin/src/study/participants/survey/SurveyResponseView.test.tsx
@@ -12,26 +12,44 @@ import userEvent from '@testing-library/user-event'
 import { RawEnrolleeSurveyView } from './SurveyResponseView'
 
 describe('RawEnrolleeSurveyView', () => {
-  test('shows the download/print modal', async () => {
-    const enrollee = {
-      ...mockEnrollee(),
-      surveyResponses: [
-        {
-          ...mockSurveyResponse(),
-          answers: [
-            mockAnswer()
-          ]
-        }
-      ]
-    }
+  const mockResponseWithAnswer = {
+    ...mockSurveyResponse(),
+    answers: [
+      mockAnswer()
+    ]
+  }
+
+  test('Printing mode shows the download/print modal', async () => {
     const printSpy = jest.spyOn(window, 'print').mockImplementation(() => 1)
     const { RoutedComponent } = setupRouterTest(
-      <RawEnrolleeSurveyView enrollee={enrollee} studyEnvContext={mockStudyEnvContext()}
+      <RawEnrolleeSurveyView enrollee={mockEnrollee()}
+        studyEnvContext={mockStudyEnvContext()} response={mockResponseWithAnswer}
         configSurvey={mockConfiguredSurvey()} onUpdate={jest.fn()}/>)
     render(RoutedComponent)
     await userEvent.click(screen.getByText('Printing'))
     await waitFor(() => expect(screen.getByText('Done')).toBeVisible())
     await waitFor(() => expect(printSpy).toHaveBeenCalledTimes(1))
+  })
+
+  test('Viewing mode shows survey response view', async () => {
+    const { RoutedComponent } = setupRouterTest(
+      <RawEnrolleeSurveyView enrollee={mockEnrollee()}
+        studyEnvContext={mockStudyEnvContext()} response={mockResponseWithAnswer}
+        configSurvey={mockConfiguredSurvey()} onUpdate={jest.fn()}/>)
+    render(RoutedComponent)
+    const viewingElements = screen.getAllByText('Viewing')
+    expect(viewingElements).toHaveLength(2)
+    await waitFor(() => expect(screen.getByText('Show all questions')).toBeVisible())
+  })
+
+  test('Editing mode shows the survey response editor', async () => {
+    const { RoutedComponent } = setupRouterTest(
+      <RawEnrolleeSurveyView enrollee={mockEnrollee()}
+        studyEnvContext={mockStudyEnvContext()} response={mockResponseWithAnswer}
+        configSurvey={mockConfiguredSurvey()} onUpdate={jest.fn()}/>)
+    render(RoutedComponent)
+    await userEvent.click(screen.getByText('Editing'))
+    await waitFor(() => expect(screen.queryByText('Show all questions')).not.toBeInTheDocument())
   })
 })
 
