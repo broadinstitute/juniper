55cbb7946d0ee5e1634e0f92f04fbd11ad791c4d
Devon Bush <dbush@broadinstitute.org>
2024-07-16 07:31:14 -0400

    more test fixes

--- ui-core/vite.config.ts
+++ ui-core/vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig, ConfigEnv, UserConfig } from 'vite'
+import { defineConfig, ConfigEnv, PluginOption, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
+import typescript from '@rollup/plugin-typescript'
 
 // https://vitejs.dev/config/
 /**
@@ -8,6 +9,23 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
  * https://github.com/DataBiosphere/terra-ui/blob/dev/packages/build-utils/src/baseViteConfig.ts
  */
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const preserveExternalImports = (): PluginOption => {
+    // Depends on running the build from the package's directory.
+    // This is a valid assumption when running the build from a package.json script with yarn or npm.
+    const packageDirectory = process.cwd()
+    return {
+      name: 'vite-plugin-leave-external-imports-unchanged',
+      enforce: 'pre',
+      resolveId: id => {
+        const isInternal = id.startsWith('.') || id.startsWith(`${packageDirectory}/`)
+        return isInternal ? null : {
+          id,
+          external: true
+        }
+      }
+    }
+  }
+
   return {
     base: '/',
     build: {
@@ -35,12 +53,31 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
           chunkFileNames: '[name].js',
           preserveModules: true,
           preserveModulesRoot: 'src'
-        }
+        },
+        // Check types and emit type declarations.
+        // Because the library is built in two different formats, this results in the type declarations
+        // being written twice. This isn't ideal, but it's acceptable to keep the build within Vite
+        // and avoid running tsc separately. Using `@rollup/plugin-typescript` instead of `tsc` keeps
+        // package.json scripts simpler, allows us to use Vite's watcher to regenerate types after changes,
+        // and allows us to fail the Vite build if there are type errors (using noEmitOnError).
+        //
+        // emitDeclarationOnly is specified here because putting it in tsconfig.json breaks check-dts.
+        // noEmitOnError causes the Vite build to fail if there are type errors. This is disabled in
+        // the `dev` package.json script.
+        plugins: [
+          typescript({
+            compilerOptions: {
+              emitDeclarationOnly: true,
+              noEmitOnError: mode !== 'development'
+            }
+          })
+        ]
       }
     },
     plugins: [
       react(),
-      viteTsconfigPaths()
+      viteTsconfigPaths(),
+      preserveExternalImports()
     ]
   }
 })
