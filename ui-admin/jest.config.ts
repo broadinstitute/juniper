fc941b64f1e79c1a89c7d042d9ef5c9f4f9af631
Devon Bush <dbush@broadinstitute.org>
2024-07-16 15:31:41 -0400

    removing build requirement for tests

--- ui-admin/jest.config.ts
+++ ui-admin/jest.config.ts
@@ -12,7 +12,7 @@ export default {
     '\\.(gif|ttf|eot|svg|png|css|scss)$': '<rootDir>/../ui-core/src/test-utils/mockFile.js',
     'react-markdown': '<rootDir>/../node_modules/react-markdown/react-markdown.min.js',
     'micromark': '<rootDir>/../ui-core/src/test-utils/micromark-mock.js',
-    '@juniper/ui-core': '<rootDir>/../ui-core/build/es/index.js',
+    '@juniper/ui-core': '<rootDir>/../ui-core/src/index.ts',
     'authConfig': '<rootDir>/../ui-core/src/test-utils/mockAuthConfig.js'
   }
 }
