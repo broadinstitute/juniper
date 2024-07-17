04846503716cabac16bd581fa9d86c51fc338c6d
Devon Bush <dbush@broadinstitute.org>
2024-07-17 19:58:55 -0400

    hiding email by default

--- ui-admin/src/study/participants/participantList/WithdrawnEnrolleeList.tsx
+++ ui-admin/src/study/participants/participantList/WithdrawnEnrolleeList.tsx
@@ -3,9 +3,16 @@ import { paramsFromContext, StudyEnvContextT } from '../../StudyEnvironmentRoute
 import { renderPageHeader } from '../../../util/pageUtils'
 import { useLoadingEffect } from '../../../api/api-utils'
 import Api, { WithdrawnEnrollee } from '../../../api/api'
-import { basicTableLayout } from '../../../util/tableUtils'
+import { basicTableLayout, ColumnVisibilityControl } from '../../../util/tableUtils'
 import LoadingSpinner from '../../../util/LoadingSpinner'
-import { ColumnDef, getCoreRowModel, getSortedRowModel, SortingState, useReactTable } from '@tanstack/react-table'
+import {
+  ColumnDef,
+  getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+  VisibilityState
+} from '@tanstack/react-table'
 import { instantToDefaultString } from '@juniper/ui-core'
 import { NavBreadcrumb } from '../../../navbar/AdminNavbar'
 import { DocsKey, ZendeskLink } from '../../../util/zendeskUtils'
@@ -22,7 +29,9 @@ type WithdrawnEnrolleeExtract = WithdrawnEnrollee & {
 export default function WithdrawnEnrolleeList({ studyEnvContext }: { studyEnvContext: StudyEnvContextT}) {
   const [enrollees, setEnrollees] = useState<WithdrawnEnrolleeExtract[]>([])
   const [sorting, setSorting] = React.useState<SortingState>([{ 'id': 'createdAt', 'desc': true }])
-
+  const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({
+    'email': false
+  })
   const { isLoading } = useLoadingEffect(async () => {
     const result = await Api.fetchWithdrawnEnrollees(paramsFromContext(studyEnvContext))
     const extracts: WithdrawnEnrolleeExtract[] = result.map(enrollee => ({
@@ -37,6 +46,7 @@ export default function WithdrawnEnrolleeList({ studyEnvContext }: { studyEnvCon
     accessorKey: 'shortcode'
   }, {
     header: 'Email',
+    id: 'email',
     accessorKey: 'userDataObj.username'
   }, {
     header: 'Account created',
@@ -58,8 +68,10 @@ export default function WithdrawnEnrolleeList({ studyEnvContext }: { studyEnvCon
     data: enrollees,
     columns,
     state: {
-      sorting
+      sorting,
+      columnVisibility
     },
+    onColumnVisibilityChange: setColumnVisibility,
     enableRowSelection: true,
     onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
@@ -71,6 +83,9 @@ export default function WithdrawnEnrolleeList({ studyEnvContext }: { studyEnvCon
     <FontAwesomeIcon icon={faInfoCircle}/> More information about the
     <ZendeskLink doc={DocsKey.WITHDRAWAL}> withdrawl process</ZendeskLink>.
     <LoadingSpinner isLoading={isLoading}>
+      <div className="d-flex justify-content-end">
+        <ColumnVisibilityControl table={table}/>
+      </div>
       <div className="d-flex align-items-center justify-content-between">
         { basicTableLayout(table) }
       </div>
