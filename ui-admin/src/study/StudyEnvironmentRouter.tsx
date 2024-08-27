28a914fc68d19bc317a78fa7c9a8d18c5dd8af2d
Matt Bemis <mbemis@broadinstitute.org>
2024-08-27 12:43:20 -0400

    feedback

--- ui-admin/src/study/StudyEnvironmentRouter.tsx
+++ ui-admin/src/study/StudyEnvironmentRouter.tsx
@@ -49,7 +49,7 @@ import { previewApi } from 'util/apiContextUtils'
 import DataImportView from '../portal/DataImportView'
 import DataImportList from '../portal/DataImportList'
 import FamilyRouter from './families/FamilyRouter'
-import { KitCollection } from './kits/KitCollection'
+import { KitCollection } from './kits/kitcollection/KitCollection'
 
 export type StudyEnvContextT = { study: Study, currentEnv: StudyEnvironment, currentEnvPath: string, portal: Portal }
 
@@ -112,8 +112,8 @@ function StudyEnvironmentRouter({ study }: { study: Study }) {
             portalContext={portalContext}/>}/>
           <Route path="participants/*" element={<ParticipantsRouter studyEnvContext={studyEnvContext}/>}/>
           <Route path="families/*" element={<FamilyRouter studyEnvContext={studyEnvContext}/>}/>
+          <Route path="kits/collectKit/*" element={<KitCollection studyEnvContext={studyEnvContext}/>}/>
           <Route path="kits/*" element={<KitsRouter studyEnvContext={studyEnvContext}/>}/>
-          <Route path="collectKit/*" element={<KitCollection studyEnvContext={studyEnvContext}/>}/>
           <Route path="siteContent" element={<SiteContentLoader portalEnvContext={portalEnvContext}/>}/>
           <Route path="media" element={<SiteMediaList portalContext={portalContext} portalEnv={portalEnv}/>}/>
           <Route path="alerts" element={<DashboardSettings currentEnv={portalEnv}
