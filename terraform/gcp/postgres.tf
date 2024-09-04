4141d9761d21a5b10fbfa17338068606642ce5a8
Connor Barker <connorlbark@gmail.com>
2024-09-04 11:16:22 -0400

    untested

--- terraform/gcp/postgres.tf
+++ terraform/gcp/postgres.tf
@@ -1,3 +1,4 @@
+# Create database
 resource "google_sql_database_instance" "d2p" {
   name = "d2p"
   database_version = "POSTGRES_15"
@@ -5,17 +6,66 @@ resource "google_sql_database_instance" "d2p" {
   deletion_protection = false
   settings {
     tier = "db-f1-micro"
-  }
-}
 
-resource "google_sql_user" "users" {
-  name     = "d2p"
-  instance = google_sql_database_instance.d2p.name
-  password = "d2p4eva!"
+    database_flags {
+      name  = "cloudsql.iam_authentication"
+      value = "on"
+    }
+  }
 }
 
 resource "google_sql_database" "database" {
   name     = "d2p"
   instance = google_sql_database_instance.d2p.name
+}
+
+# Create database user
+
+resource "google_sql_user" "users" {
+  name     = "d2p"
+  instance = google_sql_database_instance.d2p.name
+  password = "d2p4eva!"
+  type = "CLOUD_IAM_USER"
+}
+
+# Create GSA and assign roles
+
+resource "google_service_account" "db_sa" {
+  account_id = "db-d2p-gsa"
+}
+
+resource "google_sql_user" "iam_service_account_user" {
+  # Note: for PostgreSQL only, Google Cloud requires that you omit the
+  # ".gserviceaccount.com" suffix
+  # from the service account email due to length limits on database usernames.
+  name     = trimsuffix(google_service_account.db_sa.email, ".gserviceaccount.com")
+  instance = google_sql_database_instance.d2p.name
+  type     = "CLOUD_IAM_SERVICE_ACCOUNT"
+}
+
+# resource "google_project_iam_member" "db_iam_user_cloudsql_instance_user" {
+#   project = "broad-ddp-dev"
+#   role    = "roles/cloudsql.instanceUser"
+#   member  = format("user:%s", google_sql_user.db_iam_user.name)
+# }
+
+# Create KSA + workload identity to link it to GSA with DB access
+
+resource "kubernetes_service_account" "db-d2p-ksa" {
+  metadata {
+    name      = "db-d2p-ksa"
+    namespace = "default"
+    annotations = {
+      "iam.gke.io/gcp-service-account" = "${google_service_account.db_sa.account_id}@broad-ddp-dev.iam.gserviceaccount.com"
+    }
+  }
+}
+
+resource "google_service_account_iam_binding" "workload_identity_binding" {
+  service_account_id = google_service_account.db_sa.name
+  role               = "roles/iam.workloadIdentityUser"
+  members = [
+    "serviceAccount:broad-ddp-dev.svc.id.goog[${kubernetes_service_account.db-d2p-ksa.metadata.0.namespace}/${kubernetes_service_account.db-d2p-ksa.metadata.0.name}]"
+  ]
 }
 
