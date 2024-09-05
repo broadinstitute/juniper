1d365080d14babef8f5f90da5df3fbd846ca4ded
Matt Bemis <mbemis@broadinstitute.org>
2024-09-05 13:46:30 -0400

    add more to comment

--- ui-participant/src/login/RedirectFromOAuth.tsx
+++ ui-participant/src/login/RedirectFromOAuth.tsx
@@ -14,7 +14,7 @@ import {
 import { enrollCurrentUserInStudy } from 'util/enrolleeUtils'
 import { PageLoadingIndicator } from 'util/LoadingSpinner'
 import { filterUnjoinableStudies } from 'Navbar'
-import { logError } from 'util/loggingUtils'
+import { log, logError } from 'util/loggingUtils'
 import { useI18n } from '@juniper/ui-core'
 
 export const RedirectFromOAuth = () => {
@@ -38,6 +38,18 @@ export const RedirectFromOAuth = () => {
       // do nothing and wait until a render after AuthProvider is done.
       // Also, we'll be manipulating state, so we may get rendered more than once before we navigate away, so make sure
       // we only process the return from OAuth once (when the user is still "anonymous")
+
+      if (auth.error && user) {
+        // This case can happen if the user is already logged in and tries to log in again with a consumed oauth state.
+        // The user already has a valid session, so we'll log that this happened but navigate to the hub without hassle.
+        // Note: this logs as INFO because we want to know that this happened, but it's a non-fatal error.
+        log({
+          eventType: 'INFO', eventName: 'oauth-error',
+          stackTrace: auth.error.stack, eventDetail: auth.error.message
+        })
+        navigate('/hub', { replace: true })
+        return
+      }
 
       if (auth.error) {
         logError({ message: auth.error.message || 'error' }, auth.error.stack || 'stack', 'oauth-error')
