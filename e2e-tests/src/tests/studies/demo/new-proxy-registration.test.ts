403396354231b6244e9b6e56adbc94eb9333ceb6
Matt Bemis <mbemis@broadinstitute.org>
2024-09-06 12:39:42 -0400

    fix

--- e2e-tests/src/tests/studies/demo/new-proxy-registration.test.ts
+++ e2e-tests/src/tests/studies/demo/new-proxy-registration.test.ts
@@ -91,9 +91,9 @@ test.describe('Proxy', () => {
       progress = await consent.progress()
       expect(progress).toStrictEqual('Page 2 of 3')
       await consent.agree()
-      await consent.fillIn(data.Consent.VoluntaryParticipation, 'JS', page.locator('body'))
-      await consent.fillIn(data.Consent.PermissionToUseDNA, 'JS', page.locator('body'))
-      await consent.fillIn(data.Consent.AgreeToContact, 'JS', page.locator('body'))
+      await consent.getQuestion(data.Consent.VoluntaryParticipation).fillIn('JS')
+      await consent.getQuestion(data.Consent.PermissionToUseDNA).fillIn('JS')
+      await consent.getQuestion(data.Consent.AgreeToContact).fillIn('JS')
       await consent.clickByRole('button', 'Next')
 
 
