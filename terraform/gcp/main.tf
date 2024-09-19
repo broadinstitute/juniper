22624ef887ddaa5b995bf07da15d6890d5dfa275
Connor Barker <connorlbark@gmail.com>
2024-09-19 16:14:20 -0400

    woo!

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -27,13 +27,13 @@ provider "google" {
 
 data "google_client_config" "provider" {}
 
-provider "kubernetes" {
-  host  = "https://${google_container_cluster.juniper_cluster.endpoint}"
-  token = data.google_client_config.provider.access_token
-  cluster_ca_certificate = base64decode(
-    google_container_cluster.juniper_cluster.master_auth[0].cluster_ca_certificate,
-  )
-}
+# provider "kubernetes" {
+#   host  = "https://${google_container_cluster.juniper_cluster.endpoint}"
+#   token = data.google_client_config.provider.access_token
+#   cluster_ca_certificate = base64decode(
+#     google_container_cluster.juniper_cluster.master_auth[0].cluster_ca_certificate,
+#   )
+# }
 
 
 terraform {
