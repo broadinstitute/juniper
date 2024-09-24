03d2d0b5e89b702ac1c3d867b8f764441ad22a40
Connor Barker <connorlbark@gmail.com>
2024-09-24 09:21:15 -0400

    [JN-1355] Get admin & participant APIs fully running in GCP (#1102)

--- terraform/gcp/cluster.tf
+++ terraform/gcp/cluster.tf
@@ -16,15 +16,6 @@ resource "google_compute_subnetwork" "juniper_subnetwork" {
   ipv6_access_type = "EXTERNAL" # Change to "EXTERNAL" if creating an external loadbalancer
 
   network = google_compute_network.juniper_network.id
-  secondary_ip_range {
-    range_name    = "services-range"
-    ip_cidr_range = "192.168.0.0/24"
-  }
-
-  secondary_ip_range {
-    range_name    = "pod-ranges"
-    ip_cidr_range = "192.168.1.0/24"
-  }
 }
 
 resource "google_container_cluster" "juniper_cluster" {
@@ -39,8 +30,6 @@ resource "google_container_cluster" "juniper_cluster" {
 
   ip_allocation_policy {
     stack_type                    = "IPV4_IPV6"
-    services_secondary_range_name = google_compute_subnetwork.juniper_subnetwork.secondary_ip_range[0].range_name
-    cluster_secondary_range_name  = google_compute_subnetwork.juniper_subnetwork.secondary_ip_range[1].range_name
   }
 
   # Set `deletion_protection` to `true` will ensure that one cannot
