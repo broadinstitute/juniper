aa87ab1f77770a075ad0f9c444c4cb13435c1881
Matt Bemis <mbemis@broadinstitute.org>
2024-10-02 19:23:59 -0400

    participant side

--- ui-core/src/types/study.ts
+++ ui-core/src/types/study.ts
@@ -37,6 +37,7 @@ export type StudyEnvironmentConfig = {
   password: string
   useStubDsm: boolean
   useDevDsmRealm: boolean
+  enableInPersonKits: boolean
 }
 
 export type StudyEnvironmentSurvey = {
