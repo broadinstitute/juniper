352075de51d7d38250be65fc1855e51d27fca6c3
Devon Bush <dbush@broadinstitute.org>
2024-10-02 16:07:52 -0400

    fixing pop errors

--- api-admin/src/main/java/bio/terra/pearl/api/admin/config/InitialPopulate.java
+++ api-admin/src/main/java/bio/terra/pearl/api/admin/config/InitialPopulate.java
@@ -19,17 +19,31 @@ public class InitialPopulate {
 
   @EventListener(ApplicationReadyEvent.class)
   public void populateSeedIfNeeded() throws IOException {
+    if (isJUnitTest()) {
+      log.info("Skipping seed populate for JUnit test");
+      return;
+    }
     if (adminUserService.count() == 0) {
       log.info("No admin users found, populating base");
       baseSeedPopulator.populate("seed");
     } else {
       log.info("Existing admin users found, skipping seed populate");
     }
+
     log.info("Repopulating core language texts");
     baseSeedPopulator.populateLanguageTexts();
     log.info("Repopulating roles and permissions");
     baseSeedPopulator.populateRolesAndPermissions();
     log.info("Repopulating kit types");
     baseSeedPopulator.populateKitTypes();
+  }
+
+  public static boolean isJUnitTest() {
+    for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+      if (element.getClassName().startsWith("org.junit.")) {
+        return true;
+      }
+    }
+    return false;
   }
 }
