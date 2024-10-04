798e79cb54b09037895fc69a5b5f25f55ee938d3
Matt Bemis <mbemis@broadinstitute.org>
2024-10-04 15:43:07 -0400

    bunch of feedback addressed

--- ui-admin/src/navbar/AdminSidebar.tsx
+++ ui-admin/src/navbar/AdminSidebar.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useUser } from '../user/UserProvider'
 import { Link, NavLink, useParams } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
 import { Study } from '@juniper/ui-core'
 import { studyShortcodeFromPath } from '../study/StudyRouter'
 import { useNavContext } from './NavContextProvider'
@@ -44,6 +44,13 @@ const AdminSidebar = ({ config }: { config: Config }) => {
 
   const color = ZONE_COLORS[config.deploymentZone] || ZONE_COLORS['prod']
 
+  //if current path is /kits/scan, we automatically collapse the sidebar
+  useEffect(() => {
+    if (window.location.pathname.endsWith('kits/scan')) {
+      setOpen(false)
+    }
+  }, [])
+
   return <div style={{ backgroundColor: color, minHeight: '100vh', minWidth: open ? '250px' : '50px' }}
     className="p-2 pt-3">
     <>
@@ -55,7 +62,7 @@ const AdminSidebar = ({ config }: { config: Config }) => {
             localStorage.setItem(SHOW_SIDEBAR_KEY, (!open).toString())
           }}
           tooltip={open ? 'Hide sidebar' : 'Show sidebar'}>
-          <FontAwesomeIcon icon={faArrowRightFromBracket}
+          <FontAwesomeIcon icon={faCaretRight}
             className={classNames(open ? 'fa-rotate-180' : '')}/>
         </Button>
       </div>
