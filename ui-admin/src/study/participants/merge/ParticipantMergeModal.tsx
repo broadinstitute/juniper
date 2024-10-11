d845a7c935109219fa43b81b93970f916f254499
Devon Bush <dbush@broadinstitute.org>
2024-10-11 18:12:26 -0400

    WIP

--- ui-admin/src/study/participants/merge/ParticipantMergeModal.tsx
+++ ui-admin/src/study/participants/merge/ParticipantMergeModal.tsx
@@ -1,81 +1,52 @@
-import { useNavContext } from '../navbar/NavContextProvider'
 import React, { useState } from 'react'
-import { Portal, PortalStudy } from '@juniper/ui-core'
 import Modal from 'react-bootstrap/Modal'
 import { Button } from '../components/forms/Button'
-import Select from 'react-select'
-import { ExportData } from '../api/api'
-import { QueryBuilder } from 'react-querybuilder'
 import 'react-querybuilder/dist/query-builder.scss'
-
-export type MergeParticipant = {
-  id?: string,
-  username: string
-}
-
+import { useLoadingEffect } from '../../../api/api-utils'
 
 /**
  * Returns a cohort builder modal
  */
 export default function ParticipantMergeModal({ source, target, portalShortcode, onDismiss }:
- {onDismiss: () => void, source: MergeParticipant }) {
-  const { portalList } = useNavContext()
-  const initialQuery = { combinator: 'and', rules: [] }
-  const [query, setQuery] = useState(initialQuery)
-  const [cohortName, setCohortName] = useState('')
-  const initialPortal = portalList.length == 1 ? portalList[0] : undefined
-  const [selectedPortal, setSelectedPortal] = useState<Portal | undefined>(initialPortal)
-  const initialStudy = selectedPortal?.portalStudies.length == 1 ? selectedPortal?.portalStudies[0] : undefined
-  const [selectedStudy, setSelectedStudy] = useState<PortalStudy | undefined>(initialStudy)
-  const [participantFields, setParticipantFields] = useState<ExportData>()
+ {onDismiss: () => void, source?: string, target?: string, portalShortcode: string }) {
+  const [sourceEmail, setSourceEmail] = useState(source)
+  const [targetEmail, setTargetEmail] = useState(target)
+  const [mergePlan, setMergePlan] = useState<MergePlan>()
 
+  const { isLoading, reload } = useLoadingEffect(() => {
+    if (!sourceEmail || !targetEmail) { return }
+    const mergePlan = Api.fetchMergePlan(portalShortcode, sourceEmail, targetEmail)
+    setMergePlan(mergePlan)
+  },  [sourceEmail, targetEmail])
 
   return <Modal show={true} className="modal-xl" onHide={onDismiss}>
     <Modal.Header closeButton>
       <Modal.Title>Merge Participants</Modal.Title>
     </Modal.Header>
     <Modal.Body>
-      <label htmlFor="cohortName" className="h5">Cohort Name</label>
-      <p className="text-muted mb-2">Give your cohort a unique name. You can reuse this cohort when targeting
-        this same subset of participants for new opportunities.</p>
-      <div className="container">
-        <div className="row my-3">
-          <input type="text" className="form-control" id="cohortName"
-            onChange={e => setCohortName(e.target.value)} value={cohortName}/>
+      <div className="d-flex">
+        <div className="container">
+          <h5>Source Account</h5>
+          <p className="text-muted mb-2">The account that you want to merge.</p>
+          <div className="row my-3">
+            <input type="text" className="form-control" id="source"
+              onChange={e => setSourceEmail(e.target.value)} value={sourceEmail}/>
+          </div>
         </div>
-      </div>
-      <h5>Cohort Source</h5>
-      <p className="text-muted mb-2">Choose the source of your cohort participants.</p>
-      <div className="my-3">
-        <label htmlFor={selectPortalInputId}>Portal</label>
-        <Select inputId={selectPortalInputId} options={portalOptions}
-          value={selectedPortalOption} onChange={portalOnChange}/>
-        <label htmlFor={selectStudyInputId} className="mt-3">Study</label>
-        <Select isDisabled={!selectedPortal} inputId={selectStudyInputId} options={studyOptions}
-          value={selectedStudyOption} onChange={studyOnChange}/>
-      </div>
-      <hr/>
-      <h5>Cohort Criteria</h5>
-      <span className="text-muted mb-2">Choose the criteria for your cohort based on previous
-        survey responses from the participants in your studies. You may define as many rules
-        as you would like to narrow down your cohort.
-      </span>
-      <div className="mt-3">
-        { selectedStudy ?
-          <QueryBuilder
-            fields={fields}
-            query={query}
-            onQueryChange={q => setQuery(q)}
-            addRuleToNewGroups
-            controlClassnames={{ queryBuilder: 'queryBuilder-branches' }}
-          /> :
-          <span className="text-muted fst-italic">You must select a study before selecting cohort criteria</span> }
+        <div className="container">
+          <h5>Target Account</h5>
+          <p className="text-muted mb-2">The account that you want to keep.</p>
+          <div className="row my-3">
+            <input type="text" className="form-control" id="target"
+              onChange={e => setTargetEmail(e.target.value)} value={targetEmail}/>
+          </div>
+        </div>
       </div>
 
     </Modal.Body>
     <Modal.Footer>
       <Button variant="primary"
-        disabled={!cohortName || !selectedStudy}
+        disabled={!mergePlan}
         onClick={() => alert('not yet implemented')}
       >Create</Button>
       <button className="btn btn-secondary" onClick={onDismiss}>Cancel</button>
