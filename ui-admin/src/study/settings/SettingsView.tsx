ba82dbbc07ca2ff6f49f213f0dd01c531abcc02c
Matt Bemis <mbemis@broadinstitute.org>
2024-10-18 13:09:06 -0400

    fix import

--- ui-admin/src/study/settings/SettingsView.tsx
+++ ui-admin/src/study/settings/SettingsView.tsx
@@ -28,7 +28,7 @@ import { doApiLoad } from 'api/api-utils'
 import { Store } from 'react-notifications-component'
 import { successNotification } from 'util/notifications'
 import LoadingSpinner from 'util/LoadingSpinner'
-import { withStateResetOnEnvChange } from '../../util/withStateResetOnEnvChange'
+import { withStateResetOnEnvChange } from 'util/withStateResetOnEnvChange'
 
 
 /** shows a url-routable settings page for both the portal and the selected study */
