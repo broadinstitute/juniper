24eedd307c09f2b4c258dd13c7abbfb39b74978c
Devon <devonbush@gmail.com>
2024-10-21 14:22:31 -0400

    [JN-1425] withdrawn reason (#1150)

--- api-admin/src/main/java/bio/terra/pearl/api/admin/service/auth/context/OperatorAuthContext.java
+++ api-admin/src/main/java/bio/terra/pearl/api/admin/service/auth/context/OperatorAuthContext.java
@@ -1,6 +1,7 @@
 package bio.terra.pearl.api.admin.service.auth.context;
 
 import bio.terra.pearl.core.model.admin.AdminUser;
+import bio.terra.pearl.core.model.audit.DataAuditInfo;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
@@ -13,5 +14,9 @@ public class OperatorAuthContext {
 
   public static OperatorAuthContext of(AdminUser operator) {
     return OperatorAuthContext.builder().operator(operator).build();
+  }
+
+  public DataAuditInfo dataAuditInfo() {
+    return DataAuditInfo.builder().responsibleAdminUserId(getOperator().getId()).build();
   }
 }
