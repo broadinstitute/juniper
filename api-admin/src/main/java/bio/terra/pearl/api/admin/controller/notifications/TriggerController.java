a98e6c9d23fe8b3535e3ca05898c2d6701a9d3d6
Connor Barker <connorlbark@gmail.com>
2025-02-04 09:49:45 -0500

    backend

--- api-admin/src/main/java/bio/terra/pearl/api/admin/controller/notifications/TriggerController.java
+++ api-admin/src/main/java/bio/terra/pearl/api/admin/controller/notifications/TriggerController.java
@@ -12,12 +12,14 @@ import bio.terra.pearl.core.service.exception.NotFoundException;
 import bio.terra.pearl.core.service.rule.EnrolleeContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 
 @Controller
 public class TriggerController implements TriggerApi {
@@ -105,13 +107,24 @@ public class TriggerController implements TriggerApi {
     AdminUser adminUser = authUtilService.requireAdminUser(request);
     AdHocNotification adHoc = objectMapper.convertValue(body, AdHocNotification.class);
     EnvironmentName environmentName = EnvironmentName.valueOfCaseInsensitive(envName);
-    Trigger configUsed =
-        notificationExtService.sendAdHoc(
-            PortalStudyEnvAuthContext.of(
-                adminUser, portalShortcode, studyShortcode, environmentName),
-            adHoc.enrolleeShortcodes,
-            adHoc.customMessages,
-            adHoc.triggerId);
+    Trigger configUsed;
+
+    if (StringUtils.isNotEmpty(adHoc.filter())) {
+      configUsed = notificationExtService.sendAdHoc(
+              PortalStudyEnvAuthContext.of(
+                      adminUser, portalShortcode, studyShortcode, environmentName),
+              adHoc.filter(),
+              adHoc.customMessages,
+              adHoc.triggerId);
+    } else {
+      configUsed = notificationExtService.sendAdHoc(
+              PortalStudyEnvAuthContext.of(
+                      adminUser, portalShortcode, studyShortcode, environmentName),
+              adHoc.enrolleeShortcodes,
+              adHoc.customMessages,
+              adHoc.triggerId);
+    }
+
     return ResponseEntity.ok(configUsed);
   }
 
@@ -143,5 +156,9 @@ public class TriggerController implements TriggerApi {
 
   /** object for specifying an adhoc notification. */
   public record AdHocNotification(
-      List<String> enrolleeShortcodes, UUID triggerId, Map<String, String> customMessages) {}
+          List<String> enrolleeShortcodes,
+          String filter, // if specified, ignore enrolleeShortcodes and use this filter
+          UUID triggerId,
+          Map<String, String> customMessages) {
+  }
 }
--- api-admin/src/main/java/bio/terra/pearl/api/admin/service/notifications/NotificationExtService.java
+++ api-admin/src/main/java/bio/terra/pearl/api/admin/service/notifications/NotificationExtService.java
@@ -3,8 +3,10 @@ package bio.terra.pearl.api.admin.service.notifications;
 import bio.terra.pearl.api.admin.service.auth.AuthUtilService;
 import bio.terra.pearl.api.admin.service.auth.EnforcePortalStudyEnvPermission;
 import bio.terra.pearl.api.admin.service.auth.context.PortalStudyEnvAuthContext;
+import bio.terra.pearl.api.admin.service.enrollee.EnrolleeSearchExtService;
 import bio.terra.pearl.core.model.notification.Trigger;
 import bio.terra.pearl.core.model.participant.Enrollee;
+import bio.terra.pearl.core.model.search.EnrolleeSearchExpressionResult;
 import bio.terra.pearl.core.service.notification.NotificationContextInfo;
 import bio.terra.pearl.core.service.notification.NotificationDispatcher;
 import bio.terra.pearl.core.service.notification.TriggerService;
@@ -15,13 +17,15 @@ import bio.terra.pearl.core.service.rule.EnrolleeContext;
 import bio.terra.pearl.core.service.rule.EnrolleeContextService;
 import bio.terra.pearl.core.service.study.StudyEnvironmentService;
 import bio.terra.pearl.core.service.study.StudyService;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationExtService {
+  private final EnrolleeSearchExtService enrolleeSearchExtService;
   private TriggerService triggerService;
   private NotificationDispatcher notificationDispatcher;
   private EnrolleeService enrolleeService;
@@ -33,14 +37,14 @@ public class NotificationExtService {
   private StudyService studyService;
 
   public NotificationExtService(
-      TriggerService triggerService,
-      NotificationDispatcher notificationDispatcher,
-      EnrolleeService enrolleeService,
-      EnrolleeContextService enrolleeContextService,
-      AuthUtilService authUtilService,
-      StudyEnvironmentService studyEnvironmentService,
-      PortalEnvironmentService portalEnvironmentService,
-      StudyService studyService) {
+          TriggerService triggerService,
+          NotificationDispatcher notificationDispatcher,
+          EnrolleeService enrolleeService,
+          EnrolleeContextService enrolleeContextService,
+          AuthUtilService authUtilService,
+          StudyEnvironmentService studyEnvironmentService,
+          PortalEnvironmentService portalEnvironmentService,
+          StudyService studyService, EnrolleeSearchExtService enrolleeSearchExtService) {
     this.triggerService = triggerService;
     this.notificationDispatcher = notificationDispatcher;
     this.enrolleeService = enrolleeService;
@@ -49,14 +53,15 @@ public class NotificationExtService {
     this.studyEnvironmentService = studyEnvironmentService;
     this.portalEnvironmentService = portalEnvironmentService;
     this.studyService = studyService;
+    this.enrolleeSearchExtService = enrolleeSearchExtService;
   }
 
   @EnforcePortalStudyEnvPermission(permission = "participant_data_edit")
   public Trigger sendAdHoc(
-      PortalStudyEnvAuthContext authContext,
-      List<String> enrolleeShortcodes,
-      Map<String, String> customMessages,
-      UUID configId) {
+          PortalStudyEnvAuthContext authContext,
+          List<String> enrolleeShortcodes,
+          Map<String, String> customMessages,
+          UUID configId) {
 
     List<Enrollee> enrollees = enrolleeService.findAllByShortcodes(enrolleeShortcodes);
     for (Enrollee enrollee : enrollees) {
@@ -67,13 +72,25 @@ public class NotificationExtService {
 
     // bulk load the enrollees
     List<EnrolleeContext> enrolleeRuleData =
-        enrolleeContextService.fetchData(
-            enrollees.stream().map(enrollee -> enrollee.getId()).toList());
+            enrolleeContextService.fetchData(
+                    enrollees.stream().map(enrollee -> enrollee.getId()).toList());
     NotificationContextInfo context = notificationDispatcher.loadContextInfo(config);
     for (EnrolleeContext enrolleeRuleDatum : enrolleeRuleData) {
       notificationDispatcher.dispatchNotification(
-          config, enrolleeRuleDatum, context, customMessages);
+              config, enrolleeRuleDatum, context, customMessages);
     }
     return config;
+  }
+
+  @EnforcePortalStudyEnvPermission(permission = "participant_data_edit")
+  public Trigger sendAdHoc(
+          PortalStudyEnvAuthContext authContext,
+          String expression,
+          Map<String, String> customMessages,
+          UUID configId) {
+
+    List<EnrolleeSearchExpressionResult> enrollees = enrolleeSearchExtService.executeSearchExpression(authContext, expression, null, List.of());
+
+    return sendAdHoc(authContext, enrollees.stream().map(result -> result.getEnrollee().getShortcode()).toList(), customMessages, configId);
   }
 }
