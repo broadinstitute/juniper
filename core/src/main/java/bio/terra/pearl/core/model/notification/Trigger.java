45f0cefd19d6ef0ef1735c25c9d8c709bd9d4de3
Connor Barker <connorlbark@gmail.com>
2025-02-14 12:29:40 -0500

    fix comment

--- core/src/main/java/bio/terra/pearl/core/model/notification/Trigger.java
+++ core/src/main/java/bio/terra/pearl/core/model/notification/Trigger.java
@@ -54,9 +54,9 @@ public class Trigger extends BaseEntity implements VersionedEntityConfig, StudyE
     private EmailTemplate emailTemplate;
     private String rule;
 
-    // for admin notifications, comma separated list of email addresses.
-    // will verify that the email address is valid before sending.
-    // if blank, all admins will be notified
+    // for admin notifications, comma separated list of admin emails.
+    // will not send if the email does not have an associated admin account
+    // if blank, no admins will be notified
     private String adminEmailFilter;
     /**
      * notificationTypes of TASK_REMINDER, if specified, will limit to one type of task.  if null,
