4b75f52f9d841e3fc2b06350010f1d84633396af
Matt Bemis <MatthewBemis@users.noreply.github.com>
2025-02-21 11:22:53 -0500

    Update terraform/gcp/buckets.tf
    Co-authored-by: Connor Barker <connorlbark@gmail.com>

--- terraform/gcp/buckets.tf
+++ terraform/gcp/buckets.tf
@@ -1,7 +1,11 @@
 resource "google_storage_bucket" "participant_documents" {
   name     = var.documents_bucket_name
   location = var.region
+  # no public access allowed
+  public_access_prevention    = "enforced"
 
+  # only allow access if you have iam perms
+  uniform_bucket_level_access = true
   versioning {
     enabled = true
   }
