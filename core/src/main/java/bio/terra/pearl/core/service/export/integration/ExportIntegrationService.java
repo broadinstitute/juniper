35c3c392214d617e611c16048c1d3f0c5a021fb1
Devon <devonbush@gmail.com>
2026-01-20 11:33:42 -0500

    [JN-1766] adding delay between syncs (#1760)

--- core/src/main/java/bio/terra/pearl/core/service/export/integration/ExportIntegrationService.java
+++ core/src/main/java/bio/terra/pearl/core/service/export/integration/ExportIntegrationService.java
@@ -69,8 +69,17 @@ public class ExportIntegrationService extends CrudService<ExportIntegration, Exp
 
     public void doAllExports(ResponsibleEntity operator) {
         List<ExportIntegration> integrations = dao.findAllActiveWithOptions();
-        for (ExportIntegration integration : integrations) {
+        for ( int i = 0; i < integrations.size(); i++ ) {
+            ExportIntegration integration = integrations.get(i);
             doExport(integration, operator);
+            if (i < integrations.size() - 1) {
+                try {
+                    // Wait three minutes so that if related studies are exporting to the same table, Airtable can finish indexing
+                    Thread.sleep(180 * 1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
         }
     }
 
